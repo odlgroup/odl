bc04b815da616f0c33be8f3270b8ae63a84ece69
Holger Kohr <kohr@kth.se>
2015-02-10 23:07:38 +0100

    update name and copyright notice

--- datamodel/coord.py
+++ datamodel/coord.py
@@ -2,22 +2,22 @@
 """
 coord.py -- coordinates for n-dimensional rectangular grids
 
-Copyright 2014 Holger Kohr
+Copyright 2014, 2015 Holger Kohr
 
-This file is part of tomok.
+This file is part of RL.
 
-tomok is free software: you can redistribute it and/or modify
+RL is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-tomok is distributed in the hope that it will be useful,
+RL is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with tomok.  If not, see <http://www.gnu.org/licenses/>.
+along with RL.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from __future__ import unicode_literals
--- test/simple_test.py
+++ test/simple_test.py
@@ -8,25 +8,32 @@ Created on Tue Feb 10 13:04:25 2015
 """
 simple_test.py -- a simple test script
 
-Copyright 2014 Holger Kohr
+Copyright 2014, 2015 Holger Kohr
 
-This file is part of tomok.
+This file is part of RL.
 
-tomok is free software: you can redistribute it and/or modify
+RL is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-tomok is distributed in the hope that it will be useful,
+RL is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with tomok.  If not, see <http://www.gnu.org/licenses/>.
+along with RL.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 
 import numpy as np
 
-import ugrid as ug
+import RL.datamodel.gfunc as gf
+
+vol = np.zeros([100, 100, 100])
+vol[25:75, 25:72, 25:75] = 1.0
+voxel_size = 0.5
+vol_func = gf.Gfunc(fvals=vol, spacing=voxel_size)
+
+vol_func[:, :, 50].display()
