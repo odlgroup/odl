fix broken InputValidationError; try first to broadcast in init methods

src/datamodel/gfunc.py
@@ -81,18 +81,19 @@ class Gfunc(Ugrid):
 
         if shape is None:
             super().__init__(fvals.shape, center, spacing)
-            self._fvals[...] = np.asarray(fvals)
+            self._fvals = np.asarray(fvals)  # copy only if necessary
         else:
             super().__init__(shape, center, spacing)
             self._fvals = np.empty(shape)
 
             # Reshape or broadcast fvals
-            # TODO: deal with datatype mismatch
+            # TODO: deal with datatype mismatch -> do not use this for
+            # re-assignment
             if fvals is not None:
-                try:
-                    self._fvals = np.asarray(fvals).reshape(shape)
-                except ValueError:
-                    self._fvals[...] = np.asarray(fvals)
+                try:  # broadcast if single value is given
+                    self._fvals[...] = float(fvals)
+                except TypeError:  # fvals is array-like
+                    self._fvals[...] = np.asarray(fvals).reshape(shape)
 
     # Essential properties
 
@@ -121,7 +122,7 @@ class Gfunc(Ugrid):
         # TODO: Cythonize
         vec = np.asarray(vec)
         if vec.shape != (self.dim,):
-            raise InputValidationError('vec.shape', (self.dim,))
+            raise InputValidationError(vec.shape, (self.dim,), 'vec.shape')
         if np.any(vec < self.xmin) or np.any(vec > self.xmax):
             raise LookupError("Vector outside grid.")
 

src/datamodel/ugrid.py
@@ -73,20 +73,29 @@ class Ugrid(object):
 
         shape = np.array(shape)
         if not shape.ndim == 1:
-            raise InputValidationError('shape.ndim', 1)
+            raise InputValidationError(shape.ndim, 1, 'shape.ndim')
         self._shape = shape
         dim = len(shape)
 
         # `None` implies all zeros
         center = np.zeros(dim) if center is None else np.array(center)
         if not center.shape == (dim,):
-            raise InputValidationError('center.shape', dim)
+            raise InputValidationError(center.shape, (dim,), 'center.shape')
         self._center = center
 
-        # None implies all ones
-        spacing = np.ones(dim) if spacing is None else np.array(spacing)
-        if not spacing.shape == (dim,):
-            raise InputValidationError('spacing.shape', dim)
+        # `None` implies all ones
+        if spacing is None:
+            spacing = np.ones(dim)
+        else:
+            try:  # single value is broadcast
+                spacing = float(spacing)
+                spacing = spacing * np.ones(dim)
+            except TypeError:  # spacing is an array - test for correct shape
+                spacing = np.array(spacing)
+                if not spacing.shape == (dim,):
+                    raise InputValidationError(spacing.shape, dim,
+                                               'spacing.shape')
+
         if not np.all(spacing > 0):
             raise ValueError('`spacing` must be all positive.')
         self._spacing = spacing
@@ -114,7 +123,8 @@ class Ugrid(object):
         else:
             new_center = np.array(new_center)
         if not new_center.shape == (self.dim,):
-            raise InputValidationError('new_center.shape', (self.dim,))
+            raise InputValidationError(new_center.shape, (self.dim,),
+                                       'new_center.shape')
         self._center = new_center
         self._update_coord()
         # Add code here to update depending cached properties
@@ -136,7 +146,8 @@ class Ugrid(object):
         else:
             new_spacing = np.array(new_spacing)
         if not new_spacing.shape == (self.dim,):
-            raise InputValidationError('new_spacing.shape', (self.dim,))
+            raise InputValidationError(new_spacing.shape, (self.dim,),
+                                       new_spacing.shape)
         self._spacing = new_spacing
         self._update_coord()
         # Add code here to update depending cached properties