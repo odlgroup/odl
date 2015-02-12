ed3f44a94e84f5d3c4896a289e0b2f2941b52f85
Holger Kohr <kohr@kth.se>
2015-02-12 17:43:23 +0100

    write landweber method

--- test/simple_test.py
+++ test/simple_test.py
@@ -22,6 +22,7 @@ along with RL.  If not, see <http://www.gnu.org/licenses/>.
 
 from math import pi
 import numpy as np
+from matplotlib import pyplot as plt
 
 from RL.datamodel import ugrid as ug
 from RL.datamodel import gfunc as gf
@@ -47,19 +48,15 @@ xray_geometry = xray.xray_ct_parallel_geom_3d(sample_grid, detector_grid,
                                               axis=2, angles=tilt_angles,
                                               rotating_sample=True)
 
-#print('initial sample system', xray_geometry.sample.coord_sys(0))
-#print('sample system at -90 deg', xray_geometry.sample.coord_sys(-pi / 2))
-#print('sample system at 90 deg', xray_geometry.sample.coord_sys(pi / 2))
-
 # Initialize the volume values (cuboid of value 1.0)
 sample_fvals = np.zeros(sample_shape)
 sample_fvals[25:75, 17:57, 20:30] = 1.0  # thicknesses 50, 40, 10
 sample_func = gf.Gfunc(fvals=sample_fvals, spacing=sample_voxel_size)
 
 # Show central slices
-#sample_func[50, :, :].display()
-#sample_func[:, 37, :].display()
-#sample_func[:, :, 25].display()
+sample_func[50, :, :].display(saveto='test/temp/orig_x.png')
+sample_func[:, 37, :].display(saveto='test/temp/orig_y.png')
+sample_func[:, :, 25].display(saveto='test/temp/orig_z.png')
 
 # Create forward and backward projectors
 forward_projector = xray.xray_ct_parallel_3d_projector(xray_geometry)
@@ -79,12 +76,29 @@ bp_func = backprojector(proj_func)
 
 print(bp_func.shape)
 
-bp_func[50, :, :].display()
-bp_func[:, 37, :].display()
-bp_func[:, :, 25].display()
+#bp_func[50, :, :].display()
+#bp_func[:, 37, :].display()
+#bp_func[:, :, 25].display()
 
-# TODO:
-# - wrap ASTRA forward and backward projections into a Projector class
-#   + use parallel beam 3D CUDA forward projection
-#
-# - define the Landweber algorithm in terms of operators
+
+def landweber(fwd_proj, backproj, data, init_guess, niter, relax=0.5):
+
+    cur_guess = init_guess.copy()
+    for i in range(niter):
+        residual = fwd_proj(cur_guess) - data
+        residual_bp = backproj(residual)
+        cur_guess = cur_guess - relax * residual_bp
+
+    return cur_guess
+
+# Start Landweber method with start value 0
+init_guess = gf.Gfunc(fvals=0., shape=sample_shape, spacing=sample_voxel_size)
+
+landw_sol = landweber(forward_projector, backprojector, proj_func,
+                      init_guess, niter=1, relax=0.5)
+
+landw_sol[50, :, :].display(saveto='test/temp/landw_3_x.png')
+landw_sol[:, 37, :].display(saveto='test/temp/landw_3_y.png')
+landw_sol[:, :, 25].display(saveto='test/temp/landw_3_z.png')
+
+plt.show(block=True)
