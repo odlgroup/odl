e10f9416bf60525f7f7c42f66cb713bb76d2fac4
Holger Kohr <kohr@kth.se>
2015-04-30 12:17:53 +0200

    Copyright notice as comment; fixed indentation

--- RL/utility/utility.py
+++ RL/utility/utility.py
@@ -1,24 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-utility.py -- utility functions
+# Copyright 2014, 2015 Holger Kohr, Jonas Adler
+#
+# This file is part of RL.
+#
+# RL is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RL is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RL.  If not, see <http://www.gnu.org/licenses/>.
 
-Copyright 2014, 2015 Holger Kohr
-
-This file is part of RL.
-
-RL is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-RL is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with RL.  If not, see <http://www.gnu.org/licenses/>.
-"""
 
 from __future__ import division, unicode_literals, print_function
 from __future__ import absolute_import
@@ -253,12 +249,12 @@ def to_local_sys(vec_in_lab_coords, local_sys):
 
 
 def allEqual(iterator, comp):
-      try:
-         iterator = iter(iterator)
-         first = next(iterator)
-         return all(comp(first,rest) for rest in iterator)
-      except StopIteration: #True for empty list
-         return True
+    try:
+        iterator = iter(iterator)
+        first = next(iterator)
+        return all(comp(first, rest) for rest in iterator)
+    except StopIteration:  # True for empty list
+        return True
 
 if __name__ == '__main__':
     import doctest
