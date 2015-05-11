76ed6d804a9f073780619248ee03e066033eb666
Holger Kohr <kohr@kth.se>
2015-05-11 10:30:38 +0200

    Re-added magic __contains__ method

--- RL/space/set.py
+++ RL/space/set.py
@@ -58,6 +58,9 @@ class Set(with_metaclass(ABCMeta, object)):
     def __ne__(self, other):
         return not self.equals(other)
 
+    def __contains__(self, other):
+        return self.contains(other)
+
 
 class EmptySet(Set):
     """ The empty set has no members (None is considered "no element")
