6ba50e7cc70cf1d1fba6b1d1dff373e24bd90a45
Jonas <jonasadl@kth.se>
2015-05-13 15:23:32 +0200

    Example implementation of issue-35

---
+++ examples/simple_operator.py
@@ -0,0 +1,29 @@
+from __future__ import unicode_literals, print_function, division
+from __future__ import absolute_import
+from future.builtins import object, zip
+from future import standard_library
+standard_library.install_aliases()
+
+from RL.space.euclidean import RN
+from RL.operator.operator import Operator
+
+""" An example of a very simple operator on RN
+"""
+
+class addOp(Operator):
+    def __init__(self, n, x):
+        self.n = n
+        self.x = x
+        self.range = RN(n)
+        self.domain = RN(n)
+
+    def applyImpl(self, rhs, out):
+        out.values[:] = rhs.values[:] + self.x
+
+n = 3
+rn = RN(n)
+x = rn.makeVector([1, 2, 3])
+
+op = addOp(n, 10)
+
+print(op(x))
