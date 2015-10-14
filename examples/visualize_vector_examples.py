27b10af473c67aeabd26a1783eecbbd5cdb4a7dd
Jonas Adler <jonasadl@kth.se>
2015-10-14 14:37:08 +0200

    fixed visualize vector examples to use odl.diagnostics

--- examples/visualize_vector_examples.py
+++ examples/visualize_vector_examples.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 spc = odl.L2(odl.Interval(0,1))
 disc = odl.l2_uniform_discretization(spc, 100)
 
-for name, vec in odl.test.vector_examples(disc):
+for name, vec in odl.diagnostics.vector_examples(disc):
     vec.show(title=name)
     
 spc = odl.L2(odl.Rectangle([0,0],[1,1]))
 disc = odl.l2_uniform_discretization(spc, [100, 100])
 
-for name, vec in odl.test.vector_examples(disc):
+for name, vec in odl.diagnostics.vector_examples(disc):
     vec.show(title=name)
 
 plt.show()
