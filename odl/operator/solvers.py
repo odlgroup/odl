44f25b28925cc177dcec28b8c64967ed16b82310
aringh <aringh@kth.se>
2015-10-28 16:27:25 +0100

    Changed display of iteration number in steepest decent to be optional

--- odl/operator/solvers.py
+++ odl/operator/solvers.py
@@ -278,14 +278,15 @@ def quasi_newton(op, x, line_search, niter=1, partial=None):
         if partial is not None:
             partial.send(x)
 
-def steepest_decent(deriv_op, x, line_search, niter=1):
+def steepest_decent(deriv_op, x, line_search, niter=1, print_iter_num=None):
     """ General implementation of steepest decent for solving min f(x)
     for x in C, where we define f(x) = infty if x is not in C. Needs to
     be done in the line search.
     """
     
     for _ in range(niter):
-        print('Iteration', _)
+        if print_iter_num is not None:
+            print('Iteration', _)
         grad = deriv_op(x)
         x -= line_search(x, -grad, grad)*grad
 
