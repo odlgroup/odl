49337700a421e6250e96ac50244a69e71dfdd743
Jonas Adler <jonasadl@kth.se>
2015-11-11 17:58:01 +0100

    added sphinxext in conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -30,6 +30,8 @@ import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+sys.path.insert(0, os.path.abspath('../sphinxext'))
+
 extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.autodoc',
