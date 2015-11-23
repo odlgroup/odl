56b520798d7c7fd31b1d1a66a9c5ec985ec71920
aringh <aringh@kth.se>
2015-11-23 15:24:10 +0100

    TST: Change test to call correct function.
    In agreement with previous commit (which renamed things). See #49.

--- test/discr/operator_utilities_test.py
+++ test/discr/operator_utilities_test.py
@@ -28,7 +28,7 @@ import numpy as np
 
 # ODL imports
 import odl
-from odl.discr.utility import matrix_representation
+from odl.discr.operator_utilities import matrix_representation
 from odl.util.testutils import almost_equal
 
 
