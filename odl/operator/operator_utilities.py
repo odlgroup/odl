d41acdec23c70b8028a1e34a55adc5540196f893
aringh <aringh@kth.se>
2015-11-25 17:24:16 +0100

    ENH: Update so that matrix representation can handle product spaces.
    This version of the matrix representation function can handle operators on product spaces, both in range and domain. See #49

--- odl/operator/operator_utilities.py
+++ odl/operator/operator_utilities.py
@@ -29,6 +29,7 @@ import numpy as np
 
 # Internal
 from odl.space.base_ntuples import FnBase
+from odl.set.pspace import ProductSpace
 
 
 def matrix_representation(op):
@@ -56,25 +57,58 @@ def matrix_representation(op):
               'representation of it.')
         return
 
-    if not isinstance(op.domain, FnBase):
+    if not (isinstance(op.domain, FnBase) or
+            isinstance(op.domain, ProductSpace)):
         print('WARNING: The operator domain is not discrete or produc space;',
               'cannot produce matrix representation of it.')
         return
 
-    if not isinstance(op.range, FnBase):
+    if not (isinstance(op.range, FnBase) or
+            isinstance(op.range, ProductSpace)):
         print('WARNING: The operator range is not discrete; cannot produce',
               'matrix representation of it.')
-        return
 
-    n = op.range.size
-    m = op.domain.size
+    # Get the size of the range, and handle ProductSpace
+    op_ran = op.range
+    op_ran_is_prod_space = isinstance(op_ran, ProductSpace)
+    if op_ran_is_prod_space:
+        num_ran = op_ran.size
+        n = [op_ran[i].size for i in range(num_ran)]
+    else:
+        num_ran = 1
+        n = [op_ran.size]
 
-    matrix = np.zeros([n, m])
-    v = op.domain.element()
-    tmp = op.range.element()
-    for i in range(m):
-        v.set_zero()
-        v[i] = 1.0
-        matrix[:, i] = op(v, out=tmp)
+    # Get the size of the domain, and handle ProductSpace
+    op_dom = op.domain
+    op_dom_is_prod_space = isinstance(op_dom, ProductSpace)
+    if op_dom_is_prod_space:
+        num_dom = op_dom.size
+        m = [op_dom[i].size for i in range(num_dom)]
+    else:
+        num_dom = 1
+        m = [op_dom.size]
+
+    # Generate the matrix
+    matrix = np.zeros([np.sum(n), np.sum(m)])
+    tmp_1 = op_ran.element()
+    v = op_dom.element()
+    index = 0
+
+    for i in range(num_dom):
+        for j in range(m[i]):
+            v.set_zero()
+            if op_dom_is_prod_space:
+                v[i][j] = 1.0
+            else:
+                v[j] = 1.0
+            tmp_2 = op(v, out=tmp_1)
+            tmp_3 = []
+            if op_ran_is_prod_space:
+                for k in range(num_ran):
+                    tmp_3 = np.concatenate((tmp_3, tmp_2[k].asarray()))
+            else:
+                tmp_3 = tmp_2.asarray()
+            matrix[:, index] = tmp_3
+            index += 1
 
     return matrix
