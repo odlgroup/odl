9558ab2cfe0cedcdba5874708751d5f483db7694
Jonas Adler <jonasadl@kth.se>
2015-12-08 20:48:23 +0100

    DOC: recursively find sub-folders in generate_doc

--- doc/source/generate_doc.py
+++ doc/source/generate_doc.py
@@ -44,11 +44,26 @@ string = """{shortname}
 {fun_string}
 """
 
+def import_submodules(package, recursive=True):
+    """ Import all submodules of a module, recursively, including subpackages
 
-def make_interface():
+    :param package: package (name or actual module)
+    :type package: str | module
+    :rtype: dict[str, types.ModuleType]
+    """
+    if isinstance(package, str):
+        package = importlib.import_module(package)
+    results = {}
+    for loader, name, is_pkg in pkgutil.walk_packages(package.__path__):
+        full_name = package.__name__ + '.' + name
+        results[full_name] = importlib.import_module(full_name)
+        if recursive and is_pkg:
+            results.update(import_submodules(full_name))
+    return results
+    
 
-    modnames = [modname for _, modname, _ in pkgutil.walk_packages(
-        path=odl.__path__, prefix=odl.__name__ + '.', onerror=lambda x: None)]
+def make_interface():        
+    modnames = [modname for modname in import_submodules(odl)]
 
     modnames += ['odl']
 
