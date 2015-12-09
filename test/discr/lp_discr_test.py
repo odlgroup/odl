8ec4a88a5ff5a871121429cbb7e5da87d1a56fc4
Jonas Adler <jonasadl@kth.se>
2015-12-09 17:06:09 +0100

    ENH: add cuda test for reductions

--- test/discr/lp_discr_test.py
+++ test/discr/lp_discr_test.py
@@ -614,19 +614,23 @@ def _impl_test_ufuncs(fn, name, n_args, n_out):
 
 def test_ufuncs():
     # Cannot use fixture due to bug in pytest
-    fn = odl.uniform_discr([0, 0], [1, 1], [2, 2])
+    spaces = [odl.uniform_discr([0, 0], [1, 1], [2, 2])]
 
-    for name, n_args, n_out, _ in odl.util.ufuncs.UFUNCS:
-        if (np.issubsctype(fn.dtype, np.floating) and
-                name in ['bitwise_and',
-                         'bitwise_or',
-                         'bitwise_xor',
-                         'invert',
-                         'left_shift',
-                         'right_shift']):
-            # Skip integer only methods if floating point type
-            continue
-        yield _impl_test_ufuncs, fn, name, n_args, n_out
+    if odl.CUDA_AVAILABLE:
+        spaces += [odl.uniform_discr([0, 0], [1, 1], [2, 2], impl='cuda')]
+
+    for fn in spaces:
+        for name, n_args, n_out, _ in odl.util.ufuncs.UFUNCS:
+            if (np.issubsctype(fn.dtype, np.floating) and
+                    name in ['bitwise_and',
+                             'bitwise_or',
+                             'bitwise_xor',
+                             'invert',
+                             'left_shift',
+                             'right_shift']):
+                # Skip integer only methods if floating point type
+                continue
+            yield _impl_test_ufuncs, fn, name, n_args, n_out
 
 
 def _impl_test_reduction(fn, name):
@@ -639,10 +643,14 @@ def _impl_test_reduction(fn, name):
 
 
 def test_reductions():
-    fn = odl.uniform_discr([0, 0], [1, 1], [2, 2])
+    spaces = [odl.uniform_discr([0, 0], [1, 1], [2, 2])]
 
-    for name, _ in odl.util.ufuncs.REDUCTIONS:
-        yield _impl_test_reduction, fn, name
+    if odl.CUDA_AVAILABLE:
+        spaces += [odl.uniform_discr([0, 0], [1, 1], [2, 2], impl='cuda')]
+
+    for fn in spaces:
+        for name, _ in odl.util.ufuncs.REDUCTIONS:
+            yield _impl_test_reduction, fn, name
 
 
 if __name__ == '__main__':
