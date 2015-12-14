abdbe7c6bc4dc93aafe4d5c6a2e6dd8cbf8f3d10
Kati Niinimäki <niin@kth.se>
2015-12-14 13:24:53 +0100

    TST: Add tests for 1D wavelet transforms and helper functions.

--- test/trafos/wavelet_test.py
+++ test/trafos/wavelet_test.py
@@ -31,7 +31,8 @@ if PYWAVELETS_AVAILABLE:
 # ODL imports
 import odl
 from odl.trafos.wavelet import (
-    coeff_size_list, pywt_coeff_to_array2d, array_to_pywt_coeff2d,
+    coeff_size_list, pywt_coeff_to_array1d, array_to_pywt_coeff1d,
+    pywt_coeff_to_array2d, array_to_pywt_coeff2d,
     pywt_coeff_to_array3d, array_to_pywt_coeff3d, wavelet_decomposition3d,
     wavelet_reconstruction3d, DiscreteWaveletTransform)
 from odl.util.testutils import (all_almost_equal, all_equal,
@@ -41,16 +42,20 @@ from odl.util.testutils import (all_almost_equal, all_equal,
 @skip_if_no_pywavelets
 def test_coeff_size_list():
     # Verify that the helper function does indeed work as expected
-    shape = (64, 64)
+    shape = (16,)
     nscale = 3
-    wbasis = pywt.Wavelet('db2')
+    wbasis = pywt.Wavelet('db1')
     mode = 'per'
-    size_list = coeff_size_list(shape, nscale, wbasis, mode)
-    S2d = [(8, 8), (8, 8), (16, 16), (32, 32), (64, 64)]
-    shape = (64, 64, 64)
+    size_list1d = coeff_size_list(shape, nscale, wbasis, mode)
+    S1d = [(2,), (2,), (4,), (8,), (16,)]
+    shape = (16, 16)
+    size_list2d = coeff_size_list(shape, nscale, wbasis, mode)
+    S2d = [(2, 2), (2, 2), (4, 4), (8, 8), (16, 16)]
+    shape = (16, 16, 16)
     size_list3d = coeff_size_list(shape, nscale, wbasis, mode)
-    S3d = [(8, 8, 8), (8, 8, 8), (16, 16, 16), (32, 32, 32), (64, 64, 64)]
-    assert all_equal(size_list, S2d)
+    S3d = [(2, 2, 2), (2, 2, 2), (4, 4, 4), (8, 8, 8), (16, 16, 16)]
+    assert all_equal(size_list1d, S1d)
+    assert all_equal(size_list2d, S2d)
     assert all_equal(size_list3d, S3d)
 
 
@@ -87,6 +92,28 @@ def test_wavelet_decomposition3d_and_reconstruction3d():
 
 
 @skip_if_no_pywavelets
+def test_pywt_coeff_to_array_and_array_to_pywt_coeff1d():
+    # Verify that the helper function does indeed work as expected
+    wbasis = pywt.Wavelet('db1')
+    mode = 'zpd'
+    nscales = 2
+    n = 16
+    size_list = coeff_size_list((n,), nscales, wbasis, mode)
+    x = np.random.rand(n)
+    coeff_list = pywt.wavedec(x, wbasis, mode, nscales)
+    coeff_arr = pywt_coeff_to_array1d(coeff_list, size_list)
+    assert isinstance(coeff_arr, (np.ndarray))
+    length_of_array = np.prod(size_list[0])
+    length_of_array += sum(np.prod(shape) for shape in size_list[1:-1])
+    assert all_equal(len(coeff_arr), length_of_array)
+
+    coeff_list2 = array_to_pywt_coeff1d(coeff_arr, size_list)
+    assert all_equal(coeff_list, coeff_list2)
+    reconstruction = pywt.waverec(coeff_list2, wbasis, mode)
+    assert all_almost_equal(reconstruction, x)
+
+
+@skip_if_no_pywavelets
 def test_pywt_coeff_to_array_and_array_to_pywt_coeff2d():
     # Verify that the helper function does indeed work as expected
     wbasis = pywt.Wavelet('db1')
@@ -118,13 +145,13 @@ def test_pywt_coeff_to_array_and_array_to_pywt_coeff3d():
     size_list = coeff_size_list((n, n, n), nscales, wbasis, mode)
     x = np.random.rand(n, n, n)
     coeff_dict = wavelet_decomposition3d(x, wbasis, mode, nscales)
-    coeff_arr = pywt_coeff_to_array3d(coeff_dict, size_list, nscales)
+    coeff_arr = pywt_coeff_to_array3d(coeff_dict, size_list)
     assert isinstance(coeff_arr, (np.ndarray))
     length_of_array = np.prod(size_list[0])
     length_of_array += sum(7 * np.prod(shape) for shape in size_list[1:-1])
     assert len(coeff_arr) == length_of_array
 
-    coeff_dict2 = array_to_pywt_coeff3d(coeff_arr, size_list, nscales)
+    coeff_dict2 = array_to_pywt_coeff3d(coeff_arr, size_list)
     reconstruction = wavelet_reconstruction3d(coeff_dict2, wbasis, mode,
                                               nscales)
     assert all_equal(coeff_dict, coeff_dict)
@@ -134,6 +161,50 @@ def test_pywt_coeff_to_array_and_array_to_pywt_coeff3d():
 @skip_if_no_pywavelets
 def test_dwt():
     # Verify that the operator works as axpected
+    # 1D test
+    n = 16
+    x = np.zeros(n)
+    x[5:10] = 1
+    wbasis = pywt.Wavelet('db1')
+    nscales = 2
+    mode = 'sym'
+    size_list = coeff_size_list((n,), nscales, wbasis, mode)
+
+    # Define a discretized domain
+    domain = odl.FunctionSpace(odl.Interval([-1], [1]))
+    nPoints = np.array([n])
+    disc_domain = odl.uniform_discr_fromspace(domain, nPoints)
+    disc_phantom = disc_domain.element(x)
+
+    # Create the discrete wavelet transform operator.
+    # Only the domain of the operator needs to be defined
+    Wop = DiscreteWaveletTransform(disc_domain, nscales, wbasis, mode)
+
+    # Compute the discrete wavelet transform of discrete imput image
+    coeffs = Wop(disc_phantom)
+
+    # Determine the correct range for Wop and verify that coeffs
+    # is an element of it
+    ran_size = np.prod(size_list[0])
+    ran_size += sum(np.prod(shape) for shape in size_list[1:-1])
+    disc_range = disc_domain.dspace_type(ran_size, dtype=disc_domain.dtype)
+    assert coeffs in disc_range
+
+    # Compute the inverse wavelet transform
+    reconstruction1 = Wop.inverse(coeffs)
+    # With othogonal wavelets the inverse is the adjoint
+    reconstruction2 = Wop.adjoint(coeffs)
+    # Verify that the output of Wop.inverse and Wop.adjoint are the same
+    assert all_almost_equal(reconstruction1.asarray(),
+                            reconstruction2.asarray())
+
+    # Verify that reconstructions lie in correct discretized domain
+    assert reconstruction1 in disc_domain
+    assert reconstruction2 in disc_domain
+    assert all_almost_equal(reconstruction1.asarray(), x)
+    assert all_almost_equal(reconstruction2.asarray(), x)
+
+    # ---------------------------------------------------------------
     # 2D test
     n = 16
     x = np.zeros((n, n))
@@ -177,6 +248,7 @@ def test_dwt():
     assert all_almost_equal(reconstruction1.asarray(), x)
     assert all_almost_equal(reconstruction2.asarray(), x)
 
+    # -------------------------------------------------------------
     # 3D test
     n = 16
     x = np.zeros((n, n, n))
