ENH: fix rotation to work properly when using fanbeam

odl/tomo/backends/astra_cpu.py
@@ -96,11 +96,8 @@ def astra_cpu_forward_projector_call(vol_data, geometry, proj_space, out=None):
     vol_geom = astra_volume_geometry(vol_data.space)
     proj_geom = astra_projection_geometry(geometry)
 
-    if not proj_geom['type'] == 'fanflat':
-        raise ValueError('type {} is not fanflat'.format(proj_geom['type']))
-
     # for fanflat geometry an angle offset is needed
-    if isinstance(geometry, FanFlatGeometry):
+    if proj_geom['type'] == 'fanflat':
         proj_geom['ProjectionAngles'] += np.pi
 
     # Create ASTRA data structures