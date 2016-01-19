1b9e2580b755173ed04d1c3334d342ee0a3ac2b3
Jonas Adler <jonasadl@kth.se>
2016-01-19 11:24:27 +0100

    ENH: remove ignore of largescale tests

--- conftest.py
+++ conftest.py
@@ -21,8 +21,6 @@ from __future__ import print_function, division, absolute_import
 from future import standard_library
 standard_library.install_aliases()
 
-import py.test
-
 from odl.space.cu_ntuples import CUDA_AVAILABLE
 from odl.trafos.wavelet import PYWAVELETS_AVAILABLE
 
@@ -32,6 +30,7 @@ if not CUDA_AVAILABLE:
     collect_ignore.append('odl/space/cu_ntuples.py')
 if not PYWAVELETS_AVAILABLE:
     collect_ignore.append('odl/trafos/wavelet.py')
+
 
 def pytest_addoption(parser):
     parser.addoption('--largescale', action='store_true',
