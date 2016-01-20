a972c611137f0ca9ad9acc334e05371de5238c95
Julian Moosmann <moosmann@kth.se>
2016-01-20 09:23:47 +0100

    MAINT: refactor, update doc

--- odl/solvers/advanced/chambolle_pock.py
+++ odl/solvers/advanced/chambolle_pock.py
@@ -15,27 +15,33 @@
 # You should have received a copy of the GNU General Public License
 # along with ODL.  If not, see <http://www.gnu.org/licenses/>.
 
-"""(Quasi-)Newton schemes to find zeros of functions (gradients)."""
+"""First-order primal-dual algorithm developed by Chambolle and Pock."""
 
 # Imports for common Python 2/3 codebase
 from __future__ import print_function, division, absolute_import
 from future import standard_library
 standard_library.install_aliases()
 from builtins import super
-import numpy as np
 
 # External
 
 # Internal
 from odl.operator.operator import Operator
 
+__all__ = ('chambolle_pock_solver', 'f_dual_prox_l2_tv', 'g_prox_none')
 
-def chambolle_pock(K, f_dual_prox, g_prox, niter=1, partial=None):
-    """
+
+def chambolle_pock_solver(K, f_dual_prox, g_prox, tau, sigma, theta=1,
+                          niter=100, partial=None):
+    """Chambolle-Pock aglgorithms for convex optimization problems.
+
+    First order primal-dual hybrid-gradient (PDHG) method for non-smooth
+    convex optimization probelms with known saddle-point structure developed
+    by `Chambolle and Pock`_ (CP).
+
 
     Parameters
     ----------
-
     niter : `int`, optional
         Number of iterations
     partial : `Partial`, optional
@@ -43,17 +49,20 @@ def chambolle_pock(K, f_dual_prox, g_prox, niter=1, partial=None):
 
     Returns
     -------
-    `None`
+    x : `DiscreteLpVector`
 
     References
     ----------
+    Original paper by `Chambolle and Pock`_. Also see `diagonal
+    preconditioning`_ techniques. This implementation is based on the
+    article on convex optimization problem prototyping for image
+    reconstruction in computed tomography by `Sidky et al.`_.
 
+    .. _Chambolle and Pock: http://dx.doi.org/10.1007/s10851-010-0251-1/
+    .. _diagonal preconditioning: http://dx.doi.org/10.1109/ICCV.2011.6126441
+    .. _Sidky et al.: http://stacks.iop.org/0031-9155/57/i=10/a=3065
     """
-    sigma = 1
-    tau = 1
-    theta = 1
-
-    x = K.domain.zero()
+    x = K.domain.one()
     xbar = x.copy()
     y = K.range.zero()
 
@@ -65,64 +74,170 @@ def chambolle_pock(K, f_dual_prox, g_prox, niter=1, partial=None):
 
         xold = x.copy()
         f_dual_prox_sigma(y + sigma * K(xbar), out = y)
-        x = g_prox_tau(x - tau * Kadjoint(y))
+        g_prox_tau(x - tau * Kadjoint(y), out=x)
         xbar = x + theta * (x - xold)
 
-
         # TODO: decide on what to send
-        # if partial is not None:
-        #     partial.send(x)
+        if partial is not None:
+            partial.send((x[0], _))
 
     return x
 
 
-def f_dual_prox_l2_tv(K, g, lam):
+def f_dual_prox_l2_tv(space, g, lam):
+    """Function for the proximal operator with l2-data plus TV-regularization.
+
+    Factory function which provides a function to initialize the proximal
+    operator of the convex conjugate of the functional ``F`` given by the
+    L2-data term and the isotropic total variation semi-norm as in the primal
+    minimization problem:
+
+    F(y,z)= 1/2 * ||y - g||_2^2 + lambda * ||(|grad u|)||_1
+
+    with y = Au and z = grad u. The operators ``A`` and ``grad`` are
+    combined to a matrix operator ``K`` as:
+
+        K = (A, grad)^T
+
+    The domain and range of the proximal operator are given by the range of
+    ``K``.
+
+    Parameters
+    ----------
+    space : `ProductSpace`
+       Product space of the range of forward operator ``A`` and of the range
+       of the gradient operator i.e. a product space of the image space
+    g : `DiscreteLpVector`
+        Element in the range of the forward operator ``A``
+    lam : positive `float`
+        Regularization parameter
+
+    Returns
+    -------
+    make_prox : `function`
+        Function initialize the proximal operator with a given step
+        length parameter ``sigma``
+    """
 
     def make_prox(sigma):
+        """Returns an instance of the proximal operator.
+
+        Parameters
+        ----------
+        sigma : positive `float'
+            Step length parameter
+
+        Returns
+        -------
+        prox_op : `Operator`
+            Proximal operator initialized with ``sigma``
+        """
 
         class _prox_op(Operator):
 
+            """The proximal operator."""
+
             def __init__(self, sigma):
+                """Initialize the proximal operator.
+
+                Parameters
+                ----------
+                sigma
+
+                Returns
+                -------
+
+                """
                 self.sigma = sigma
-                super().__init__(K.domain, K.range)
+                super().__init__(domain=space, range=space)
 
             def _call(self, x, out):
-
+                """Apply the operator to ``x`` and stores the result in
+                ``out``"""
 
                 y = x[0]
                 z = x[1]
 
-                out[0] = (y - self.sigma * g) / (1 + self.sigma)
-                out[1] = lam * z /
-                return out
+                # First component: (y - sig*g) / (1 + sig)
 
+                sig = self.sigma
+                out[0][...] = y / sig
+                out[0] -= g
+                out[0] *= sig / (1 + sig)
+
+                # Second component: lam * z / (max(lam, |z|))
+
+                # Calculate |z| = pointwise 2-norm of z
+                tmp = z[0].space.zero()
+                for zi in z:
+                    tmp += zi ** 2
+                tmp.ufunc.sqrt(out=tmp)
+
+                # Pointwise maximum of |z| and lambda
+                tmp.ufunc.maximum(lam, out=tmp)
+                tmp /= lam
+
+                out[1] = z.copy()
+                for zi in out[1]:
+                    zi /= tmp
 
         return _prox_op(sigma)
 
     return make_prox
 
 
-import odl
-from odl import IdentityOperator
+def g_prox_none(space):
+    """Function to create the proximal operator of the constraining functional.
 
-g_space = odl.uniform_discr(0, 10, 10)
-g1_space = odl.uniform_discr(0, 10, 10)
-g2_space = odl.uniform_discr(0, 10, 10)
+    This factory function which provides a function to initialize the
+    proximal operator for the functional which accounts for constraints on
+    the image ``x``.
 
-prod1 = odl.ProductSpace(g1_space, g2_space)
-prod2 = odl.ProductSpace(g_space, prod1)
-print(prod2)
-print(prod2.zero())
 
-K = IdentityOperator(g_space)
-g = g_space.one()
+    Parameters
+    ----------
+    space : `DiscreteLp`
+        Image domain
 
-tmp = f_dual_prox_l2_tv(K, g)
-t = tmp(1)
-print(type(tmp))
-print(type(tmp(1)))
-print(type(t))
+    Returns
+    -------
+    make_prox : `function`
+        Function to initialize the proximal operator
+    """
 
-out = t(g)
-print(type(out))
+    def make_prox(tau):
+        """Returns an instance of the proximal operator.
 
+        Parameters
+        ----------
+        tau : positive `float'
+            Step length parameter
+
+        Returns
+        -------
+        prox_op : `Operator`
+            Proximal operator initialized with ``sigma``
+
+        """
+
+        class _prox_op(Operator):
+
+            """The proximal operator. """
+
+            def __init__(self, tau):
+                self.tau = tau
+                super().__init__(domain=space, range=space)
+
+            def _call(self, x, out):
+                """TODO"""
+
+                out.assign(x)
+
+        return _prox_op(tau)
+
+    return make_prox
+
+
+if __name__ == '__main__':
+    from doctest import testmod, NORMALIZE_WHITESPACE
+    testmod(optionflags=NORMALIZE_WHITESPACE)
