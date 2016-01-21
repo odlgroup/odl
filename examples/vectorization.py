d55d060b73aab5bb0eb3b7d9055774ee11064f5d
Holger Kohr <kohr@kth.se>
2016-01-21 11:12:02 +0100

    ENH: make vectorization example more comprehensible

--- examples/vectorization.py
+++ examples/vectorization.py
@@ -23,21 +23,24 @@ from future import standard_library
 standard_library.install_aliases()
 import numpy as np
 import odl
-from odl.util.vectorization import vectorize
 import timeit
 
 
 def performace_example():
-    # Create a function space
-    X = odl.FunctionSpace(odl.Interval(0, 1))
+    # Create a space of functions on the interval [0, 1].
+    fspace = odl.FunctionSpace(odl.Interval(0, 1))
 
-    # Functions default to vectorized
-    f_vec = X.element(lambda x: x ** 2)
+    # Simple function, already supports vectorization.
+    f_vec = fspace.element(lambda x: x ** 2)
 
-    # If 'vectorized=False' is used, odl automatically vectorizes
-    f_novec = X.element(lambda x: x ** 2, vectorized=False)
+    # If 'vectorized=False' is used, odl automatically vectorizes with
+    # the help of numpy.vectorize. This will be very slow, though, since
+    # the implementation is basically a Python loop.
+    f_novec = fspace.element(lambda x: x ** 2, vectorized=False)
 
-    # Example of runtime, expect vectorized to be much faster
+    # We test both versions with 10000 evaluation points. The natively
+    # vectorized version should be much faster than the one using
+    # numpy.vectorize.
     points = np.linspace(0, 1, 10000)
 
     print('Vectorized runtime:     {:5f}'
@@ -47,8 +50,9 @@ def performace_example():
 
 
 def numba_example():
-    # Some functions are not easily vectorized,
-    # here we can use numba to improve performance.
+    # Some functions are not easily vectorized, here we can use Numba to
+    # improve performance.
+    # See http://numba.pydata.org/
 
     try:
         import numba
@@ -70,39 +74,59 @@ def numba_example():
 
     def myfunc_vec(x):
         """Return x - y if x > y, otherwise return x + y."""
-        return vectorized(*x)
+        return vectorized(x[0], x[1])
 
     def myfunc_native_vec(x):
         """Return x - y if x > y, otherwise return x + y."""
+        # This implementation uses Numpy's fast built-in vectorization
+        # directly. The function np.where checks the condition in the
+        # first argument and takes the values from the second argument
+        # for all entries where the condition is `True`, otherwise
+        # the values from the third argument are taken. The arrays are
+        # automatically broadcast, i.e. the broadcast shape of the
+        # condition expression determines the output shape.
         return np.where(x[0] > x[1], x[0] - x[1], x[0] + x[1])
 
-    # Create functions
+    # Create (continuous) functions in the space of function defined
+    # on the rectangle [0, 1] x [0, 1].
     fspace = odl.FunctionSpace(odl.Rectangle([0, 0], [1, 1]))
     f_default = fspace.element(myfunc, vectorized=False)
     f_numba = fspace.element(myfunc_vec)
     f_native = fspace.element(myfunc_native_vec, vectorized=True)
 
-    # Create points
-    points = odl.uniform_sampling(fspace.domain, [2000, 2000]).points().T
-    mesh = odl.uniform_sampling(fspace.domain, [2000, 2000]).meshgrid()
+    # Create a unform grid in [0, 1] x [0, 1] (fspace.domain) with 2000
+    # samples per dimension.
+    grid = odl.uniform_sampling(fspace.domain, [2000, 2000])
+    # The points() method really creates all grid points (2000^2) and
+    # stores them one-by-one (row-wise) in a large array with shape
+    # (2000*2000, 2). Since the function expects points[i] to be the
+    # array of i-th components of all points, we need to transpose.
+    points = grid.points().T
+    # The meshgrid() method only returns a sparse representation of the
+    # grid, a tuple whose i-th entry is the vector of all possible i-th
+    # components in the grid (2000). Extra dimensions are added to the
+    # vector in order to support automatic broadcasting. This is both
+    # faster and more memory-friendly than creating the full point array.
+    # See the numpy.meshgrid function for more information.
+    mesh = grid.meshgrid()  # Creates a sparse meshgrid (2000 * 2)
 
-    print('Non-Vectorized runtime (points):     {:5f}'
+    print('Non-Vectorized runtime (points):      {:5f}'
           ''.format(timeit.timeit(lambda: f_default(points), number=1)))
-    print('Non-Vectorized runtime (meshgrid):     {:5f}'
+    print('Non-Vectorized runtime (meshgrid):    {:5f}'
           ''.format(timeit.timeit(lambda: f_default(mesh), number=1)))
-    print('Numba vectorized runtime (points): {:5f}'
+    print('Numba vectorized runtime (points):    {:5f}'
           ''.format(timeit.timeit(lambda: f_numba(points), number=1)))
-    print('Numba vectorized runtime (meshgrid): {:5f}'
+    print('Numba vectorized runtime (meshgrid):  {:5f}'
           ''.format(timeit.timeit(lambda: f_numba(mesh), number=1)))
-    print('Native vectorized runtime (points): {:5f}'
+    print('Native vectorized runtime (points):   {:5f}'
           ''.format(timeit.timeit(lambda: f_native(points), number=1)))
     print('Native vectorized runtime (meshgrid): {:5f}'
           ''.format(timeit.timeit(lambda: f_native(mesh), number=1)))
 
 
 if __name__ == '__main__':
-    print('Running performance example')
+    print('Running vectorization performance example.')
     performace_example()
 
-    print('Running numba example')
+    print('Running Numba example.')
     numba_example()
