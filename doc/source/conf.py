4d705799017a0f2a9f913804c60596f24cc38746
Jonas Adler <jonasadl@kth.se>
2016-01-26 16:08:00 +0100

    BUG: remove all numpy mocks for RTD

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -49,7 +49,6 @@ if on_rtd:
                   'past', 'past.builtins',
                   'scipy', 'scipy.linalg', 'scipy.sparse', 'scipy.sparse.base',
                   'scipy.interpolate', 'scipy.interpolate.interpnd',
-                  'numpy.linalg', 'numpy.distutils',
                   'matplotlib', 'matplotlib.pyplot',
                   'odlpp', 'odlpp.odlpp_cuda'])
 
