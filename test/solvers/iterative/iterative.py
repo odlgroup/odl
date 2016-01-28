5b9a867e4a706b7210acfca2385f6cb7d3da2ae4
Jonas Adler <jonasadl@kth.se>
2016-01-28 18:01:57 +0100

    ENH: add test for most common iterative solvers

---
+++ test/solvers/iterative/iterative.py
@@ -0,0 +1,84 @@
+# Copyright 2014, 2015 The ODL development group
+#
+# This file is part of ODL.
+#
+# ODL is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ODL is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ODL.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Test iterative solvers."""
+
+# Imports for common Python 2/3 codebase
+from __future__ import print_function, division, absolute_import
+from future import standard_library
+standard_library.install_aliases()
+
+import odl
+from odl.util.testutils import all_almost_equal
+import pytest
+import numpy as np
+
+
+# Find the valid projectors
+solvers = ['gradient_descent',
+           'landweber',
+           'conjugate_gradient',
+           'conjugate_gradient_normal']
+
+
+@pytest.fixture(scope="module",
+                params=solvers)
+def iterative_solver(request):
+    solver_name = request.param
+
+    if solver_name == 'gradient_descent':
+        def solver(A, x, rhs):
+            norm2 = A.adjoint(A(x)).norm() / x.norm()
+
+            # Define gradient as A* (Ax - b)
+            gradient_op = A.adjoint * odl.ResidualOperator(A, rhs)
+            odl.solvers.gradient_descent(gradient_op, x, niter=10,
+                                         omega=0.5/norm2)
+    elif solver_name == 'landweber':
+        def solver(A, x, rhs):
+            norm2 = A.adjoint(A(x)).norm() / x.norm()
+            odl.solvers.landweber(A, x, rhs, niter=10, omega=0.5/norm2)
+    elif solver_name == 'conjugate_gradient':
+        def solver(A, x, rhs):
+            odl.solvers.conjugate_gradient(A, x, rhs, niter=10)
+    elif solver_name == 'conjugate_gradient_normal':
+        def solver(A, x, rhs):
+            odl.solvers.conjugate_gradient_normal(A, x, rhs, niter=10)
+    else:
+        raise ValueError('solver not valid')
+
+    return solver
+
+
+def test_solver(iterative_solver):
+    """Test discrete X-ray transform using ASTRA for reconstruction."""
+
+    places = 2
+
+    space = odl.Rn(5)
+    rhs_arr = np.ones(5)
+    op_arr = np.eye(5) * 5 + np.ones([5, 5])
+
+    op = odl.MatVecOperator(op_arr, space, space)
+    x = op.domain.one()
+    rhs = op.range.element(rhs_arr)
+    iterative_solver(op, x, rhs)
+
+    assert all_almost_equal(op(x), rhs, places)
+
+if __name__ == '__main__':
+    pytest.main(str(__file__.replace('\\', '/') + ' -v'))
