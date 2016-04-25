6445a12a34c40c039c0c3e240a8524307fdfcac9
Jonas Adler <jonasadl@kth.se>
2016-04-25 16:50:56 +0200

    ENH: cleaned up FBP example

--- examples/tomo/filtered_backprojection_parallel_2d.py
+++ examples/tomo/filtered_backprojection_parallel_2d.py
@@ -15,52 +15,59 @@
 # You should have received a copy of the GNU General Public License
 # along with ODL.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Example using the ray transform with 2d parallel beam geometry."""
+"""
+Example using a filtered backprojection in 2d using the ray transform and a
+ramp filter. The ramp filter is implemented in fourier space.
 
-# Imports for common Python 2/3 codebase
-from __future__ import print_function, division, absolute_import
-from future import standard_library
-standard_library.install_aliases()
+See https://en.wikipedia.org/wiki/Radon_transform#Inversion_formulas for
+more information.
+"""
 
 import numpy as np
 import odl
 
 
+# --- Set-up of geometry of problem --- #
+
+
 # Discrete reconstruction space: discretized functions on the rectangle
 # [-20, 20]^2 with 300 samples per dimension.
 reco_space = odl.uniform_discr(
-    min_corner=[-20, -20], max_corner=[20, 20], nsamples=[200, 200],
+    min_corner=[-20, -20], max_corner=[20, 20], nsamples=[300, 300],
     dtype='float32')
 
+# Angles: uniformly spaced, n = 1000, min = 0, max = pi
+angle_partition = odl.uniform_partition(0, np.pi, 1000)
+
+# Detector: uniformly sampled, n = 500, min = -30, max = 30
+detector_partition = odl.uniform_partition(-30, 30, 500)
+
 # Make a parallel beam geometry with flat detector
-# Angles: uniformly spaced, n = 360, min = 0, max = 2 * pi
-angle_partition = odl.uniform_partition(0, 2 * np.pi, 360)
-# Detector: uniformly sampled, n = 558, min = -30, max = 30
-detector_partition = odl.uniform_partition(-30, 30, 2000)
 geometry = odl.tomo.Parallel2dGeometry(angle_partition, detector_partition)
+
+
+# --- Create FilteredBackProjection (FBP) operator --- #
+
 
 # ray transform aka forward projection. We use ASTRA CUDA backend.
 ray_trafo = odl.tomo.RayTransform(reco_space, geometry, impl='astra_cuda')
+
 # fourier transform in detector direction
 fourier = odl.trafos.FourierTransform(ray_trafo.range, axes=[1])
 
-# Create filter
-theta_max = detector_partition.extent() / 2.0
+# Create ramp in the detector direction
+ramp_function = fourier.range.element(lambda x: 0*x[0] + np.abs(x[1]))
 
-
-def window(x):
-    # Select cutoff value
-    cutoff = 0.7
-    nonzero = np.abs(x) < theta_max * cutoff
-    return nonzero * (1 + np.cos(np.abs(x) / (theta_max / cutoff)))
-
-
-ramp_function = fourier.range.element(lambda x: 0*x[0] + np.abs(x[1]) * window(x[1]))
+# Create ramp filter via the convolution formula with fourier transforms
 ramp_filter = fourier.inverse * ramp_function * fourier
 
+# Create filtered backprojection by composing the backprojection (adjoint) with
+# the ramp filter. Also apply a scaling.
+fbp = ray_trafo.adjoint * ramp_filter / (2 * np.pi)
 
-# Create filtered backprojection
-fbp = ray_trafo.adjoint * ramp_filter
+
+# --- Show some examples --- #
+
 
 # Create a discrete Shepp-Logan phantom (modified version)
 phantom = odl.util.phantom.shepp_logan(reco_space, modified=True)
@@ -68,15 +75,11 @@ phantom = odl.util.phantom.shepp_logan(reco_space, modified=True)
 # Create projection data by calling the ray transform on the phantom
 proj_data = ray_trafo(phantom)
 
-# Back-projection can be done by simply calling the adjoint operator on the
-# projection data (or any element in the projection space).
-backproj = ray_trafo.adjoint(proj_data)
-
 # Calculate filtered backprojection of data
 fbp_reconstruction = fbp(proj_data)
 
 # Shows a slice of the phantom, projections, and reconstruction
 phantom.show(title='Phantom')
 proj_data.show(title='Projection data (sinogram)')
-backproj.show(title='Back-projected data')
 fbp_reconstruction.show(title='Filtered backprojection')
+(phantom - fbp_reconstruction).show(title='Error')
