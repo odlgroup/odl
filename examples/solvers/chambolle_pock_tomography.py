e87b2d8cab40f3fe241f49cbb4df5ade6fcfe6e4
Jonas Adler <jonasadl@kth.se>
2016-04-26 11:07:46 +0200

    ENH: improve style of cp tomo example

--- examples/solvers/chambolle_pock_tomography.py
+++ examples/solvers/chambolle_pock_tomography.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with ODL.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Total variation base image deconvolution using the Chambolle-Pock solver.
+"""Total variation tomography using the Chambolle-Pock solver.
 
 Let X and Y be finite-dimensional Hilbert spaces and K a linear mapping from
 X to Y with induce norm ||K||. The (primal) minimization problem we want to
@@ -27,20 +27,19 @@ where the proper, convex, lower-semicontinuous functionals
 F : Y -> [0, +infinity] and G : X -> [0, +infinity] are given
 by an l2-data fitting term regularized by isotropic total variation
 
-    F(K x) = 1/2 ||conv(x) - g||_2^2 + lam || |grad(x)| ||_1
+    F(K x) = 1/2 ||A(x) - g||_2^2 + lam || |grad(x)| ||_1
 
 and
 
    G(x) = 0 ,
 
-respectively. Here, conv denotes the convolution operator, g the image to
-deconvolve, ||.||_2 the l2-norm, ||.||_1  the l1-semi-norm, grad the spatial
+respectively. Here, A denotes the ray transform operator, g the projection
+data, ||.||_2 the l2-norm, ||.||_1  the l1-semi-norm, grad the spatial
 gradient, lam the regularization parameter, |.| the point-wise magnitude
 across the vector components of grad(x), and K is a column vector of
-operators K = (conv, grad)^T.
+operators K = (A, grad)^T.
 
-First we define a convolution operator and generate an image to be
-deconvolved by convolving a Shepp-Logan phantom with a Gaussian kernel.
+First we define a ray transform operator and generate projections.
 
 In order to use the Chambolle-Pock solver, we have to create the column
 operator K, choose a starting point x, create the proximal operator for G,
@@ -52,13 +51,12 @@ For details see :ref:`chambolle_pock`, :ref:`proximal_operators`, and
 references therein.
 """
 
-# Imports for common Python 2/3 codebase
-from __future__ import print_function, division, absolute_import
-from future import standard_library
-standard_library.install_aliases()
-
 import numpy as np
 import odl
+
+
+# --- Set up the forward operator (ray transform) --- #
+
 
 # Discrete reconstruction space: discretized functions on the rectangle
 # [-20, 20]^2 with 300 samples per dimension.
@@ -74,38 +72,36 @@ detector_partition = odl.uniform_partition(-30, 30, 558)
 geometry = odl.tomo.Parallel2dGeometry(angle_partition, detector_partition)
 
 # The implementation of the ray transform to use, options:
-# 'astra_cpu', 'astra_cuda'.  Require astra tomography to be installed.
-# 'scikit'.                   Requires scikit-image (can be installed by
+# 'scikit'                    Requires scikit-image (can be installed by
 #                             running ``pip install scikit-image``).
+# 'astra_cpu', 'astra_cuda'   Require astra tomography to be installed.
+#                             Astra is much faster than scikit. Webpage:
+#                             https://github.com/astra-toolbox/astra-toolbox
 impl = 'scikit'
 
 # ray transform aka forward projection.
 ray_trafo = odl.tomo.RayTransform(reco_space, geometry, impl=impl)
 
-# Optional: Run diagnostics to assure the adjoint is properly implemented
-# odl.diagnostics.OperatorTest(conv_op).run_tests()
+
+# --- Generate artificial data --- #
+
 
 # Create phantom
 discr_phantom = odl.util.shepp_logan(reco_space, modified=True)
 
-# Create vector of convolved phantom
+# Create vector of forward projected phantom with noise
 data = ray_trafo(discr_phantom)
 data += odl.util.white_noise(ray_trafo.range) * np.mean(data) * 0.1
 
-# Set up the Chambolle-Pock solver:
+
+# --- Set up the inverse problem --- #
+
 
 # Initialize gradient operator
 gradient = odl.Gradient(reco_space, method='forward')
 
 # Column vector of two operators
 op = odl.BroadcastOperator(ray_trafo, gradient)
-
-# Choose a starting point
-x = op.domain.one()
-
-# Estimated operator norm, add 10 percent to ensure ||K||_2^2 * sigma * tau < 1
-op_norm = 1.1 * odl.operator.oputils.power_method_opnorm(op, 5)
-print('Norm of the product space operator: {}'.format(op_norm))
 
 # Create the proximal operator for unconstrained primal variable
 proximal_primal = odl.solvers.proximal_zero(op.domain)
@@ -124,19 +120,23 @@ prox_convconj_l1 = odl.solvers.proximal_convexconjugate_l1(
 proximal_dual = odl.solvers.combine_proximals(
     [prox_convconj_l2, prox_convconj_l1])
 
-# Number of iterations
-niter = 400
 
-# Step size for the proximal operator for the primal variable x
-tau = 1 / op_norm
+# --- Select solver parameters and solve using Chambolle-Pock --- #
 
-# Step size for the proximal operator for the dual variable y
-sigma = 1 / op_norm
+
+# Estimated operator norm, add 10 percent to ensure ||K||_2^2 * sigma * tau < 1
+op_norm = 1.1 * odl.operator.oputils.power_method_opnorm(op, 5)
+
+niter = 400  # Number of iterations
+tau = 1.0 / op_norm  # Step size for the primal variable
+sigma = 1.0 / op_norm  # Step size for the dual variable
 
 # Optionally pass partial to the solver to display intermediate results
 partial = (odl.solvers.util.PrintIterationPartial() &
-           odl.solvers.util.PrintTimingPartial() &
            odl.solvers.util.ShowPartial())
+
+# Choose a starting point
+x = op.domain.zero()
 
 # Run the algorithm
 odl.solvers.chambolle_pock_solver(
