0a4c8ea852d19be052ac99dae4abce98b9dda129
Jonas Adler <jonasadl@kth.se>
2016-05-21 23:10:15 +0200

    BUG: fix slow stir test missing skip

--- odl/util/testutils.py
+++ odl/util/testutils.py
@@ -32,8 +32,8 @@ from time import time
 
 
 __all__ = ('almost_equal', 'all_equal', 'all_almost_equal', 'never_skip',
-           'skip_if_no_cuda', 'skip_if_no_pywavelets', 'skip_if_no_pyfftw',
-           'skip_if_no_largescale',
+           'skip_if_no_cuda', 'skip_if_no_stir', 'skip_if_no_pywavelets',
+           'skip_if_no_pyfftw', 'skip_if_no_largescale',
            'Timer', 'timeit', 'ProgressBar', 'ProgressRange',
            'test', 'run_doctests')
 
@@ -177,11 +177,6 @@ def is_subdict(subdict, dictionary):
     return all(item in dictionary.items() for item in subdict.items())
 
 
-def _pass(function):
-    """Trivial decorator used if pytest marks are not available."""
-    return function
-
-
 try:
     # Try catch in case user does not have pytest
     import pytest
@@ -195,6 +190,11 @@ try:
     skip_if_no_cuda = pytest.mark.skipif(
         "not odl.CUDA_AVAILABLE",
         reason='CUDA not available'
+    )
+
+    skip_if_no_stir = pytest.mark.skipif(
+        "not odl.tomo.backends.stir_bindings.STIR_AVAILABLE",
+        reason='STIR not available'
     )
 
     skip_if_no_pywavelets = pytest.mark.skipif(
@@ -217,8 +217,13 @@ try:
     )
 
 except ImportError:
+    def _pass(function):
+        """Trivial decorator used if pytest marks are not available."""
+        return function
+
     never_skip = _pass
     skip_if_no_cuda = _pass
+    skip_if_no_stir = _pass
     skip_if_no_pywavelets = _pass
     skip_if_no_pyfftw = _pass
     skip_if_no_largescale = _pass
