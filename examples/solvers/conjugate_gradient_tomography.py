f7e504652707b09c0a0b7e7b1691094ef6d35509
Jonas Adler <jonasadl@kth.se>
2016-06-01 18:03:43 +0200

    ENH: add proper tomography example

---
+++ examples/solvers/conjugate_gradient_tomography.py
@@ -0,0 +1,85 @@
+# Copyright 2014-2016 The ODL development group
+#
+# This file is part of ODL.
+#
+# ODL is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ODL is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ODL.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Total variation tomography using the `conjugate_gradient_normal` solver.
+
+Solves the inverse problem
+
+    A(x) = g
+
+Where ``A`` is a parallel beam forward projector, ``x`` the result and
+ ``g`` is given noisy data.
+"""
+
+import numpy as np
+import odl
+
+
+# --- Set up the forward operator (ray transform) --- #
+
+
+# Discrete reconstruction space: discretized functions on the rectangle
+# [-20, 20]^2 with 300 samples per dimension.
+reco_space = odl.uniform_discr(
+    min_corner=[-20, -20], max_corner=[20, 20], nsamples=[300, 300],
+    dtype='float32')
+
+# Make a parallel beam geometry with flat detector
+# Angles: uniformly spaced, n = 360, min = 0, max = 2 * pi
+angle_partition = odl.uniform_partition(0, 2 * np.pi, 360)
+
+# Detector: uniformly sampled, n = 558, min = -30, max = 30
+detector_partition = odl.uniform_partition(-30, 30, 558)
+geometry = odl.tomo.Parallel2dGeometry(angle_partition, detector_partition)
+
+# The implementation of the ray transform to use, options:
+# 'scikit'                    Requires scikit-image (can be installed by
+#                             running ``pip install scikit-image``).
+# 'astra_cpu', 'astra_cuda'   Require astra tomography to be installed.
+#                             Astra is much faster than scikit. Webpage:
+#                             https://github.com/astra-toolbox/astra-toolbox
+impl = 'scikit'
+
+# Ray transform aka forward projection.
+ray_trafo = odl.tomo.RayTransform(reco_space, geometry, impl=impl)
+
+
+# --- Generate artificial data --- #
+
+
+# Create phantom
+discr_phantom = odl.util.shepp_logan(reco_space, modified=True)
+
+# Create sinogram of forward projected phantom with noise
+data = ray_trafo(discr_phantom)
+data += odl.util.white_noise(ray_trafo.range) * np.mean(data) * 0.1
+
+# Optionally pass partial to the solver to display intermediate results
+partial = (odl.solvers.PrintIterationPartial() &
+           odl.solvers.ShowPartial())
+
+# Choose a starting point
+x = ray_trafo.domain.zero()
+
+# Run the algorithm
+odl.solvers.conjugate_gradient_normal(
+    ray_trafo, x, data, niter=20, partial=partial)
+
+# Display images
+discr_phantom.show(title='original image')
+data.show(title='convolved image')
+x.show(title='deconvolved image', show=True)
