ENH/BUG: Add connection derivative/gradient in functional
This commit add the connection between the gradient and
the derivative of a functional. Moreover, it also corrects
minor bugs:
- make gradient a property
- make conjugate_functional a property
- rename lipschitz to grad_lipschitz

odl/solvers/functional/functional.py
@@ -37,7 +37,7 @@ class Functional(Operator):
 
     """Quick hack for a functional class."""
 
-    def __init__(self, domain, linear=False, smooth=False, concave=False, convex=False, lipschitz=np.inf):
+    def __init__(self, domain, linear=False, smooth=False, concave=False, convex=False, grad_lipschitz=np.inf):
         """Initialize a new instance.
 
         Parameters
@@ -65,12 +65,13 @@ class Functional(Operator):
         
         self._is_concave = bool(concave)
 
-        self._lipschitz = float(lipschitz)
+        self._grad_lipschitz = float(grad_lipschitz)
 
         super().__init__(domain=domain, range=domain.field, linear=linear)
 
-    def gradient(self, x, out=None):
-        """Evaluate the gradient of the functional.
+    @property
+    def gradient(self):
+        """Gradient operator of the functional.
 
         Parameters
         ----------
@@ -81,9 +82,8 @@ class Functional(Operator):
 
         Returns
         -------
-        out : domain element
-            Result of the gradient calcuation. If ``out`` was given,
-            the returned object is a reference to it.
+        out : `Operator`
+            Gradient operator of this functional.
         """
         raise NotImplementedError
 
@@ -102,9 +102,9 @@ class Functional(Operator):
         """
         raise NotImplementedError
 
-
+    @property
     def conjugate_functional(self):
-        """Return the conjugate functional of the functional.
+        """Convex conjugate functional of the functional.
 
         Parameters
         ----------
@@ -116,6 +116,24 @@ class Functional(Operator):
             Domain equal to domain of functional 
         """
         raise NotImplementedError
+
+
+    def derivative(self, point):
+        functional = self
+        
+        
+        class DerivativeOperator(Functional):
+            def __init__(self):
+                super().__init__(functional.domain, linear=True)
+
+            self.point=point
+            
+            #self.gradi=functional.gradient()
+
+            def _call(self, x):
+                return x.inner(functional.gradient(point)) #gradi(point))
+        
+        return DerivativeOperator()
 
 
     def __mul__(self, other):
@@ -203,9 +221,9 @@ class Functional(Operator):
         return self._is_convex   
 
     @property
-    def lipschitz(self):
-        """A Lipschitz constant for the functional"""
-        return self._lipschitz   
+    def grad_lipschitz(self):
+        """Lipschitz constant for the gradient of the functional"""
+        return self._grad_lipschitz   
 
 class FunctionalComp(Functional, OperatorComp):
 