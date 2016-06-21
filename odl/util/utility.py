2a83286d694164f236c203ed1f033ace541c14f4
Chong Chen <chchen@kth.se>
2016-06-21 10:18:52 +0200

    EHN: modify comments for snr.

--- odl/util/utility.py
+++ odl/util/utility.py
@@ -458,9 +458,10 @@ def snr(signal, noise, impl):
 
     Returns
     -------
-    The value of signal-to-noise ratio.
-        If the power of noise is zero, then the return is 'inf',
-        otherwise, the return is the computed value.
+    snr : `float`
+    Value of signal-to-noise ratio.
+    If the power of noise is zero, then the return is 'inf',
+    otherwise, the computed value.
     """
     if np.abs(np.asarray(noise)).sum() != 0:
         ave1 = np.sum(signal) / signal.size
