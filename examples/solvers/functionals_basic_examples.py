94298944e14164b80c40e32a17fb213b11741c47
aringh <aringh@kth.se>
2016-07-20 17:43:39 +0200

    ENH: Add an example on how to use the functionals.
    This commit adds an example on how to use the default functionals,
    and how to 'connect' them to solvers.

--- examples/solvers/functionals_basic_examples.py
+++ examples/solvers/functionals_basic_examples.py
@@ -1,96 +1,86 @@
-# -*- coding: utf-8 -*-
+# Copyright 2014-2016 The ODL development group
+#
+# This file is part of ODL.
+#
+# ODL is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ODL is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ODL.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Basic examples on how to use the fucntional class.
+
+This file contains two basic examples on how to use the functional class. It
+contains one example of how to use the default functionals, and how they can be
+used in connection to solvers. It also contains one example on how to
+implement a new functional.
 """
-Created on Tue May 31 13:36:12 2016
-
-@author: johan79
-"""
-
-# This file should be removed before pull request! (Or filled with some more interesting examples)
-
-from __future__ import print_function, division, absolute_import
-from future import standard_library
-standard_library.install_aliases()
-#from builtins import super
 
 import numpy as np
-
 import odl
-from odl.solvers.functional.default_functionals import (L1Norm, L2Norm, L2NormSquare)
 
-# Discretization parameters
-n = 4
+# First we show how the default functionals can be used in order to set up and
+# solve an optimization problem. The problem we will solve is to minimize
+# 1/2 * ||x - g||_2^2 + lam*||x||_1, for some vector g and some constant lam,
+# subject to that all components in x are greater than or equal to 0.
 
-# Discretized spaces
-space = odl.uniform_discr([0, 0], [1, 1], [n, n])
+# Create a space with dimensiona n.
+n = 10
+space = odl.rn(n)
 
+# Create parameters. First half of g are ones, second half are minus ones.
+g = space.element(np.hstack((np.ones(n/2), -np.ones(n/2))))
+lam = 0.5
 
-print(space)
+# Create the L1-norm functional and multiplyit with the constant lam.
+lam_l1_func = lam * odl.solvers.L1Norm(space)
 
-l1func = L1Norm(space)
-l1prox = l1func.proximal(sigma=1.5)
-l1conjFun = l1func.conjugate_functional
+# Create the squared L2-norm and translate it with g.
+l2_func = 1.0 / 2.0 * odl.solvers.L2NormSquare(space)
+trans_l2_func = l2_func.translate(g)
 
+# The problem will be solved using the Chambolle-Pock algorithm. Here we create
+# the necessary proximal factories of the conjugate functionals (see the
+# Chambolle-Pock algorithm and examples on this for more information).
+prox_cc_l2 = trans_l2_func.conjugate_functional.proximal
+prox_cc_l1 = lam_l1_func.conjugate_functional.proximal
 
-# Create phantom
-phantom = space.element(np.random.standard_normal((n, n)))
-# phantom = odl.util.shepp_logan(space, modified=True)*5+1
+# Combined the proximals for use in the solver
+proximal_dual = odl.solvers.combine_proximals(prox_cc_l2, prox_cc_l1)
 
-LogPhantom = np.log(phantom)
+# Create the matrix of operators for the Chambolle-Pock solver
+op = odl.BroadcastOperator(odl.IdentityOperator(space),
+                           odl.IdentityOperator(space))
 
-onevector = space.one() * 5
+# Create the proximal operator for the constraint
+proximal_primal = odl.solvers.proximal_nonnegativity(op.domain)
 
-prox_phantom = l1prox(phantom)
-l1conjFun_phantom = l1conjFun(phantom)
+# The operator norm is 1, since only identity operators are used
+op_norm = 1
 
-l2func = odl.solvers.functional.L2Norm(space)
-l2prox = l2func.proximal(sigma=1.5)
-l2conjFun = l2func.conjugate_functional
-l2Grad = l2func.gradient
+# Some solver parameters
+niter = 100  # Number of iterations
+tau = 1.0 / op_norm  # Step size for the primal variable
+sigma = 1.0 / op_norm  # Step size for the dual variable
 
-prox2_phantom = l2prox(phantom*10)
-l2conjFun_phantom = l2conjFun(phantom/10)
+# Optional: pass callback objects to solver
+callback = (odl.solvers.CallbackPrintIteration())
 
-l22 = odl.solvers.functional.L2NormSquare(space)
-prox22 = l22.proximal(1)(phantom)
+# Starting point, and also updated inplace in the solver
+x = op.domain.zero()
 
-l22(phantom)
-cf22 = l22.conjugate_functional(phantom)
+# Run the algorithm
+odl.solvers.chambolle_pock_solver(
+    op, x, tau=tau, sigma=sigma, proximal_primal=proximal_primal,
+    proximal_dual=proximal_dual, niter=niter, callback=callback)
 
-l1func3 = -3*l1func
-
-l1func3(phantom)
-l1func(phantom)
-
-F = l22
-
-epsK = 1e-8
-
-print(epsK)
-
-(F(phantom+epsK*LogPhantom)-F(phantom))/epsK
-
-LogPhantom.inner(F.gradient(phantom))
-
-print(l22(phantom))
-
-print((-2*l22)(phantom))
-
-print((l22*2)(phantom))
-
-x = space.element(np.random.standard_normal((n, n)))
-#    y = space.element(np.random.rand(n,n))
-
-scal = np.random.standard_normal()
-F = odl.solvers.functional.L2Norm(space)
-
-Fs = F*scal
-
-print(Fs(x))
-
-
-l2grad = (scal*F).gradient
-
-print((scal*F).gradient(x))
-print((F*scal).gradient(x))
-
-z = l2grad(x)
+# The theoretical optimal solution to this problem is x = (g - lam)_+, where
+# ( )+ denotes the positive part of (i.e., (z)_+ = z if z >= 0, 0 otherwise).
+print(x.asarray())
