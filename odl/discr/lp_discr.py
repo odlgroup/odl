410db790b5a7a2014a14f711b97f5f2d97a637c8
Holger Kohr <kohr@kth.se>
2016-08-11 20:19:30 +0200

    BUG: fix vector_field_space in DiscreteLp after rebase

--- odl/discr/lp_discr.py
+++ odl/discr/lp_discr.py
@@ -234,9 +234,8 @@ class DiscreteLp(DiscretizedSpace):
 
     @property
     def vector_field_space(self):
-        """vector field space."""
-        real_space = self.astype(self._real_dtype)
-        return ProductSpace(real_space, self.ndim)
+        """Space of vector fields based on ``self``."""
+        return ProductSpace(self.real_space, self.ndim)
 
     def element(self, inp=None, **kwargs):
         """Create an element from ``inp`` or from scratch.
