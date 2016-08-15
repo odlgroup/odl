7d505ea10125a9c2058d7d1fd32961c69a87365b
Jonas Adler <jonasadl@kth.se>
2016-08-15 17:10:51 +0200

    MAINT: fixes to the release script

--- tools/release.sh
+++ tools/release.sh
@@ -59,11 +59,17 @@ if [ -z $TWINE ]; then
 fi
 
 PYPI_DIST_DIR=$(cd dist && echo "$PWD")  # Absolute path
-PYPI_DIST_FILES="$(ls -1a $PYPI_DIST_DIR | grep ${PACKAGE_NAME}-${VERSION})"
+PYPI_DIST_FILES="$(ls -1ad $PYPI_DIST_DIR/* | grep ${PACKAGE_NAME}-${VERSION})"
 
-if [ -z $PYPI_DIST_FILES ]; then
+if [[ -z $PYPI_DIST_FILES ]]; then
     echo 'No distribution files found in the `dist` directory.'
     echo 'You need to run `python setup.py sdist` and `python setup.py bdist_wheel` first to build the distribution.'
+    exit 1
+fi
+
+if [ -z $PYPI_PASSWORD ]; then
+    echo 'No password set for pypi.'
+    echo 'Set the environment variable PYPI_PASSWORD and try again.'
     exit 1
 fi
 
@@ -71,7 +77,7 @@ if [ $DRY_RUN -eq 1 ]; then
     echo "The following files would be uploaded to PyPI:"
     echo -e "$PYPI_DIST_FILES"
 else
-    $TWINE upload -u $PYPI_USER $PYPI_DIST_FILES || exit 1
+    $TWINE upload -u $PYPI_USER -p $PYPI_PASSWORD $PYPI_DIST_FILES || exit 1
 fi
 
 echo
@@ -79,32 +85,37 @@ echo
 ### Conda upload
 CONDA_USER="odlgroup"
 
-# Find conda build directory
-if [ -n $CONDA_ENV_PATH ]; then
-    CONDA_BUILD_DIR="$CONDA_ENV_PATH/../../conda-bld"
-else
-    CONDA_BUILD_DIR="$(which conda)/../../conda-bld"
-fi
+if ! type "anaconda" > /dev/null; then
 
-# Compile all files in the build folders corresponding to package and version
-CONDA_DIST_FILES=""
-CONDA_DIST_DIRS="linux-32 linux-64 osx-64 win-32 win-64"
-for DIR in $CONDA_DIST_DIRS; do
-    if [ -d $CONDA_BUILD_DIR/$DIR ]; then
-        if [ -z $CONDA_DIST_FILES ]; then
-            CONDA_DIST_FILES="$(ls -1 $CONDA_BUILD_DIR/$DIR/$PACKAGE_NAME-$VERSION*)\n"
-        else
-            CONDA_DIST_FILES="$CONDA_DIST_FILES$(ls -1 $CONDA_BUILD_DIR/$DIR/$PACKAGE_NAME-$VERSION*)\n"
-        fi
+    # Find conda build directory
+    if [ -n $CONDA_ENV_PATH ]; then
+        CONDA_BUILD_DIR="$CONDA_ENV_PATH/../../conda-bld"
+    else
+        CONDA_BUILD_DIR="$(which conda)/../../conda-bld"
     fi
-done
 
-# Do the upload (or pretend to)
-if [ $DRY_RUN -eq 1 ]; then
-    echo "The following files would be uploaded to Anaconda Cloud:"
-    echo -e $CONDA_DIST_FILES
+    # Compile all files in the build folders corresponding to package and version
+    CONDA_DIST_FILES=""
+    CONDA_DIST_DIRS="linux-32 linux-64 osx-64 win-32 win-64"
+    for DIR in $CONDA_DIST_DIRS; do
+        if [ -d $CONDA_BUILD_DIR/$DIR ]; then
+            if [ -z $CONDA_DIST_FILES ]; then
+                CONDA_DIST_FILES="$(ls -1 $CONDA_BUILD_DIR/$DIR/$PACKAGE_NAME-$VERSION*)\n"
+            else
+                CONDA_DIST_FILES="$CONDA_DIST_FILES$(ls -1 $CONDA_BUILD_DIR/$DIR/$PACKAGE_NAME-$VERSION*)\n"
+            fi
+        fi
+    done
+
+    # Do the upload (or pretend to)
+    if [ $DRY_RUN -eq 1 ]; then
+        echo "The following files would be uploaded to Anaconda Cloud:"
+        echo -e $CONDA_DIST_FILES
+    else
+        anaconda login --username $CONDA_USER || exit 1
+        anaconda upload $CONDA_DIST_FILES || exit 1
+        anaconda logout
+    fi
 else
-    anaconda login --username $CONDA_USER || exit 1
-    anaconda upload $CONDA_DIST_FILES || exit 1
-    anaconda logout
+    echo "anaconda not installed, skipping"
 fi
