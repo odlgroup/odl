a38f50dc7be7ccff656027a4d091595976936895
Jonas Adler <jonasadl@kth.se>
2016-09-22 18:24:16 +0200

    ENH/BUG: Update Broyden solvers
    * Update BFGS and Broyden solvers to use functionals according to #569
    * Fix bugs with Broyden solvers, closes #599
    * Changed Broyden design from two separate solvers to a single solver with with a flag
    * Add test of using the BFGS and Broyden solvers

--- odl/solvers/findroot/newton.py
+++ odl/solvers/findroot/newton.py
@@ -22,16 +22,18 @@ from __future__ import print_function, division, absolute_import
 from future import standard_library
 standard_library.install_aliases()
 
+import numpy as np
 from odl.operator import IdentityOperator
+from odl.solvers.scalar.steplen import ConstantLineSearch
+from odl.operator.oputils import matrix_representation
 
-
-__all__ = ('bfgs_method', 'broydens_first_method', 'broydens_second_method')
+__all__ = ('bfgs_method', 'broydens_method')
 
 
 # TODO: update all docs
 
 
-def bfgs_method(grad, x, line_search, niter=1, callback=None):
+def bfgs_method(f, x, line_search=1.0, maxiter=1000, tol=1e-16, callback=None):
     """Quasi-Newton BFGS method to minimize a differentiable function.
 
     This is a general and optimized implementation of a quasi-Newton
@@ -58,34 +60,42 @@ Goldfarb%E2%80%93Shanno_algorithm>`_
 
     Parameters
     ----------
-    grad : `Operator`
-        Gradient mapping of the objective function, i.e. the mapping
-        :math:`x \mapsto \\nabla f(x) \\in \mathcal{X}`
-    x : ``grad.domain`` element
+    f : `Functional`
+        Functional with ``f.gradient``
+    x : ``f.domain`` element
         Starting point of the iteration
-    line_search : `LineSearch`
-        Strategy to choose the step length
-    niter : int, optional
-        Number of iterations
+    line_search : float or `LineSearch`, optional
+        Strategy to choose the step length. If a float is given, uses it as a
+        fixed step length.
+    maxiter : int, optional
+        Maximum number of iterations.
+        ``tol``.
+    tol : float, optional
+        Tolerance that should be used for terminating the iteration.
     callback : `callable`, optional
         Object executing code per iteration, e.g. plotting each iterate.
-
-    Returns
-    -------
-    None
     """
 
-    if x not in grad.domain:
+    if x not in f.domain:
         raise TypeError('`x` {!r} is not in the domain of `grad` {!r}'
-                        ''.format(x, grad.domain))
+                        ''.format(x, f.domain))
 
-    hess = ident = IdentityOperator(grad.range)
+    if not callable(line_search):
+        line_search = ConstantLineSearch(line_search)
+
+    grad = f.gradient
+
+    hess = ident = IdentityOperator(f.domain)
     grad_x = grad(x)
-    for _ in range(niter):
+    for _ in range(maxiter):
+        # Determine a stepsize using line search
         search_dir = -hess(grad_x)
         dir_deriv = search_dir.inner(grad_x)
+        if np.abs(dir_deriv) < tol:
+            return  # we found an optimum
         step = line_search(x, direction=search_dir, dir_derivative=dir_deriv)
 
+        # Update x
         x_update = search_dir
         x_update *= step
         x += x_update
@@ -95,8 +105,7 @@ Goldfarb%E2%80%93Shanno_algorithm>`_
         grad_diff.space.lincomb(-1, grad_diff, 1, grad_x, out=grad_diff)
 
         y_inner_s = grad_diff.inner(x_update)
-        # TODO: use a small (adjustable) tolerance instead of 0.0
-        if y_inner_s == 0.0:
+        if np.abs(y_inner_s) < tol:
             return
 
         # Update Hessian
@@ -109,12 +118,13 @@ Goldfarb%E2%80%93Shanno_algorithm>`_
             callback(x)
 
 
-def broydens_first_method(grad, x, line_search, niter=1, callback=None):
+def broydens_method(f, x, line_search=1.0, impl='first', maxiter=1000,
+                    tol=1e-16, callback=None):
     """Broyden's first method, a quasi-Newton scheme.
 
-    This is a general and optimized implementation of Broyden's first
-    (or 'good') method, a quasi-Newton method for solving a general
-    unconstrained optimization problem
+    This is a general and optimized implementation of Broyden's  method,
+    a quasi-Newton method for solving a general unconstrained optimization
+    problem
 
         :math:`\min f(x)`
 
@@ -132,113 +142,66 @@ def broydens_first_method(grad, x, line_search, niter=1, callback=None):
 
     Parameters
     ----------
-    grad : `Operator`
-        Gradient mapping of the objective function, i.e. the mapping
-        :math:`x \mapsto \\nabla f(x) \\in \mathcal{X}`
-    x : ``grad.domain`` element
+    f : `Functional`
+        Functional with ``f.gradient``
+    x : ``f.domain`` element
         Starting point of the iteration
-    line_search : `LineSearch`
-        Strategy to choose the step length
-    niter : int, optional
-        Number of iterations
+    line_search : float or `LineSearch`, optional
+        Strategy to choose the step length. If a float is given, uses it as a
+        fixed step length.
+    impl : {'first', 'second'}
+        What version of Broydens method to use. First is also known as Broydens
+        'good' method, while the second is known as Broydens 'bad' method.
+    maxiter : int, optional
+        Maximum number of iterations.
+        ``tol``.
+    tol : float, optional
+        Tolerance that should be used for terminating the iteration.
     callback : `callable`, optional
         Object executing code per iteration, e.g. plotting each iterate.
-
-    Returns
-    -------
-    None
     """
-    hess = IdentityOperator(grad.range)
-    grad_x = grad(x)
-    search_dir = hess(grad_x)
+    if x not in f.domain:
+        raise TypeError('`x` {!r} is not in the domain of `grad` {!r}'
+                        ''.format(x, f.domain))
+    grad = f.gradient
 
-    for _ in range(niter):
-        step = line_search(x, search_dir, grad_x)
+    if not callable(line_search):
+        line_search = ConstantLineSearch(line_search)
 
-        u = search_dir.copy()  # Save old hess(grad f(x))
-
-        x_update = search_dir  # Just rename, still writing to search_dir
-        x_update *= step
-        x += x_update  # x_(k+1) = x_k + s * d
-
-        grad_x, grad_diff = grad(x), grad_x
-        # grad_diff = grad(x) - grad(x_old)
-        grad_diff.space.lincomb(-1, grad_diff, 1, grad_x, out=grad_diff)
-
-        search_dir = hess(grad_x)  # Calculate new hess(grad f (x))
-
-        v = hess(x_update)  # v = H(s * d)
-        scalprod = v.inner(grad_diff)
-        if scalprod == 0:
-            return
-
-        u *= (step - 1)
-        u += search_dir
-        u /= scalprod
-        hess -= u * v.T
-
-        if callback is not None:
-            callback(x)
-
-
-def broydens_second_method(grad, x, line_search, niter=1, callback=None):
-    """Broyden's first method, a quasi-Newton scheme.
-
-    This is a general and optimized implementation of Broyden's second
-    (or 'bad') method, a quasi-Newton method for solving a general
-    unconstrained optimization problem
-
-        :math:`\min f(x)`
-
-    for a differentiable function
-    :math:`f: \mathcal{X}\\to \mathbb{R}` on a Hilbert space
-    :math:`\mathcal{X}`. It does so by finding a zero of the gradient
-
-        :math:`\\nabla f: \mathcal{X} \\to \mathcal{X}`
-
-    using a Newton-type update scheme with approximate Hessian.
-
-    The algorithm is described in [Bro1965]_ and [Kva1991]_, and in a
-    `Wikipedia article
-    <https://en.wikipedia.org/wiki/Broyden's_method>`_
-
-    Parameters
-    ----------
-    grad : `Operator`
-        Gradient mapping of the objective function, i.e. the mapping
-        :math:`x \mapsto \\nabla f(x) \\in \mathcal{X}`
-    x : ``grad.domain`` element
-        Starting point of the iteration
-    line_search : `LineSearch`
-        Strategy to choose the step length
-    niter : int, optional
-        Number of iterations
-    callback : `callable`, optional
-        Object executing code per iteration, e.g. plotting each iterate
-    """
-    # TODO: potentially make the implementation faster by considering
-    # performance optimization according to Kvaalen.
-
-    hess = IdentityOperator(grad.range)
+    ident = IdentityOperator(f.domain)
+    hess = IdentityOperator(f.domain)
     grad_x = grad(x)
 
-    for _ in range(niter):
-        search_dir = hess(grad_x)
-        step = line_search(x, search_dir, grad_x)
+    for _ in range(maxiter):
+        # find step size
+        search_dir = -hess(grad_x)
+        dir_deriv = search_dir.inner(grad_x)
+        if np.abs(dir_deriv) < tol:
+            return  # we found an optimum
+        step = line_search(x, search_dir, dir_deriv)
 
-        x_update = search_dir  # Just rename, still writing to search_dir
-        x_update *= step
-        x += x_update
+        # update x
+        delta_x = step * search_dir
+        x += delta_x
 
-        grad_x, grad_diff = grad(x), grad_x
-        # grad_diff = grad(x) - grad(x_old)
-        grad_diff.space.lincomb(-1, grad_diff, 1, grad_x, out=grad_diff)
+        # compute new gradient
+        grad_x, grad_x_old = grad(x), grad_x
+        delta_grad = grad_x - grad_x_old
 
-        grad_diff_norm2 = grad_diff.norm() ** 2
-        if grad_diff_norm2 == 0:
-            return
-        u = (x_update + hess(grad_diff)) / grad_diff_norm2
-        hess -= u * grad_diff.T
+        # update hessian
+        v = hess(delta_grad)
+        if impl == 'first':
+            divisor = delta_x.inner(v)
+            if np.abs(divisor) < tol:
+                return
+            u = (search_dir - v) * (1 / divisor)
+            hess = (ident + u * delta_x.T) * hess
+        elif impl == 'second':
+            divisor = delta_grad.inner(delta_grad)
+            if np.abs(divisor) < tol:
+                return
+            u = (search_dir - v) * (1 / divisor)
+            hess = hess + u * delta_grad.T
 
         if callback is not None:
             callback(x)
