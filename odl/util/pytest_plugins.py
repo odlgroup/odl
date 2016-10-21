5d9b3027b16f05bf90c24d41bf66c4256d872bda
Holger Kohr <h.kohr@cwi.nl>
2016-10-22 00:22:02 +0200

    MAINT: fence fixture definitions in pytest plugin

--- odl/util/pytest_plugins.py
+++ odl/util/pytest_plugins.py
@@ -23,17 +23,25 @@ standard_library.install_aliases()
 
 import numpy as np
 import operator
-import pytest
 
 import odl
 from odl.trafos.backends import PYFFTW_AVAILABLE, PYWT_AVAILABLE
-from odl.util import dtype_repr
+from odl.util import dtype_repr, OptionalArgDecorator
+
+try:
+    from pytest import fixture
+except ImportError:
+    # Make trivial decorator
+    class fixture(OptionalArgDecorator):
+        @staticmethod
+        def _wrapper(f, *a, **kw):
+            return f
 
 
 # --- Add numpy and ODL to all doctests ---
 
 
-@pytest.fixture(autouse=True)
+@fixture(autouse=True)
 def add_doctest_np_odl(doctest_namespace):
     doctest_namespace['np'] = np
     doctest_namespace['odl'] = odl
@@ -71,7 +79,7 @@ fn_impl_params = odl.FN_IMPLS.keys()
 fn_impl_ids = [" impl='{}' ".format(p) for p in fn_impl_params]
 
 
-@pytest.fixture(scope="module", ids=fn_impl_ids, params=fn_impl_params)
+@fixture(scope="module", ids=fn_impl_ids, params=fn_impl_params)
 def fn_impl(request):
     """String with an available `FnBase` implementation name."""
     return request.param
@@ -80,8 +88,7 @@ ntuples_impl_params = odl.NTUPLES_IMPLS.keys()
 ntuples_impl_ids = [" impl='{}' ".format(p) for p in ntuples_impl_params]
 
 
-@pytest.fixture(scope="module", ids=ntuples_impl_ids,
-                params=ntuples_impl_params)
+@fixture(scope="module", ids=ntuples_impl_ids, params=ntuples_impl_params)
 def ntuples_impl(request):
     """String with an available `NtuplesBase` implementation name."""
     return request.param
@@ -92,8 +99,7 @@ floating_dtype_ids = [' dtype={} '.format(dtype_repr(dt))
                       for dt in floating_dtype_params]
 
 
-@pytest.fixture(scope="module", ids=floating_dtype_ids,
-                params=floating_dtype_params)
+@fixture(scope="module", ids=floating_dtype_ids, params=floating_dtype_params)
 def floating_dtype(request):
     """Floating point (real or complex) dtype."""
     return request.param
@@ -106,8 +112,7 @@ scalar_dtype_ids = [' dtype={} '.format(dtype_repr(dt))
                     for dt in scalar_dtype_params]
 
 
-@pytest.fixture(scope="module", ids=scalar_dtype_ids,
-                params=scalar_dtype_params)
+@fixture(scope="module", ids=scalar_dtype_ids, params=scalar_dtype_params)
 def scalar_dtype(request):
     """Scalar (integers or real or complex) dtype."""
     return request.param
@@ -117,7 +122,7 @@ ufunc_params = odl.util.ufuncs.UFUNCS
 ufunc_ids = [' ufunc={} '.format(p[0]) for p in ufunc_params]
 
 
-@pytest.fixture(scope="module", ids=ufunc_ids, params=ufunc_params)
+@fixture(scope="module", ids=ufunc_ids, params=ufunc_params)
 def ufunc(request):
     """Tuple with information on a ufunc.
 
@@ -139,7 +144,7 @@ reduction_params = odl.util.ufuncs.REDUCTIONS
 reduction_ids = [' reduction={} '.format(p[0]) for p in reduction_params]
 
 
-@pytest.fixture(scope="module", ids=reduction_ids, params=reduction_params)
+@fixture(scope="module", ids=reduction_ids, params=reduction_params)
 def reduction(request):
     """Tuple with information on a reduction.
 
@@ -164,7 +169,7 @@ arithmetic_op_ids = [' + ', ' / ', ' * ', ' - ',
                      ' += ', ' /= ', ' *= ', ' -= ']
 
 
-@pytest.fixture(ids=arithmetic_op_ids, params=arithmetic_op_par)
+@fixture(ids=arithmetic_op_ids, params=arithmetic_op_par)
 def arithmetic_op(request):
     """An arithmetic operator, e.g. +, -, // etc."""
     return request.param
