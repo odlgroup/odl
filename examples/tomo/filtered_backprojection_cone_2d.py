ENH: Fix scaling of FBP for cone and fan beam geometries

examples/tomo/filtered_backprojection_cone_2d.py
@@ -16,7 +16,7 @@
 # along with ODL.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-Example using a filtered backprojection (FBP) in cone-beam 3d using `fbp_op`.
+Example using a filtered backprojection (FBP) in fan beam using `fbp_op`.
 
 Note that the FBP is only approximate in this geometry, but still gives a
 decent reconstruction that can be used as an initial guess in more complicated
@@ -31,20 +31,19 @@ import odl
 
 
 # Discrete reconstruction space: discretized functions on the cube
-# [-20, 20]^3 with 300 samples per dimension.
+# [-20, 20]^2 with 300 samples per dimension.
 reco_space = odl.uniform_discr(
-    min_pt=[-20, -20, -20], max_pt=[20, 20, 20], shape=[300, 300, 300],
+    min_pt=[-20, -20], max_pt=[20, 20], shape=[300, 300],
     dtype='float32')
 
 # Make a circular cone beam geometry with flat detector
 # Angles: uniformly spaced, n = 360, min = 0, max = 2 * pi
 angle_partition = odl.uniform_partition(0, 2 * np.pi, 360)
-# Detector: uniformly sampled, n = (558, 558), min = (-40, -40), max = (40, 40)
-detector_partition = odl.uniform_partition([-40, -40], [40, 40], [558, 558])
-# Geometry with large cone and fan angle and tilted axis.
-geometry = odl.tomo.CircularConeFlatGeometry(
-    angle_partition, detector_partition, src_radius=40, det_radius=40,
-    axis=[1, 1, 1])
+# Detector: uniformly sampled, n = 558, min = -60, max = 60
+detector_partition = odl.uniform_partition(-60, 60, 558)
+# Geometry with large fan angle
+geometry = odl.tomo.FanFlatGeometry(
+    angle_partition, detector_partition, src_radius=40, det_radius=40)
 
 
 # --- Create FilteredBackProjection (FBP) operator --- #

odl/test/largescale/tomo/analytic_slow_test.py
@@ -117,9 +117,9 @@ def projector(request, dtype):
                                              [100, 100], dtype=dtype)
 
         # Geometry
-        dpart = odl.uniform_partition(-30, 30, 200)
+        dpart = odl.uniform_partition(-40, 40, 200)
         geom = tomo.FanFlatGeometry(apart, dpart,
-                                    src_radius=500, det_radius=100)
+                                    src_radius=100, det_radius=100)
 
         # Ray transform
         return tomo.RayTransform(discr_reco_space, geom,
@@ -131,9 +131,9 @@ def projector(request, dtype):
                                              [100, 100, 100], dtype=dtype)
 
         # Geometry
-        dpart = odl.uniform_partition([-30, -30], [30, 30], [200, 200])
+        dpart = odl.uniform_partition([-40, -40], [40, 40], [200, 200])
         geom = tomo.CircularConeFlatGeometry(
-            apart, dpart, src_radius=500, det_radius=100, axis=[1, 0, 0])
+            apart, dpart, src_radius=100, det_radius=100, axis=[1, 0, 0])
 
         # Ray transform
         return tomo.RayTransform(discr_reco_space, geom,
@@ -150,7 +150,7 @@ def projector(request, dtype):
         apart = odl.uniform_partition(0, 8 * 2 * np.pi, n_angle)
         dpart = odl.uniform_partition([-30, -3], [30, 3], [200, 20])
         geom = tomo.HelicalConeFlatGeometry(apart, dpart, pitch=5.0,
-                                            src_radius=500, det_radius=100)
+                                            src_radius=100, det_radius=100)
 
         # Ray transform
         return tomo.RayTransform(discr_reco_space, geom,

odl/tomo/analytic/filtered_back_projection.py
@@ -98,9 +98,17 @@ def fbp_op(ray_trafo, padding=True):
         else:
             axes = [1, 2]
 
+        # Add scaling for cone-beam case
+        if hasattr(ray_trafo.geometry, 'src_radius'):
+            scale = (ray_trafo.geometry.src_radius /
+                     (ray_trafo.geometry.src_radius +
+                      ray_trafo.geometry.det_radius))
+        else:
+            scale = 1.0
+
         # Define ramp filter
         def fft_filter(x):
-            return np.abs(c[0] * x[1] + c[1] * x[2]) / (2 * alen)
+            return np.abs(c[0] * x[1] + c[1] * x[2]) * (scale / (2 * alen))
 
         # Define (padded) fourier transform
         if padding: