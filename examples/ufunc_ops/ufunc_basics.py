776cd23d4b75ff563959b2cb032dc0fee7118798
Jonas Adler <jonasadl@kth.se>
2016-12-05 14:55:03 +0100

    MAINT: Improve UFuncOperator examples

--- examples/ufunc_ops/ufunc_basics.py
+++ examples/ufunc_ops/ufunc_basics.py
@@ -15,15 +15,37 @@
 # You should have received a copy of the GNU General Public License
 # along with ODL.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Examples of using the trigonometric UFunc functionals in ODL."""
+"""Basic examples of using the UFunc functionals in ODL."""
 
 from __future__ import print_function
 import odl
 
-# Trigonometric functions can be computed, along with their gradient
+
+# Trigonometric functions can be computed, along with their gradients.
+
 
 cos = odl.ufunc_ops.cos()
 sin = odl.ufunc_ops.sin()
 
+# Compute cosine and its gradient
+
 print('cos(0)={}, cos.gradient(0.2)={}, -sin(0.2)={}'.format(
     cos(0), cos.gradient(0.2), -sin(0.2)))
+
+
+# Other functions include the square, exponential, etc
+# Higher order derivatives are obtained via the gradient of the gradient, etc.
+
+square = odl.ufunc_ops.square()
+
+print('[x^2](3) = {}, [d/dx x^2](3) = {}, '
+      '[d^2/dx^2 x^2](3) = {}, [d^3/dx^3 x^2](3) = {}'.format(
+    square(3), square.gradient(3),
+    square.gradient.gradient(3), square.gradient.gradient.gradient(3)))
+
+
+# Can also define UFuncs on vector-spaces, then they act pointwise.
+
+r3 = odl.rn(3)
+exp_r3 = odl.ufunc_ops.exp(r3)
+print('e^[1, 2, 3] = {}'.format(exp_r3([1, 2, 3])))
--- examples/ufunc_ops/ufunc_solvers.py
+++ examples/ufunc_ops/ufunc_solvers.py
@@ -22,7 +22,6 @@ Here, we minimize the logarithm of the rosenbrock function:
     min_x log(rosenbrock(x) + 0.1)
 """
 
-from __future__ import print_function
 import odl
 
 # Create space and functionals
@@ -30,15 +29,16 @@ r2 = odl.rn(2)
 rosenbrock = odl.solvers.RosenbrockFunctional(r2, scale=2.0)
 log = odl.ufunc_ops.log()
 
-# Create optimization function by composing log with rosenbrock, add 0.1 to
+# Create goal functional by composing log with rosenbrock and add 0.1 to
 # avoid singularity at 0
 opt_fun = log * (rosenbrock + 0.1)
 
-# Select solution method, steepest descent with line search
+# Solve problem using steepest descent with backtracking line search,
+# starting in the point x = [0, 0]
 line_search = odl.solvers.BacktrackingLineSearch(opt_fun)
 
 x = opt_fun.domain.zero()
-odl.solvers.steepest_descent(opt_fun.gradient, x, niter=100,
+odl.solvers.steepest_descent(opt_fun, x, maxiter=100,
                              line_search=line_search)
 
 print('optimization result={}. Should be [1, 1]'.format(x))
