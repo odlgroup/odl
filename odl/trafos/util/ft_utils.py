58eec42933b0385a8b29b7ca63d62a036d4e5970
Jonas Adler <jonasadl@kth.se>
2016-12-19 14:38:20 +0100

    ENH: Add axis_labels option to DiscreteLp, closes #769

--- odl/trafos/util/ft_utils.py
+++ odl/trafos/util/ft_utils.py
@@ -639,8 +639,14 @@ def reciprocal_space(space, axes=None, halfcomplex=False, shift=True,
     else:
         part = uniform_partition_fromgrid(recip_grid)
 
+    # Use convention of adding a hat to represent fourier transform of variable
+    axis_labels = list(space.axis_labels)
+    for i in axes:
+        axis_labels[i] = '$\^{{{}}}$'.format(axis_labels[i])
+
     recip_spc = uniform_discr_frompartition(part, exponent=exponent,
-                                            dtype=dtype, impl=impl)
+                                            dtype=dtype, impl=impl,
+                                            axis_labels=axis_labels)
 
     return recip_spc
 
