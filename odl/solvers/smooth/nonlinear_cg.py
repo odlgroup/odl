a5a5e749fe4b0176807b0fce81af9d0ddc123014
Jonas Adler <jonasadl@kth.se>
2017-01-19 14:51:34 +0100

    MAINT: Style fixes to nonlinear CG

--- odl/solvers/smooth/nonlinear_cg.py
+++ odl/solvers/smooth/nonlinear_cg.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with ODL.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Nonlinear version of conjugate gradient."""
+"""Nonlinear version of the conjugate gradient."""
 
 # Imports for common Python 2/3 codebase
 from __future__ import print_function, division, absolute_import
@@ -32,24 +32,7 @@ __all__ = ('conjugate_gradient_nonlinear',)
 def conjugate_gradient_nonlinear(f, x, line_search=1.0, maxiter=1000, nreset=0,
                                  tol=1e-16, beta_method='FR',
                                  callback=None):
-    """Conjugate gradient for nonlinear problems.
-
-    Notes
-    -----
-    This is a general and optimized implementation of the nonlinear conjguate
-    gradient method for solving a general unconstrained optimization problem
-
-        :math:`\min f(x)`
-
-    for a differentiable function
-    :math:`f: \mathcal{X}\\to \mathbb{R}` on a Hilbert space
-    :math:`\mathcal{X}`. It does so by finding a zero of the gradient
-
-        :math:`\\nabla f: \mathcal{X} \\to \mathcal{X}`.
-
-    The method is described in a
-    `Wikipedia article
-    <https://en.wikipedia.org/wiki/Nonlinear_conjugate_gradient_method>`_.
+    """Conjugate gradient method for general nonlinear problems.
 
     Parameters
     ----------
@@ -60,13 +43,12 @@ def conjugate_gradient_nonlinear(f, x, line_search=1.0, maxiter=1000, nreset=0,
         used as starting point of the iteration, and its values are
         updated in each iteration step.
     line_search : float or `LineSearch`, optional
-        Strategy to choose the step length. If a float is given, uses it as a
-        fixed step length.
+        Strategy to choose the step length. If a float is given, it is used as
+        a fixed step length.
     maxiter : int, optional
         Maximum number of iterations to perform.
     nreset : int, optional
         Number of times the solver should be reset. Default: no reset.
-        fixed step length. Default: `BacktrackingLineSearch`
     tol : float, optional
         Tolerance that should be used for terminating the iteration.
     beta_method : {'FR', 'PR', 'HS', 'DY'}
@@ -76,13 +58,32 @@ def conjugate_gradient_nonlinear(f, x, line_search=1.0, maxiter=1000, nreset=0,
         * ``'HS'`` : Hestenes-Stiefel
         * ``'DY'`` : Dai-Yuan
     callback : `callable`, optional
-        Object executing code per iteration, e.g. plotting each iterate
+        Object executing code per iteration, e.g. plotting each iterate.
+
+    Notes
+    -----
+    This is a general and optimized implementation of the nonlinear conjguate
+    gradient method for solving a general unconstrained optimization problem
+
+        :math:`\min f(x)`
+
+    for a differentiable functional
+    :math:`f: \mathcal{X}\\to \mathbb{R}` on a Hilbert space
+    :math:`\mathcal{X}`. It does so by finding a zero of the gradient
+
+        :math:`\\nabla f: \mathcal{X} \\to \mathcal{X}`.
+
+    The method is described in a
+    `Wikipedia article
+    <https://en.wikipedia.org/wiki/Nonlinear_conjugate_gradient_method>`_.
 
     See Also
     --------
     bfgs_method : Quasi-newton solver for the same problem
-    conjugate_gradient : Optimized solver for linear and symmetric case
-    conjugate_gradient_normal : Equivalent solver but for linear case
+    conjugate_gradient : Optimized solver for least-squares problem with linear
+    and symmetric operator
+    conjugate_gradient_normal : Equivalent solver but for least-squares problem
+    with linear operator
     """
 
     if x not in f.domain:
@@ -128,7 +129,7 @@ def conjugate_gradient_nonlinear(f, x, line_search=1.0, maxiter=1000, nreset=0,
 
             # Find optimal step along s
             dir_derivative = -dx.inner(s)
-            if abs(dir_derivative) < tol:
+            if abs(dir_derivative) <= tol:
                 return
             a = line_search(x, s, dir_derivative)
 
