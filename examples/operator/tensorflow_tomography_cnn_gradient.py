ce186b5253d7533f25374d36779347b49bd915fc
Jonas Adler <jonasadl@kth.se>
2017-02-28 14:27:05 +0100

    ENH: Improved cnn gradient example

--- examples/operator/tensorflow_tomography_cnn_gradient.py
+++ examples/operator/tensorflow_tomography_cnn_gradient.py
@@ -26,7 +26,13 @@ def var(x):
     return tf.Variable(tf.constant(x, dtype='float32'))
 
 
+def create_variable(name, shape):
+    variable = tf.Variable(tf.truncated_normal(shape, stddev=0.01), name=name)
+    return variable
+
+
 with tf.Session() as sess:
+
     # Create ODL data structures
     size = 128
     space = odl.uniform_discr([-64, -64], [64, 64], [size, size],
@@ -67,13 +73,13 @@ with tf.Session() as sess:
 
     # Create constant right hand side
 
-    w1 = tf.Variable(tf.truncated_normal([3, 3, 7, 32], stddev=0.01))
+    w1 = create_variable('w1', shape=[3, 3, 7, 32])
     b1 = var(np.ones(32) * 0.1)
 
-    w2 = tf.Variable(tf.truncated_normal([3, 3, 32, 32], stddev=0.01))
+    w2 = create_variable('w2', shape=[3, 3, 32, 32])
     b2 = var(np.ones(32) * 0.1)
 
-    w3 = tf.Variable(tf.truncated_normal([3, 3, 32, 6], stddev=0.01))
+    w3 = create_variable('w3', shape=[3, 3, 32, 6])
     b3 = var(np.random.randn(6) * 0.001)
 
     n_iter = 5
@@ -108,7 +114,7 @@ with tf.Session() as sess:
     # Solve with an ODL callback to see what happens
     callback = odl.solvers.CallbackShow()
 
-    for i in range(10000):
+    for i in range(1000):
         _, loss_training = sess.run([optimizer, loss],
                                   feed_dict={learning_rate: 0.001,
                                              x_0: x_arr[1:],
