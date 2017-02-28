8b0efdd98b915ec2520989a147d2cb6d1deb852f
Jonas Adler <jonasadl@kth.se>
2017-02-28 14:27:05 +0100

    ENH: Improved cnn gradient example

--- examples/operator/tensorflow_tomography_cnn_gradient.py
+++ examples/operator/tensorflow_tomography_cnn_gradient.py
@@ -26,7 +26,13 @@ def var(x):
     return tf.Variable(tf.constant(x, dtype='float32'))
 
 
+def create_variable(name, shape):
+    variable = tf.Variable(tf.truncated_normal(shape, stddev=0.01), name=name)
+    return variable
+
+
 with tf.Session() as sess:
+
     # Create ODL data structures
     size = 128
     space = odl.uniform_discr([-64, -64], [64, 64], [size, size],
@@ -59,31 +65,32 @@ with tf.Session() as sess:
         x_true_arr[i] = np.asarray(phantom)[..., None]
         y_arr[i] = np.asarray(noisy_data)[..., None]
 
-    x = tf.constant(x_arr, name="x")
-    x_true = tf.constant(x_true_arr, name="x_true")
-    y = tf.constant(y_arr, name='y')
+    x_0 = tf.placeholder(tf.float32, shape=[None, space.shape[0], space.shape[1], 1], name="x_0")
+    x_true = tf.placeholder(tf.float32, shape=[None, space.shape[0], space.shape[1], 1], name="x_true")
+    y = tf.placeholder(tf.float32, shape=[None, ray_transform.range.shape[0], ray_transform.range.shape[1], 1], name="y")
 
-    s = tf.Variable(tf.constant(np.zeros([n_data, size, size, 5]), dtype='float32'))
+    s = tf.fill([tf.shape(x_0)[0], space.shape[0], space.shape[1], 5], np.float32(0.0), name="s")
 
     # Create constant right hand side
 
-    w1 = tf.Variable(tf.truncated_normal([3, 3, 7, 32], stddev=0.01))
+    w1 = create_variable('w1', shape=[3, 3, 7, 32])
     b1 = var(np.ones(32) * 0.1)
 
-    w2 = tf.Variable(tf.truncated_normal([3, 3, 32, 32], stddev=0.01))
+    w2 = create_variable('w2', shape=[3, 3, 32, 32])
     b2 = var(np.ones(32) * 0.1)
 
-    w3 = tf.Variable(tf.truncated_normal([3, 3, 32, 6], stddev=0.01))
+    w3 = create_variable('w3', shape=[3, 3, 32, 6])
     b3 = var(np.random.randn(6) * 0.001)
 
     n_iter = 5
 
+    x = x_0
     loss = 0
     for i in range(n_iter):
         # Reshape for ODL
         gradx = odl_op_layer_adjoint(odl_op_layer(x) - y)
 
-        dx = tf.concat([x, gradx, s], axis=3)
+        dx = tf.concat([x, gradx, s], axis=-1)
 
         dx = tf.nn.relu(conv2d(dx, w1) + b1)
         dx = tf.nn.relu(conv2d(dx, w2) + b2)
@@ -107,12 +114,18 @@ with tf.Session() as sess:
     # Solve with an ODL callback to see what happens
     callback = odl.solvers.CallbackShow()
 
-    for i in range(10000):
-        if i < 100:
-            sess.run(optimizer, feed_dict={learning_rate: 0.001})
-        else:
-            sess.run(optimizer, feed_dict={learning_rate: 0.001})
+    for i in range(1000):
+        _, loss_training = sess.run([optimizer, loss],
+                                  feed_dict={learning_rate: 0.001,
+                                             x_0: x_arr[1:],
+                                             x_true: x_true_arr[1:],
+                                             y: y_arr[1:]})
 
-        if i % 10 == 0:
-            callback(space.element(x[0].eval()))
-            print(loss.eval())
+        x_value, loss_value = sess.run([x, loss],
+                       feed_dict={learning_rate: 0.001,
+                                  x_0: x_arr[0:1],
+                                  x_true: x_true_arr[0:1],
+                                  y: y_arr[0:1]})
+
+        print('iter={}, training loss={}, validation loss={}'.format(i, loss_training, loss_value))
+        callback(space.element(x_value[0]))
