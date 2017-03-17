2deeaebcac9f8ff55887d8da5ecf508bea0e0464
Jonas Adler <jonasadl@kth.se>
2017-03-17 14:14:51 +0100

    ENH: Add name and name scopes to tf space

--- odl/contrib/tensorflow.py
+++ odl/contrib/tensorflow.py
@@ -173,27 +173,60 @@ def as_tensorflow_layer(odl_op, name='ODLOperator', differentiable=True):
 
 class TensorflowSpace(odl.LinearSpace):
     """A space of tensorflow Tensors."""
-    def __init__(self, shape):
+    def __init__(self, shape, name='ODLTensorflowSpace'):
         odl.LinearSpace.__init__(self, odl.RealNumbers())
         self.shape = tuple(tf.Dimension(si) if not isinstance(si, tf.Dimension) else si for si in shape)
         self.init_shape = tuple(si if si.value is not None else tf.Dimension(1) for si in self.shape)
+        self.name = name
 
     def _lincomb(self, a, x1, b, x2, out):
-        out.data = a * x1.data + b * x2.data
+        with tf.name_scope('{}_lincomb'.format(self.name)):
+            if x1 is x2:
+                # x1 is aligned with x2 -> out = (a+b)*x1
+                out.data = (a + b) * x1.data
+            elif out is x1 and out is x2:
+                # All the vectors are aligned -> out = (a+b)*out
+                if (a + b) != 1:
+                    out.data *= (a + b)
+            elif out is x1:
+                # out is aligned with x1 -> out = a*out + b*x2
+                out.data = a * out.data + b * x2.data
+            elif out is x2:
+                # out is aligned with x2 -> out = a*x1 + b*out
+                out.data = a * x1.data + b * out.data
+            else:
+                # We have exhausted all alignment options, so x1 != x2 != out
+                # We now optimize for various values of a and b
+                if b == 0:
+                    if a == 0:  # Zero assignment -> out = 0
+                        out.data *= 0
+                    else:  # Scaled copy -> out = a*x1
+                        out.data = a * x1.data
+                else:
+                    if a == 0:  # Scaled copy -> out = b*x2
+                        out.data = b * x2.data
+                    elif a == 1:  # No scaling in x1 -> out = x1 + b*x2
+                        out.data = x1.data + b * x2.data
+                    else:  # Generic case -> out = a*x1 + b*x2
+                        out.data = a * x1.data + b * x2.data
 
     def element(self, inp=None):
         if inp in self:
             return inp
         elif inp is None:
-            return TensorflowSpaceElement(self,
-                                          tf.zeros(self.init_shape,
-                                                   dtype=tf.float32))
+            return self.zero()
         else:
             return TensorflowSpaceElement(self, inp)
 
+    def zero(self):
+        with tf.name_scope('{}_zero'.format(self.name)):
+            return self.element(tf.zeros(self.init_shape,
+                                         dtype=tf.float32))
+        
     def one(self):
-        return self.element(tf.ones(self.init_shape,
-                                    dtype=tf.float32))
+        with tf.name_scope('{}_one'.format(self.name)):
+            return self.element(tf.ones(self.init_shape,
+                                        dtype=tf.float32))
 
     def __eq__(self, other):
         return isinstance(other, TensorflowSpace) and other.shape == self.shape
