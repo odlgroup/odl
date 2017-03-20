e3964d79ac88335c6552ca2f4b2f1536805a27e1
Holger Kohr <h.kohr@cwi.nl>
2017-03-20 18:28:43 +0100

    MAINT: move code from fn_ops to tensor_ops

--- odl/__init__.py
+++ odl/__init__.py
@@ -34,15 +34,16 @@ __all__ += space.__all__
 from .discr import *
 __all__ += discr.__all__
 
-from . import diagnostics
-from . import solvers
-from . import trafos
-from . import tomo
-from . import util
-from . import phantom
-from . import deform
-from . import ufunc_ops
 from . import contrib
+from . import datasets
+from . import deform
+from . import diagnostics
+from . import phantom
+from . import solvers
+from . import tomo
+from . import trafos
+from . import ufunc_ops
+from . import util
 
 from .util import test
 __all__ += ('test',)
