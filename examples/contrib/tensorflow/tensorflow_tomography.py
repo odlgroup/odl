f3d19c795a9707f336c78cbf97cc8ff66d1c81d6
Jonas Adler <jonasadl@kth.se>
2017-04-13 18:53:37 +0200

    MAINT: Cleanup tensorflow examples

--- examples/contrib/tensorflow/tensorflow_tomography.py
+++ examples/contrib/tensorflow/tensorflow_tomography.py
@@ -5,35 +5,41 @@ import numpy as np
 import odl
 
 
-with tf.Session() as sess:
-    # Create ODL data structures
-    space = odl.uniform_discr([-64, -64], [64, 64], [128, 128],
-                              dtype='float32')
-    geometry = odl.tomo.parallel_beam_geometry(space)
-    ray_transform = odl.tomo.RayTransform(space, geometry)
-    phantom = odl.phantom.shepp_logan(space, True)
-    data = ray_transform(phantom)
+sess = tf.InteractiveSession()
 
-    # Create tensorflow layer from odl operator
-    odl_op_layer = odl.as_tensorflow_layer(ray_transform, 'RayTransform')
-    x = tf.Variable(tf.constant(0.0, shape=space.shape), name="x")
+# Create ODL data structures
+space = odl.uniform_discr([-64, -64], [64, 64], [128, 128],
+                          dtype='float32')
+geometry = odl.tomo.parallel_beam_geometry(space)
+ray_transform = odl.tomo.RayTransform(space, geometry)
+phantom = odl.phantom.shepp_logan(space, True)
+data = ray_transform(phantom)
 
-    # Create constant right hand side
-    y = tf.constant(np.asarray(data))
+# Create tensorflow layer from odl operator
+odl_op_layer = odl.contrib.tensorflow.as_tensorflow_layer(
+        ray_transform, 'RayTransform')
+x = tf.Variable(tf.constant(0.0, shape=space.shape), name="x")
 
-    # Define l2 loss function
-    loss = tf.nn.l2_loss(odl_op_layer(x) - y)
+# Create constant right hand side
+y = tf.constant(np.asarray(data))
 
-    # Train using the adam optimizer
-    optimizer = tf.train.AdamOptimizer(1e-1).minimize(loss)
+# Add empty axes for batch and channel
+x = x[None, ..., None]
+y = y[None, ..., None]
 
-    # Initialize all TF variables
-    tf.global_variables_initializer().run()
+# Define l2 loss function
+loss = tf.nn.l2_loss(odl_op_layer(x) - y)
 
-    # Solve with an ODL callback to see what happens
-    callback = odl.solvers.CallbackShow()
+# Train using the adam optimizer
+optimizer = tf.train.AdamOptimizer(1e-1).minimize(loss)
 
-    for i in range(100):
-        sess.run(optimizer)
+# Initialize all TF variables
+tf.global_variables_initializer().run()
 
-        callback(space.element(x.eval()))
+# Solve with an ODL callback to see what happens
+callback = odl.solvers.CallbackShow()
+
+for i in range(100):
+    sess.run(optimizer)
+
+    callback(space.element(x.eval()))
