c9ef813ef37b1008801508c3e16f920922cf9100
Jonas Adler <jonasadl@kth.se>
2017-04-13 18:53:37 +0200

    MAINT: Cleanup tensorflow examples

--- examples/contrib/tensorflow/tensorflow_layer_matrix.py
+++ examples/contrib/tensorflow/tensorflow_layer_matrix.py
@@ -4,29 +4,35 @@ import tensorflow as tf
 import numpy as np
 import odl
 
+sess = tf.InteractiveSession()
 
-with tf.Session() as sess:
-    tf.global_variables_initializer().run()
+# Define ODL operator
+matrix = np.array([[1, 2],
+                   [0, 0],
+                   [0, 1]], dtype='float32')
+odl_op = odl.MatrixOperator(matrix)
 
-    matrix = np.array([[1, 2],
-                       [0, 0],
-                       [0, 1]], dtype='float32')
-    odl_op = odl.MatrixOperator(matrix)
+# Define evaluation points
+x = [1., 2.]
+z = [1., 2., 3.]
 
-    x = tf.constant([1., 2.])
-    z = tf.constant([1., 2., 3.])
+# Add empty axes for batch and channel
+x_tf = tf.constant(x)[None, ..., None]
+z_tf = tf.constant(z)[None, ..., None]
 
-    odl_op_layer = odl.as_tensorflow_layer(odl_op, 'MatrixOperator')
-    y = odl_op_layer(x)
+# Create tensorflow layer from odl operator
+odl_op_layer = odl.contrib.tensorflow.as_tensorflow_layer(
+        odl_op, 'MatrixOperator')
+y_tf = odl_op_layer(x_tf)
 
-    # Evaluate using tensorflow
-    print(y.eval())
+# Evaluate using tensorflow
+print(y_tf.eval().ravel())
 
-    # Compare result with pure ODL
-    print(odl_op(x.eval()))
+# Compare result with pure ODL
+print(odl_op(x))
 
-    # Evaluate the adjoint of the derivative, called gradient in tensorflow
-    print(tf.gradients(y, [x], z)[0].eval())
+# Evaluate the adjoint of the derivative, called gradient in tensorflow
+print(tf.gradients(y_tf, [x_tf], z_tf)[0].eval().ravel())
 
-    # Compare result with pure ODL
-    print(odl_op.derivative(x.eval()).adjoint(z.eval()))
+# Compare result with pure ODL
+print(odl_op.derivative(x).adjoint(z))
--- examples/contrib/tensorflow/tensorflow_layer_ray_transform.py
+++ examples/contrib/tensorflow/tensorflow_layer_ray_transform.py
@@ -4,29 +4,41 @@ import tensorflow as tf
 import numpy as np
 import odl
 
+sess = tf.InteractiveSession()
 
-with tf.Session() as sess:
-    tf.global_variables_initializer().run()
+tf.global_variables_initializer().run()
 
-    space = odl.uniform_discr([-64, -64], [64, 64], [128, 128],
-                              dtype='float32')
-    geometry = odl.tomo.parallel_beam_geometry(space)
-    ray_transform = odl.tomo.RayTransform(space, geometry)
+space = odl.uniform_discr([-64, -64], [64, 64], [128, 128],
+                          dtype='float32')
+geometry = odl.tomo.parallel_beam_geometry(space)
+ray_transform = odl.tomo.RayTransform(space, geometry)
 
-    x = tf.constant(np.asarray(ray_transform.domain.one()))
-    z = tf.constant(np.asarray(ray_transform.range.one()))
+x = tf.constant(np.asarray(ray_transform.domain.one()))
+z = tf.constant(np.asarray(ray_transform.range.one()))
 
-    odl_op_layer = odl.as_tensorflow_layer(ray_transform, 'RayTransform')
-    y = odl_op_layer(x)
+# Create tensorflow layer from odl operator
+odl_op_layer = odl.contrib.tensorflow.as_tensorflow_layer(
+        ray_transform, 'RayTransform')
 
-    # Evaluate using tensorflow
-    print(y.eval())
 
-    # Compare result with pure ODL
-    print(ray_transform(x.eval()))
+# Add empty axes for batch and channel
+x = x[None, ..., None]
+z = z[None, ..., None]
 
-    # Evaluate the adjoint of the derivative, called gradient in tensorflow
-    print(tf.gradients(y, [x], z)[0].eval())
+# Lazily apply operator in tensorflow
+y = odl_op_layer(x)
 
-    # Compare result with pure ODL
-    print(ray_transform.derivative(x.eval()).adjoint(z.eval()))
+# Evaluate using tensorflow
+print(y.eval())
+
+# Compare result with pure ODL
+print(ray_transform(x.eval()))
+
+# Evaluate the adjoint of the derivative, called gradient in tensorflow
+# We need to scale by cell size to get correct value since the derivative
+# in tensorflow uses unweighted spaces.
+scale = ray_transform.range.cell_volume / ray_transform.domain.cell_volume
+print(tf.gradients(y, [x], z)[0].eval() * scale)
+
+# Compare result with pure ODL
+print(ray_transform.derivative(x.eval()).adjoint(z.eval()))
