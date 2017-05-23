fcadfdb2ae372d2eb799f8ceed5d86dfecd1c0ae
Jonas Adler <jonasadl@kth.se>
2017-05-23 19:06:55 +0200

    STYLE: Minor improvement to simple_operator example

--- examples/operator/simple_operator.py
+++ examples/operator/simple_operator.py
@@ -4,17 +4,17 @@ import odl
 
 
 class AddOp(odl.Operator):
-    def __init__(self, size, add_this):
-        odl.Operator.__init__(self, domain=odl.rn(size), range=odl.rn(size))
-        self.value = add_this
+    def __init__(self, space, add_this):
+        odl.Operator.__init__(self, domain=space, range=space)
+        self.add_this = add_this
 
     def _call(self, x, out):
-        out[:] = x.data + self.value
+        return x + self.add_this
 
-size = 3
-rn = odl.rn(size)
-x = rn.element([1, 2, 3])
 
-op = AddOp(size, add_this=10)
+r3 = odl.rn(3)
+x = r3.element([1, 2, 3])
+
+op = AddOp(r3, add_this=10)
 
 print(op(x))
