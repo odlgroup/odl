3b33aa42f2a680ccc6f37d96e84afa5e59d21ce7
Holger Kohr <h.kohr@cwi.nl>
2017-08-17 11:19:08 +0200

    MAINT: re-organize top-level __init__.py a bit

--- odl/__init__.py
+++ odl/__init__.py
@@ -15,27 +15,38 @@ restricted to) inverse problems.
 from __future__ import absolute_import
 
 __version__ = '0.6.1.dev0'
-__all__ = ('diagnostics', 'discr', 'operator', 'set', 'space', 'solvers',
-           'tomo', 'trafos', 'util', 'phantom', 'deform', 'ufunc_ops',
-           'datasets', 'contrib')
+__all__ = ('set',
+           'space',
+           'operator',
+           'discr',
+           'contrib',
+           'deform',
+           'diagnostics',
+           'phantom',
+           'solvers',
+           'tomo',
+           'trafos',
+           'ufunc_ops',
+           'util',
+           )
 
-# Propagate names defined in __all__ of all submodules into the top-level
-# module
+# Propagate names defined in` __all__` of all "core" subpackages into
+# the top-level namespace
 from .set import *
 __all__ += set.__all__
-
-# operator must come before space because fspace imports Operator
-from .operator import *
-__all__ += operator.__all__
 
 from .space import *
 __all__ += space.__all__
 
+from .operator import *
+__all__ += operator.__all__
+
 from .discr import *
 __all__ += discr.__all__
 
+# More "advanced" subpackages keep their namespaces separate from top-level,
+# we only import the modules themselves
 from . import contrib
-from . import datasets
 from . import deform
 from . import diagnostics
 from . import phantom
@@ -45,5 +56,6 @@ from . import trafos
 from . import ufunc_ops
 from . import util
 
+# Add `test` function to global namespace so users can run `odl.test()`
 from .util import test
 __all__ += ('test',)
