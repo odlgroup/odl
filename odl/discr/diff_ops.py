59418d233e5b18243b94fc5b93d7aa11543e187f
Gregory R. Lee <gregory.lee@cchmc.org>
2017-09-08 13:39:20 -0400

    FIX: In finite_diff make pad_const complex if f is complex

--- odl/discr/diff_ops.py
+++ odl/discr/diff_ops.py
@@ -802,7 +802,12 @@ def finite_diff(f, axis, dx=1.0, method='forward', out=None, **kwargs):
     if pad_mode not in _SUPPORTED_PAD_MODES:
         raise ValueError('`pad_mode` {} not understood'
                          ''.format(pad_mode))
-    pad_const = float(kwargs.pop('pad_const', 0))
+
+    pad_const = kwargs.pop('pad_const', 0)
+    if np.iscomplexobj(f):
+        pad_const = complex(pad_const)
+    else:
+        pad_const = float(pad_const)
 
     if out is None:
         out = np.empty_like(f_arr)
