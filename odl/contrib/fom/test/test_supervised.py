b37db4482c5cfc19d560a344ea2760c024025b52
Barbara Gris <gris@kth.se>
2017-10-10 11:49:21 +0200

    BUG : remove absolute value in mean_value_difference

--- odl/contrib/fom/test/test_supervised.py
+++ odl/contrib/fom/test/test_supervised.py
@@ -183,5 +183,16 @@ def test_ssim(space):
             assert pytest.approx(result1) == result2
 
 
+def test_mean_value_difference_sign():
+    space = odl.uniform_discr(0, 1, 10)
+    I0 = space.one()
+    I1 = -I0.copy()
+    assert np.abs(odl.contrib.fom.mean_value_difference(I0, I1)) > 0.1
+    assert np.abs(odl.contrib.fom.mean_value_difference(I0, I1,
+                  normalized=True)) > 0.1
+
+
+
+
 if __name__ == '__main__':
     odl.util.test_file(__file__)
