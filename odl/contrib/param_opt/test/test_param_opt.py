3b8e038666b087266f905a961bbdf688c3e3a37b
Sebastian Banert <banert@kth.se>
2017-10-10 17:07:44 +0200

    TST: Add a two-parameter test for finding optimal reconstruction parameters.

--- odl/contrib/param_opt/test/test_param_opt.py
+++ odl/contrib/param_opt/test/test_param_opt.py
@@ -1,3 +1,13 @@
+# Copyright 2014-2017 The ODL contributors
+#
+# This file is part of ODL.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at https://mozilla.org/MPL/2.0/.
+
+"""Test for parameter optimization."""
+
 import pytest
 import odl
 import odl.contrib.fom
@@ -27,6 +37,30 @@ def test_optimal_parameters_one_parameter(space, fom):
     result = odl.contrib.param_opt.optimal_parameters(reconstruction, fom,
                                                       phantoms, data, 1)
     assert result == pytest.approx(0, abs=1e-4)
+
+
+def test_optimal_parameters_two_parameters(space, fom):
+    """Tests if optimal_parameters works for some simple examples."""
+#    fom = odl.contrib.fom.mean_squared_error
+    mynoise = odl.phantom.white_noise(space)
+    phantoms = [mynoise]
+    data = [mynoise]
+
+    def reconstruction1(data, params):
+        """Perturbs the data by adding the sum of squares of params to it."""
+        return data + sum([param ** 2 for param in params])
+
+    result1 = odl.contrib.param_opt.optimal_parameters(reconstruction1, fom,
+                                                       phantoms, data, [1, 2])
+    assert list(result1) == pytest.approx([0, 0], abs=1e-4)
+
+    def reconstruction2(data, params):
+        """Perturbs the data by adding the sum of params to it."""
+        return data + sum(params)
+
+    result2 = odl.contrib.param_opt.optimal_parameters(reconstruction2, fom,
+                                                       phantoms, data, [1, 2])
+    assert sum(result2) == pytest.approx(0, abs=1e-4)
 
 
 if __name__ == '__main__':
