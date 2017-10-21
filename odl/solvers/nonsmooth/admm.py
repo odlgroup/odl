01ec77b4e8b3f19354ff2b9a8f3023018d7678c8
Holger Kohr <h.kohr@cwi.nl>
2017-10-21 21:45:34 +0200

    MAINT: add license header to admm.py and fix unicode issue

--- odl/solvers/nonsmooth/admm.py
+++ odl/solvers/nonsmooth/admm.py
@@ -1,3 +1,12 @@
+# coding: utf-8
+# Copyright 2014-2017 The ODL contributors
+#
+# This file is part of ODL.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at https://mozilla.org/MPL/2.0/.
+
 """Alternating Direction method of Multipliers (ADMM) method variants."""
 
 from __future__ import division
