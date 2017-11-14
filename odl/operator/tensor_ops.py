TST/BUG: update of bug in gradient of GroupL1Norm

odl/operator/tensor_ops.py
@@ -340,14 +340,15 @@ class PointwiseNorm(PointwiseTensorFieldOperator):
         inner_vf = vf.copy()
 
         for gi in inner_vf:
+            gi *= np.abs(gi) ** (self.exponent - 2)
             if self.exponent >= 2:
-                tmp = vf_pwnorm_fac * np.abs(gi) ** (2 - self.exponent)
                 # Any component that is zero is not divided with
-                nz = (tmp.asarray() != 0)
-                gi[nz] /= tmp[nz]
-
+                nz = (vf_pwnorm_fac.asarray() != 0)
+                gi[nz] /= vf_pwnorm_fac[nz]
             else:
-                gi *= np.abs(gi) ** (self.exponent - 2) / vf_pwnorm_fac
+                # For exponents < 2, there will be a singularity if any
+                # component is zero
+                gi /= vf_pwnorm_fac
 
         return PointwiseInner(self.domain, inner_vf, weighting=self.weights)
 

odl/test/operator/tensor_ops_test.py
@@ -224,7 +224,7 @@ def test_pointwise_norm_gradient_real(exponent):
     direction = vfspace.element(test_direction)
     func_pwnorm = pwnorm.derivative(point)
 
-    assert not any(np.isnan(func_pwnorm(direction)))
+    assert not np.isnan(func_pwnorm(direction)).asarray().any()
 
     # 3d
     fspace = odl.uniform_discr([0, 0], [1, 1], (2, 2))
@@ -248,7 +248,7 @@ def test_pointwise_norm_gradient_real(exponent):
     direction = vfspace.element(test_direction)
     func_pwnorm = pwnorm.derivative(point)
 
-    assert not any(np.isnan(func_pwnorm(direction)))
+    assert not np.isnan(func_pwnorm(direction)).asarray().any()
 
 
 # ---- PointwiseInner ----