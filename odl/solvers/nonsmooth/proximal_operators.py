82a45863a5a6bc16d209c5da6b29d1c6f873e056
Sebastian Banert <banert@kth.se>
2017-11-25 16:43:56 +0100

    MAINT: Fix formatting of operations.

--- odl/solvers/nonsmooth/proximal_operators.py
+++ odl/solvers/nonsmooth/proximal_operators.py
@@ -159,9 +159,8 @@ def proximal_convex_conj(prox_factory):
 
         mult_inner = MultiplyOperator(1.0 / sigma, domain=space, range=space)
         mult_outer = MultiplyOperator(sigma, domain=space, range=space)
-        result = IdentityOperator(space) \
-            - OperatorComp(OperatorComp(
-                mult_outer, prox_factory(1.0 / sigma)), mult_inner)
+        result = IdentityOperator(space) - OperatorComp(OperatorComp(
+            mult_outer, prox_factory(1.0 / sigma)), mult_inner)
         return result
 
     return convex_conj_prox_factory
--- odl/test/solvers/functional/default_functionals_test.py
+++ odl/test/solvers/functional/default_functionals_test.py
@@ -555,7 +555,8 @@ def test_weighted_proximal_L2_norm_squared(space):
 
     # Check if the subdifferential inequalities are satisfied.
     # p = prox_{sigma * f}(x) iff (x - p)/sigma = grad f(p)
-    assert all_almost_equal(func.gradient(p1), space.divide(x - p1, sigma))
+    assert all_almost_equal(func.gradient(p1),
+                            (x - p1) / sigma)
 
 
 def test_weighted_proximal_L1_norm(space):
