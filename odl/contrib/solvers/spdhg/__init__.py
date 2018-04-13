955e25985db1b6ff15cc39b54e99a7d4a4ef9480
Matthias J. Ehrhardt <m.j.ehrhardt@damtp.cam.ac.uk>
2018-04-13 11:35:25 +0100

    STY: improve indent

--- odl/contrib/solvers/spdhg/__init__.py
+++ odl/contrib/solvers/spdhg/__init__.py
@@ -8,14 +8,14 @@
 
 """Contributed code for the stochastic PDHG.
 
-    [CERS2017] A. Chambolle, M. J. Ehrhardt, P. Richtarik and C.-B. Schoenlieb,
-    *Stochastic Primal-Dual Hybrid Gradient Algorithm with Arbitrary Sampling
-    and Imaging Applications*. ArXiv: http://arxiv.org/abs/1706.04957 (2017).
+[CERS2017] A. Chambolle, M. J. Ehrhardt, P. Richtarik and C.-B. Schoenlieb,
+*Stochastic Primal-Dual Hybrid Gradient Algorithm with Arbitrary Sampling
+and Imaging Applications*. ArXiv: http://arxiv.org/abs/1706.04957 (2017).
 
-    [E+2017] M. J. Ehrhardt, P. J. Markiewicz, P. Richtarik, J. Schott,
-    A. Chambolle and C.-B. Schoenlieb, *Faster PET reconstruction with a
-    stochastic primal-dual hybrid gradient method*. Wavelets and Sparsity XVII,
-    58 (2017) http://doi.org/10.1117/12.2272946.
+[E+2017] M. J. Ehrhardt, P. J. Markiewicz, P. Richtarik, J. Schott,
+A. Chambolle and C.-B. Schoenlieb, *Faster PET reconstruction with a
+stochastic primal-dual hybrid gradient method*. Wavelets and Sparsity XVII,
+58 (2017) http://doi.org/10.1117/12.2272946.
 """
 
 from __future__ import absolute_import
