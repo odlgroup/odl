7eda18c8a97b98b56152893957100b0a630f126d
Matthias J. Ehrhardt <m.j.ehrhardt@damtp.cam.ac.uk>
2018-04-13 14:35:09 +0100

    import division from future for py2

--- odl/contrib/solvers/spdhg/stochastic_primal_dual_hybrid_gradient.py
+++ odl/contrib/solvers/spdhg/stochastic_primal_dual_hybrid_gradient.py
@@ -8,7 +8,7 @@
 
 """Stochastic Primal-Dual Hybrid Gradient (SPDHG) algorithms"""
 
-from __future__ import print_function
+from __future__ import print_function, division
 import numpy as np
 import odl
 
