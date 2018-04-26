397fb3ea7177e1e31004324a8014cd0d64dad958
Hector Andrade Loarca <arsenal997@gmail.com>
2018-04-26 20:03:09 +0200

    CONTRIB: add shearlab.jl submodule

---
+++ odl/contrib/shearlab/shearlab_operator.py
@@ -0,0 +1,324 @@
+# Copyright 2014-2018 The ODL contributors
+#
+# This file is part of ODL.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at https://mozilla.org/MPL/2.0/.
+
+"""ODL integration with shearlab."""
+
+import odl
+import numpy as np
+from threading import Lock
+
+# Library for the shearlab library
+
+import julia
+import matplotlib.pyplot as plt
+from numpy import ceil
+from numpy.fft import fft2, ifft2, fftshift, ifftshift
+
+__all__ = ('ShearlabOperator',)
+
+
+class ShearlabOperator(odl.Operator):
+
+    """Shearlet transform using Shearlab.jl as backend.
+    This is the non-compact shearlet transform implemented using the Fourier
+    transform.
+    """
+
+    def __init__(self, space, num_scales):
+        """Initialize a new instance.
+
+        Parameters
+        ----------
+        space : `DiscreteLp`
+            The space on which the shearlet transform should act. Must be
+            two-dimensional.
+        num_scales : nonnegative `int`
+            The number of scales for the shearlet transform, higher numbers
+            mean better edge resolution but more computational burden.
+
+        Examples
+        --------
+        Create a 2d-shearlet transform:
+        >>> space = odl.uniform_discr([-1, -1], [1, 1], [128, 128])
+        >>> shearlet_transform = ShearlabOperator(space, num_scales=2)
+        """
+        self.shearlet_system = getshearletsystem2D(
+            space.shape[0], space.shape[1], num_scales)
+        range = space ** self.shearlet_system.nShearlets
+        self.mutex = Lock()
+        super(ShearlabOperator, self).__init__(space, range, True)
+
+    def _call(self, x):
+        """``self(x)``."""
+        with self.mutex:
+            result = sheardec2D(x, self.shearlet_system)
+            return np.moveaxis(result, -1, 0)
+
+    @property
+    def adjoint(self):
+        """The adjoint operator."""
+        op = self
+
+        class ShearlabOperatorAdjoint(odl.Operator):
+
+            """Adjoint of the shearlet transform.
+
+            See Also
+            --------
+            odl.contrib.shearlab.ShearlabOperator
+            """
+
+            def __init__(self):
+                """Initialize a new instance.
+                Parameters
+                ----------
+                op : `ShearlabOperator`
+                    The operator which this should be the adjoint of.
+                """
+                super(ShearlabOperatorAdjoint, self).__init__(
+                    op.range, op.domain, True)
+
+            def _call(self, x):
+                """``self(x)``."""
+                with op.mutex:
+                    x = np.moveaxis(x, 0, -1)
+                    return sheardecadjoint2D(x, op.shearlet_system)
+
+            @property
+            def adjoint(self):
+                """The adjoint operator."""
+                return op
+
+            @property
+            def inverse(self):
+                """The inverse operator."""
+                op = self
+
+                class ShearlabOperatorAdjointInverse(odl.Operator):
+
+                    """
+                    Adjoint of the inverse/Inverse of the adjoint
+                    of shearlet transform.
+
+                    See Also
+                    --------
+                    odl.contrib.shearlab.ShearlabOperator
+                    """
+
+                    def __init__(self):
+                        """Initialize a new instance.
+                        Parameters
+                        ----------
+                        op : `ShearlabOperator`
+                              The operator which this should be the
+                              inverse of the adjoint of.
+                        """
+                        super(ShearlabOperatorAdjointInverse, self).__init__(
+                            op.domain, op.range, True)
+
+                    def _call(self, x):
+                        """``self(x)``."""
+                        with op.mutex:
+                            result = shearrecadjoint2D(x, op.shearlet_system)
+                            return np.moveaxis(result, -1, 0)
+
+                    @property
+                    def adjoint(self):
+                        """The adjoint operator."""
+                        return self.op.inverse
+
+                    @property
+                    def inverse(self):
+                        """The inverse operator."""
+                        return self.op.adjoint
+
+        return ShearlabOperatorAdjoint()
+
+    @property
+    def inverse(self):
+        """The inverse operator."""
+        op = self
+
+        class ShearlabOperatorInverse(odl.Operator):
+
+            """Inverse of the shearlet transform.
+
+            See Also
+            --------
+            odl.contrib.shearlab.ShearlabOperator
+            """
+
+            def __init__(self):
+                """Initialize a new instance.
+                Parameters
+                ----------
+                op : `ShearlabOperator`
+                    The operator which this should be the inverse of.
+                """
+                super(ShearlabOperatorInverse, self).__init__(
+                    op.range, op.domain, True)
+
+            def _call(self, x):
+                """``self(x)``."""
+                with op.mutex:
+                    x = np.moveaxis(x, 0, -1)
+                    return shearrec2D(x, op.shearlet_system)
+
+            @property
+            def adjoint(self):
+                """The adjoint operator."""
+                return ShearlabOperatorAdjointInverse()
+
+            @property
+            def inverse(self):
+                """The inverse operator."""
+                return op
+
+        return ShearlabOperatorInverse()
+
+
+# Python library for shearlab.jl
+
+def load_julia_with_Shearlab():
+    """Function to load Shearlab."""
+    # Importing base
+    j = julia.Julia()
+    j.eval('using Shearlab')
+    j.eval('using PyPlot')
+    j.eval('using Images')
+    return j
+
+
+j = load_julia_with_Shearlab()
+
+
+def load_image(name, n, m=None, gpu=None, square=None):
+    """Function to load images with certain size."""
+    if m is None:
+        m = n
+    if gpu is None:
+        gpu = 0
+    if square is None:
+        square = 0
+    command = ('Shearlab.load_image("{}", {}, {}, {}, {})'.format(name,
+               n, m, gpu, square))
+    return j.eval(command)
+
+
+def imageplot(f, str=None, sbpt=None):
+    """Plot an image generated by the library."""
+    # Function to plot images
+    if str is None:
+        str = ''
+    if sbpt is None:
+        sbpt = []
+    if sbpt != []:
+        plt.subplot(sbpt[0], sbpt[1], sbpt[2])
+    imgplot = plt.imshow(f, interpolation='nearest')
+    imgplot.set_cmap('gray')
+    plt.axis('off')
+    if str != '':
+        plt.title(str)
+
+
+class Shearletsystem2D:
+    """Class of shearlet system in 2D."""
+    def __init__(self, shearlets, size, shearLevels, full, nShearlets,
+                 shearletIdxs, dualFrameWeights, RMS, isComplex):
+        self.shearlets = shearlets
+        self.size = size
+        self.shearLevels = shearLevels
+        self.full = full
+        self.nShearlets = nShearlets
+        self.shearletIdxs = shearletIdxs
+        self.dualFrameWeights = dualFrameWeights
+        self.RMS = RMS
+        self.isComplex = isComplex
+
+
+def getshearletsystem2D(rows, cols, nScales, shearLevels=None,
+                        full=None,
+                        directionalFilter=None,
+                        quadratureMirrorFilter=None):
+    """Function to generate de 2D system."""
+    if shearLevels is None:
+        shearLevels = [float(ceil(i / 2)) for i in range(1, nScales + 1)]
+    if full is None:
+        full = 0
+    if directionalFilter is None:
+        directionalFilter = 'Shearlab.filt_gen("directional_shearlet")'
+    if quadratureMirrorFilter is None:
+        quadratureMirrorFilter = 'Shearlab.filt_gen("scaling_shearlet")'
+    j.eval('rows=' + str(rows))
+    j.eval('cols=' + str(cols))
+    j.eval('nScales=' + str(nScales))
+    j.eval('shearLevels=' + str(shearLevels))
+    j.eval('full=' + str(full))
+    j.eval('directionalFilter=' + directionalFilter)
+    j.eval('quadratureMirrorFilter=' + quadratureMirrorFilter)
+    j.eval('shearletsystem=Shearlab.getshearletsystem2D(rows, '
+           'cols, nScales, shearLevels, full, directionalFilter, '
+           'quadratureMirrorFilter) ')
+    shearlets = j.eval('shearletsystem.shearlets')
+    size = j.eval('shearletsystem.size')
+    shearLevels = j.eval('shearletsystem.shearLevels')
+    full = j.eval('shearletsystem.full')
+    nShearlets = j.eval('shearletsystem.nShearlets')
+    shearletIdxs = j.eval('shearletsystem.shearletIdxs')
+    dualFrameWeights = j.eval('shearletsystem.dualFrameWeights')
+    RMS = j.eval('shearletsystem.RMS')
+    isComplex = j.eval('shearletsystem.isComplex')
+    j.eval('shearletsystem = 0')
+    return Shearletsystem2D(shearlets, size, shearLevels, full, nShearlets,
+                            shearletIdxs, dualFrameWeights, RMS, isComplex)
+
+
+def sheardec2D(X, shearletsystem):
+    """Shearlet Decomposition function."""
+    coeffs = np.zeros(shearletsystem.shearlets.shape, dtype=complex)
+    Xfreq = fftshift(fft2(ifftshift(X)))
+    for i in range(shearletsystem.nShearlets):
+        coeffs[:, :, i] = fftshift(ifft2(ifftshift(Xfreq * np.conj(
+                                   shearletsystem.shearlets[:, :, i]))))
+    return coeffs.real
+
+
+def shearrec2D(coeffs, shearletsystem):
+    """Shearlet Recovery function."""
+    X = np.zeros(coeffs.shape[:2], dtype=complex)
+    for i in range(shearletsystem.nShearlets):
+        X = X + fftshift(fft2(
+            ifftshift(coeffs[:, :, i]))) * shearletsystem.shearlets[:, :, i]
+    return (fftshift(ifft2(ifftshift((
+            X / shearletsystem.dualFrameWeights))))).real
+
+
+def sheardecadjoint2D(coeffs, shearletsystem):
+    """Shearlet Decomposition adjoint function."""
+    X = np.zeros(coeffs.shape[:2], dtype=complex)
+    for i in range(shearletsystem.nShearlets):
+        X = X + fftshift(fft2(
+            ifftshift(coeffs[:, :, i]))) * np.conj(
+            shearletsystem.shearlets[:, :, i])
+    return (fftshift(ifft2(ifftshift(
+            X / shearletsystem.dualFrameWeights)))).real
+
+
+def shearrecadjoint2D(X, shearletsystem):
+    """Shearlet Recovery adjoint function."""
+    coeffs = np.zeros(shearletsystem.shearlets.shape, dtype=complex)
+    Xfreq = fftshift(fft2(ifftshift(X)))
+    for i in range(shearletsystem.nShearlets):
+        coeffs[:, :, i] = fftshift(ifft2(ifftshift(
+            Xfreq * shearletsystem.shearlets[:, :, i])))
+    return coeffs.real
+
+
+if __name__ == '__main__':
+    from odl.util.testutils import run_doctests
+    run_doctests()
---
+++ odl/contrib/shearlab/test/operator_test.py
@@ -0,0 +1,57 @@
+# Copyright 2014-2018 The ODL contributors
+#
+# This file is part of ODL.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at https://mozilla.org/MPL/2.0/.
+
+"""Unit tests for the ODL-shearlab integration."""
+
+import pytest
+import numpy as np
+import odl
+import odl.contrib.shearlab
+from odl.util.testutils import all_almost_equal, simple_fixture
+
+
+dtype = simple_fixture('dtype', ['float32', 'float64'])
+shape = simple_fixture('shape', [(64, 64), (128, 128)])
+
+
+def test_operator(dtype, shape):
+    """Test ShearlabOperator operator."""
+    rel = 100 * np.finfo('float32').resolution
+
+    space = odl.uniform_discr([-1, -1], [1, 1], shape, dtype=dtype)
+
+    op = odl.contrib.pyshearlab.ShearlabOperator(space, num_scales=2)
+
+    phantom = odl.phantom.shepp_logan(space, True)
+
+    # Test evaluation
+    y = op(phantom)
+
+    # <Ax, Ax> = <x, AtAx>
+    ax = op.adjoint(y)
+    assert phantom.inner(ax) == pytest.approx(y.inner(y), rel=rel)
+
+    # A^{-1} A x = x
+    rec = op.inverse(y)
+    assert all_almost_equal(op.inverse(y), phantom)
+
+    # <A^{-1}y, A^{-1}y> = <y, A^{-*}A^{-1}y>
+    recadj = op.inverse.adjoint(rec)
+    assert pytest.approx(rec.inner(rec), rel=rel) == y.inner(recadj)
+
+    # A^{-*}A^*y = y
+    adjinvadj = op.adjoint.inverse(op.adjoint(y))
+    assert all_almost_equal(adjinvadj, y, places=5)
+
+    # A^*A^{-*}x = x
+    adjadjinv = op.adjoint(op.adjoint.inverse(phantom))
+    assert all_almost_equal(adjadjinv, phantom, places=5)
+
+
+if __name__ == '__main__':
+    odl.util.test_file(__file__)
