cbf54efe727008d96e3b090921bd885cb88e9bc1
Sebastian Banert <banert@kth.se>
2018-06-12 23:20:30 +0200

    DOC: Avoid "overly specific" comment in doctest.

--- odl/operator/oputils.py
+++ odl/operator/oputils.py
@@ -154,7 +154,7 @@ def power_method_opnorm(op, xstart=None, maxiter=100, rtol=1e-05, atol=1e-08,
 
     Examples
     --------
-    Verify that the identity operator has norm 1 (within a precision of 0.001):
+    Verify that the identity operator has norm close to 1:
 
     >>> space = odl.uniform_discr(0, 1, 5)
     >>> id = odl.IdentityOperator(space)
