f09c923924ee00f844933ae0ec2260236a60a72c
aringh <aringh@kth.se>
2018-06-29 10:17:05 +0200

    TST: update test for parameter optimization.
    Reformulate the tests so that 0 is not the optimal parameter.

--- odl/contrib/param_opt/test/test_param_opt.py
+++ odl/contrib/param_opt/test/test_param_opt.py
@@ -9,6 +9,7 @@
 """Test for parameter optimization."""
 
 import pytest
+import numpy as np
 import odl
 import odl.contrib.fom
 import odl.contrib.param_opt
@@ -26,35 +27,38 @@ fom = simple_fixture('fom',
 
 def test_optimal_parameters_one_parameter(space, fom):
     """Tests if optimal_parameters works for some simple examples."""
-    mynoise = [odl.phantom.white_noise(space) for _ in range(2)]
-    phantoms = mynoise.copy()
-    data = mynoise.copy()
+    noise = [odl.phantom.white_noise(space) for _ in range(2)]
+    phantoms = noise.copy()
+    data = noise.copy()
 
     def reconstruction(data, lam):
         """Simple reconstruction operator.
 
         The optimal reconstruction parameter in any fom is lam=0.
         """
-        return data + lam
+        return 1.23 * data / lam
 
     result = odl.contrib.param_opt.optimal_parameters(reconstruction, fom,
                                                       phantoms, data, 1)
-    assert result == pytest.approx(0, abs=1e-4)
+    assert result == pytest.approx(1.23, abs=1e-4)
 
 
 def test_optimal_parameters_two_parameters(space, fom):
     """Tests if optimal_parameters works for some simple examples."""
-    mynoise = [odl.phantom.white_noise(space) for _ in range(2)]
-    phantoms = mynoise.copy()
-    data = mynoise.copy()
+    noise = [odl.phantom.white_noise(space) for _ in range(2)]
+    # Normalize to reduce test fails due to randomness
+    noise = [noise_elem / noise_elem.norm() for noise_elem in noise]
+    phantoms = noise.copy()
+    data = noise.copy()
 
     def reconstruction1(data, params):
         """Perturbs the data by adding the sum of squares of params to it."""
-        return data + sum([param ** 2 for param in params])
+        return (1 - params[1]) * data / np.abs(params[0])
 
     result1 = odl.contrib.param_opt.optimal_parameters(reconstruction1, fom,
                                                        phantoms, data, [1, 2])
-    assert list(result1) == pytest.approx([0, 0], abs=1e-4)
+    print(result1)
+    assert 1 - result1[1] == pytest.approx(result1[0], abs=1e-4)
 
     def reconstruction2(data, params):
         """Perturbs the data by adding the sum of params to it."""
