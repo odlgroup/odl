6fe1ca82228a77b8d98bfc8bddcfcbad4db1848a
Holger Kohr <ho.kohr@zoho.com>
2019-02-24 13:35:02 +0100

    Fix simple_rn example

--- examples/space/simple_rn.py
+++ examples/space/simple_rn.py
@@ -5,7 +5,7 @@ Including some benchmarks with an optimized version.
 
 import numpy as np
 import odl
-from odl.space.base_tensors import TensorSpace, Tensor
+from odl.space.base_tensors import TensorSpace
 from odl.util.testutils import timer
 
 
@@ -13,55 +13,46 @@ class SimpleRn(TensorSpace):
     """The real space R^n, non-optimized implmentation."""
 
     def __init__(self, size):
-        super(SimpleRn, self).__init__(size, dtype=float)
+        super(SimpleRn, self).__init__(size, dtype='float64')
 
     def zero(self):
-        return self.element(np.zeros(self.size))
+        return np.zeros(self.size)
 
     def one(self):
-        return self.element(np.ones(self.size))
+        return np.ones(self.size)
 
     def _lincomb(self, a, x1, b, x2, out):
-        out.data[:] = a * x1.data + b * x2.data
+        out[:] = a * x1 + b * x2
 
     def _inner(self, x1, x2):
-        return float(np.vdot(x1.data, x2.data))
+        return float(np.vdot(x1, x2))
 
     def _multiply(self, x1, x2, out):
-        out.data[:] = x1.data * x2.data
+        out[:] = x1 * x2
 
     def _divide(self, x1, x2, out):
-        out.data[:] = x1.data / x2.data
+        out[:] = x1 / x2
+
+    def __contains__(self, other):
+        return (
+            isinstance(other, np.ndarray)
+            and other.shape == (self.size,)
+            and other.dtype == 'float64'
+        )
 
     def element(self, *args, **kwargs):
         if not args and not kwargs:
-            return self.element(np.empty(self.size))
+            return np.empty(self.size)
         if isinstance(args[0], np.ndarray):
             if args[0].shape == (self.size,):
-                return RnVector(self, args[0])
+                return np.asarray(args[0])
             else:
-                raise ValueError('input array {} is of shape {}, expected '
-                                 'shape ({},).'.format(args[0], args[0].shape,
-                                                       self.dim,))
+                raise ValueError(
+                    'input array has shape {}, expected shape ({},)'
+                    ''.format(args[0].shape, self.dim)
+                )
         else:
-            return self.element(np.array(
-                *args, **kwargs).astype(np.float64, copy=False))
-        return self.element(np.empty(self.dim, dtype=np.float64))
-
-
-class RnVector(Tensor):
-    def __init__(self, space, data):
-        super(RnVector, self).__init__(space)
-        self.data = data
-
-    def __getitem__(self, index):
-        return self.data.__getitem__(index)
-
-    def __setitem__(self, index, value):
-        return self.data.__setitem__(index, value)
-
-    def asarray(self, *args):
-        return self.data(*args)
+            return np.array(*args, **kwargs).astype('float64', copy=False)
 
 
 r5 = SimpleRn(5)
@@ -70,70 +61,69 @@ r5 = SimpleRn(5)
 # Do some tests to compare
 n = 10 ** 7
 iterations = 10
+cuda_supported = 'cuda' in odl.space.entry_points.tensor_space_impl_names()
 
 # Perform some benchmarks with rn
-opt_spc = odl.rn(n)
-simple_spc = SimpleRn(n)
+opt_space = odl.rn(n)
+simple_space = SimpleRn(n)
 
 x, y, z = np.random.rand(n), np.random.rand(n), np.random.rand(n)
-ox, oy, oz = (opt_spc.element(x.copy()), opt_spc.element(y.copy()),
-              opt_spc.element(z.copy()))
-sx, sy, sz = (simple_spc.element(x.copy()), simple_spc.element(y.copy()),
-              simple_spc.element(z.copy()))
-if 'cuda' in odl.space.entry_points.tensor_space_impl_names():
-    cu_spc = odl.rn(n, impl='cuda')
-    cx, cy, cz = (cu_spc.element(x.copy()), cu_spc.element(y.copy()),
-                  cu_spc.element(z.copy()))
+ox, oy, oz = (opt_space.element(a.copy()) for a in (x, y, z))
+sx, sy, sz = (simple_space.element(a.copy()) for a in (x, y, z))
+
+if cuda_supported:
+    cu_space = odl.rn(n, impl='cuda')
+    cx, cy, cz = (cu_space.element(a.copy()) for a in (x, y, z))
 
 print(" lincomb:")
 with timer("SimpleRn"):
     for _ in range(iterations):
-        simple_spc.lincomb(2.13, sx, 3.14, sy, out=sz)
+        simple_space.lincomb(2.13, sx, 3.14, sy, out=sz)
 print("result: {}".format(sz[1:5]))
 
 with timer("odl numpy"):
     for _ in range(iterations):
-        opt_spc.lincomb(2.13, ox, 3.14, oy, out=oz)
+        opt_space.lincomb(2.13, ox, 3.14, oy, out=oz)
 print("result: {}".format(oz[1:5]))
 
-if 'cuda' in odl.space.entry_points.tensor_space_impl_names():
+if cuda_supported:
     with timer("odl cuda"):
         for _ in range(iterations):
-            cu_spc.lincomb(2.13, cx, 3.14, cy, out=cz)
+            cu_space.lincomb(2.13, cx, 3.14, cy, out=cz)
     print("result: {}".format(cz[1:5]))
 
 
 print("\n Norm:")
 with timer("SimpleRn"):
     for _ in range(iterations):
-        result = sz.norm()
+        result = simple_space.norm(sz)
 print("result: {}".format(result))
 
 with timer("odl numpy"):
     for _ in range(iterations):
-        result = oz.norm()
+        result = opt_space.norm(oz)
 print("result: {}".format(result))
 
-if 'cuda' in odl.space.entry_points.tensor_space_impl_names():
+if cuda_supported:
     with timer("odl cuda"):
         for _ in range(iterations):
-            result = cz.norm()
+            result = cu_space.norm(cz)
     print("result: {}".format(result))
 
 
 print("\n Inner:")
 with timer("SimpleRn"):
     for _ in range(iterations):
-        result = sz.inner(sx)
+        result = simple_space.inner(sx, sz)
 print("result: {}".format(result))
 
 with timer("odl numpy"):
     for _ in range(iterations):
-        result = oz.inner(ox)
+        result = opt_space.inner(ox, oz)
 print("result: {}".format(result))
 
-if 'cuda' in odl.space.entry_points.tensor_space_impl_names():
+if cuda_supported:
     with timer("odl cuda"):
         for _ in range(iterations):
-            result = cz.inner(cx)
+            result = cu_space.inner(cx, cz)
     print("result: {}".format(result))
