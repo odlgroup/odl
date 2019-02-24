24fc0797bbe3ef12e4151966b372c38d3319a801
Holger Kohr <ho.kohr@zoho.com>
2019-02-24 15:56:22 +0100

    Fix Rosenbrock example

--- examples/solvers/rosenbrock_minimization.py
+++ examples/solvers/rosenbrock_minimization.py
@@ -1,20 +1,3 @@
-# Copyright 2014-2016 The ODL development group
-#
-# This file is part of ODL.
-#
-# ODL is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# ODL is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with ODL.  If not, see <http://www.gnu.org/licenses/>.
-
 """Minimize the Rosenbrock functional.
 
 This example shows how this can be done using a variety of solution methods.
@@ -23,51 +6,71 @@ This example shows how this can be done using a variety of solution methods.
 import odl
 from matplotlib import pyplot as plt
 
-# Create the solution space
 space = odl.rn(2)
-
-# Create objective functional
 f = odl.solvers.RosenbrockFunctional(space)
-
-# Define a line search method
 line_search = odl.solvers.BacktrackingLineSearch(f)
 
+# --- Steepest Descent --- #
 
-# Solve problem using steepest descent
-callback = odl.solvers.CallbackShowConvergence(f, logx=True, logy=True,
-                                               color='b')
+callback = odl.solvers.CallbackShowConvergence(
+    f, logx=True, logy=True, color='b'
+)
 x = space.zero()
-odl.solvers.steepest_descent(f, x, line_search=line_search,
-                             callback=callback)
-legend_artists = [callback.ax.collections[-1], ]
-legend_labels = ['SD', ]
+odl.solvers.steepest_descent(
+    f, x, line_search=line_search, callback=callback
+)
+legend_artists = [callback.ax.collections[-1]]
+legend_labels = ['SD']
 
-# Solve problem using nonlinear conjugate gradient
-callback = odl.solvers.CallbackShowConvergence(f, logx=True, logy=True,
-                                               color='g')
+# --- Nonlinear CG --- #
+
+callback = odl.solvers.CallbackShowConvergence(
+    f, logx=True, logy=True, color='g'
+)
 x = space.zero()
-odl.solvers.conjugate_gradient_nonlinear(f, x, line_search=line_search,
-                                         callback=callback)
+odl.solvers.conjugate_gradient_nonlinear(
+    f, x, line_search=line_search, callback=callback
+)
 legend_artists.append(callback.ax.collections[-1])
 legend_labels.append('CG')
 
-# Solve problem using bfgs
-callback = odl.solvers.CallbackShowConvergence(f, logx=True, logy=True,
-                                               color='r')
+# --- Broyden's Method --- #
+
+callback = odl.solvers.CallbackShowConvergence(
+    f, logx=True, logy=True, color='m'
+)
 x = space.zero()
-odl.solvers.bfgs_method(f, x, line_search=line_search,
-                        callback=callback)
+odl.solvers.broydens_method(
+    f, x, line_search=line_search, callback=callback
+)
+legend_artists.append(callback.ax.collections[-1])
+legend_labels.append('Broyden')
+
+# --- BFGS --- #
+
+callback = odl.solvers.CallbackShowConvergence(
+    f, logx=True, logy=True, color='r'
+)
+x = space.zero()
+odl.solvers.bfgs_method(
+    f, x, line_search=line_search, callback=callback
+)
 legend_artists.append(callback.ax.collections[-1])
 legend_labels.append('BFGS')
 
-# Solve problem using newtons method
-callback = odl.solvers.CallbackShowConvergence(f, logx=True, logy=True,
-                                               color='k')
+# --- Newton's Method --- #
+
+callback = odl.solvers.CallbackShowConvergence(
+    f, logx=True, logy=True, color='k'
+)
 x = space.zero()
-odl.solvers.newtons_method(f, x, line_search=line_search,
-                           callback=callback)
+odl.solvers.newtons_method(
+    f, x, line_search=line_search, callback=callback
+)
 legend_artists.append(callback.ax.collections[-1])
 legend_labels.append('Newton')
+
+# --- Add legend to plots and show it --- #
 
 plt.legend(legend_artists, legend_labels)
 plt.show()
