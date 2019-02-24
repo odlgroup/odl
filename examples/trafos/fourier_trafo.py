c3db9c39b2f6f1b45fb79a844bdf96a444c3b9e2
Holger Kohr <ho.kohr@zoho.com>
2019-02-24 12:50:53 +0100

    Fix fourier_trafo example

--- examples/trafos/fourier_trafo.py
+++ examples/trafos/fourier_trafo.py
@@ -13,18 +13,20 @@ ft_op = odl.trafos.FourierTransform(space)
 
 # Create a phantom and its Fourier transfrom and display them.
 phantom = odl.phantom.shepp_logan(space, modified=True)
-phantom.show(title='Shepp-Logan Phantom')
+space.show(phantom, title='Shepp-Logan Phantom')
 phantom_ft = ft_op(phantom)
-phantom_ft.show(title='Full Fourier Transform')
+ft_op.range.show(phantom_ft, title='Full Fourier Transform')
 
 # Calculate the inverse transform.
 phantom_ft_inv = ft_op.inverse(phantom_ft)
-phantom_ft_inv.show(title='Full Fourier Transform Inverted')
+space.show(phantom_ft_inv, title='Full Fourier Transform Inverted')
 
 # Calculate the FT only along the first axis.
 ft_op_axis0 = odl.trafos.FourierTransform(space, axes=0)
 phantom_ft_axis0 = ft_op_axis0(phantom)
-phantom_ft_axis0.show(title='Fourier transform Along Axis 0')
+ft_op_axis0.range.show(
+    phantom_ft_axis0, title='Fourier Transform Along Axis 0'
+)
 
 # If a real space is used, the Fourier transform can be calculated in the
 # "half-complex" mode. This means that along the last axis of the transform,
@@ -33,14 +35,17 @@ phantom_ft_axis0.show(title='Fourier transform Along Axis 0')
 real_space = space.real_space
 ft_op_halfc = odl.trafos.FourierTransform(real_space, halfcomplex=True)
 phantom_real = odl.phantom.shepp_logan(real_space, modified=True)
-phantom_real.show(title='Shepp-Logan Phantom, Real Version')
+space.show(phantom_real, title='Shepp-Logan Phantom, Real Version')
 phantom_real_ft = ft_op_halfc(phantom_real)
-phantom_real_ft.show(title='Half-complex Fourier Transform')
+ft_op_halfc.range.show(
+    phantom_real_ft, title='Half-complex Fourier Transform'
+)
 
 # If the space is real, the inverse also gives a real result.
 phantom_real_ft_inv = ft_op_halfc.inverse(phantom_real_ft)
-phantom_real_ft_inv.show(title='Half-complex Fourier Transform Inverted',
-                         force_show=True)
+space.show(
+    phantom_real_ft_inv, title='Half-complex Fourier Transform Inverted'
+)
 
 # The FT operator itself has no option of (zero-)padding, but it can be
 # composed with a `ResizingOperator` which does exactly that. Note that the
@@ -49,4 +54,6 @@ padding_op = odl.ResizingOperator(space, ran_shp=(768, 768))
 ft_op = odl.trafos.FourierTransform(padding_op.range)
 padded_ft_op = ft_op * padding_op
 phantom_ft_padded = padded_ft_op(phantom)
-phantom_ft_padded.show('Padded FT of the Phantom', force_show=True)
+padded_ft_op.range.show(
+    phantom_ft_padded, 'Padded FT of The Phantom', force_show=True
+)
