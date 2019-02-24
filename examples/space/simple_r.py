2647c326aee660437802af3ce3fc9cdf46c6e8b7
Holger Kohr <ho.kohr@zoho.com>
2019-02-24 13:34:56 +0100

    Fix simple_r example

--- examples/space/simple_r.py
+++ examples/space/simple_r.py
@@ -22,23 +22,7 @@ class Reals(odl.set.LinearSpace):
         return isinstance(other, Reals)
 
     def element(self, value=0):
-        return RealNumber(self, value)
-
-
-class RealNumber(odl.set.space.LinearSpaceElement):
-    """Real vectors are floats."""
-
-    __val__ = None
-
-    def __init__(self, space, v):
-        super(RealNumber, self).__init__(space)
-        self.__val__ = v
-
-    def __float__(self):
-        return self.__val__.__float__()
-
-    def __str__(self):
-        return str(self.__val__)
+        return float(value)
 
 
 R = Reals()
