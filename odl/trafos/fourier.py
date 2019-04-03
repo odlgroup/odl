b50f5e5d589047a936d7a658a03f071d1ba08667
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-04-03 16:08:38 +0200

    added correct whitespaces in fourier

--- odl/trafos/fourier.py
+++ odl/trafos/fourier.py
@@ -5,7 +5,9 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file, You can
 # obtain one at https://mozilla.org/MPL/2.0/.
+
 """Discretized Fourier transform on L^p spaces."""
+
 from __future__ import print_function, division, absolute_import
 import numpy as np
 
