2e0068b82e54ebcd1e02f4d82ce4320d88019074
kohr-h <ho.kohr@zoho.com>
2020-03-31 00:19:06 +0200

    Fix numpydoc path

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -30,7 +30,7 @@ except Exception as e:
     sys.exit(1)
 
 # Add numpydoc path
-sys.path.insert(0, "../numpydoc")
+sys.path.insert(0, os.path.abspath('../numpydoc'))
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
