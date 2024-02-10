48da34ccbe4a02628f1c8a16ba64b60b79426020
Paul Hausner <paul.hausner@it.uu.se>
2024-02-10 14:54:34 +0100

    run linter

--- odl/util/sparse.py
+++ odl/util/sparse.py
@@ -10,16 +10,18 @@ __all__ = ('COOMatrix',)
 class COOMatrix():
     """
     Custom COO matrix representation for creating product space operators.
-    
-    The columns, rows and data are stored in separate lists such that A[i[k], j[k]] = data[k].
-    
+
+    The columns, rows and data are stored in separate lists such that
+    A[i[k], j[k]] = data[k].
+
     """
+
     def __init__(self, data, index, shape):
-        
+
         # type check
         if len(data) != len(index[0]) or len(data) != len(index[1]):
             raise ValueError('data and index must have the same length')
-        
+
         self.__data = data
         self.__index = index
         self.__shape = shape
@@ -27,18 +29,18 @@ class COOMatrix():
     @property
     def row(self):
         return self.__index[0]
-    
+
     @property
     def col(self):
         return self.__index[1]
-    
+
     @property
     def shape(self):
         return self.__shape
-    
+
     @property
     def data(self):
         return self.__data
-    
+
     def __repr__(self):
         return f"COO matrix({self.data}, {self.index})"
