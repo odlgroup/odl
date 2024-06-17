f4a71c74476f37be99b76637a95c941ea947c49f
Justus Sagemüller <justussa@kth.se>
2024-06-17 11:28:30 +0200

    Propose using PyTorch convolution for finite-differences.

--- odl/discr/diff_ops.py
+++ odl/discr/diff_ops.py
@@ -555,12 +555,35 @@ class Divergence(PointwiseTensorFieldOperator):
 
     def _call(self, x, out=None):
         """Calculate the divergence of ``x``."""
-        if out is None:
-            out = self.range.element()
-        # print(f"{type(out.data)=}")
 
         ndim = self.range.ndim
         dx = self.range.cell_sides
+
+        backend = self.range.array_backend
+
+        if out is None:
+            if backend.impl == "pytorch" and len(x)==2:
+                import torch
+
+                dtype = x[0].data.dtype
+
+                assert(self.method=='backward'), f"{self.method=}"
+                assert(self.pad_mode=='constant')
+                assert(self.pad_const==0)
+
+                # Add singleton channel- and batch dimensions
+                horizconv_data = x[0].data[None,None]
+                horizconv_kern = torch.tensor([[[[-1,1]]]], dtype=dtype)
+                verticonv_data = x[1].data[None,None]
+                verticonv_kern = torch.tensor([[[[-1],[1]]]], dtype=dtype)
+                return self.range.element(
+                    torch.conv2d(horizconv_data, horizconv_kern, padding='same')[0,0]
+                      / dx[0]
+                  + torch.conv2d(verticonv_data, verticonv_kern, padding='same')[0,0]
+                      / dx[1]
+                  )
+            else:
+                out = self.range.element()
 
         tmp = self.range.element().data
 
