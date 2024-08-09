odlgroup/odl

commit c90044a9be5a3d76555491573ef65bb87d3acff2

Ensure mesh coordinate calculations are not carried out with dtype=object.
This would previously happen because meshgrids are passed in as ragged arrays,
and NumPy does not convert the rows to float dtype as should happen.

odl/discr/discr_utils.py
@@ -621,6 +621,8 @@ class _Interpolator(object):
 
         # iterate through dimensions
         for xi, cvec in zip(x, self.coord_vecs):
+            xi = np.asarray(xi, dtype = self.values.dtype)
+
             idcs = np.searchsorted(cvec, xi) - 1
 
             idcs[idcs < 0] = 0

commit 600fecd89eae318dc414184a4573751db6abbb07

For integral data, interpolating on integral points may not be appropriate.
One of the tests samples from an integral grid at non-integral points.
This failed after the explicit conversion introduced in c90044a.
Falling back to `float` fixes the test case, though perhaps it would
be better to select a dedicated meshing dtype.

odl/discr/discr_utils.py
@@ -621,7 +621,10 @@ class _Interpolator(object):
 
         # iterate through dimensions
         for xi, cvec in zip(x, self.coord_vecs):
-            xi = np.asarray(xi, dtype=self.values.dtype)
+            try:
+                xi = np.asarray(xi).astype(self.values.dtype, casting='safe')
+            except TypeError:
+                xi = np.asarray(xi, dtype=float)
 
             idcs = np.searchsorted(cvec, xi) - 1
 