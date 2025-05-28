3261cf0cd2971cd3a9c3269e0fe502963f409e0e
emilien <emilienvalat@gmail.com>
2025-05-28 14:12:44 +0200

    Removal of the deprecated odl_ufunc test fixture

--- odl/util/pytest_config.py
+++ odl/util/pytest_config.py
@@ -151,7 +151,6 @@ odl_scalar_dtype = simple_fixture(name='dtype',
 
 odl_elem_order = simple_fixture(name='order', params=['C'])
 
-odl_ufunc = simple_fixture('ufunc', [p[0] for p in odl.util.ufuncs.UFUNCS])
 odl_reduction = simple_fixture('reduction', ['sum', 'prod', 'min', 'max'])
 
 # More complicated ones with non-trivial documentation
