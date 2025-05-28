44b7569dfadd67528b0b170da68e8f9afb45cf44
emilien <emilienvalat@gmail.com>
2025-05-28 10:56:32 +0200

    Change to the NumpyWeighting interface to accomodate for device argument AND changing super class initialisation

--- odl/space/weightings/numpy_weighting.py
+++ odl/space/weightings/numpy_weighting.py
@@ -3,9 +3,10 @@ from .base_weighting import Weighting
 import array_api_compat.numpy as xp
 
 class NumpyWeighting(Weighting):
-    def __init__(self, **kwargs):
-        Weighting.__init__(self, **kwargs)
-    
+    def __init__(self, device:str, **kwargs):
+        
+        super(NumpyWeighting, self).__init__(device, **kwargs)
+
     @property
     def array_namespace(self):
         return xp
