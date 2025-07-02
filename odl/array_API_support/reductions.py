ac5220affe21db4a5fc7d9b0d15ba7d1425fbb46
emilien <emilienvalat@gmail.com>
2025-07-02 21:16:04 +0200

    Addition of the other "reductions" supported by the python array API and their documentation. There are two immediate todos, handling the kwargs and renaming the module

--- odl/array_API_support/reductions.py
+++ odl/array_API_support/reductions.py
@@ -7,24 +7,54 @@
 # obtain one at https://mozilla.org/MPL/2.0/.
 
 __all__ = (
-    'sum',
-    'prod',
-    'min',
+    'cumulative_prod',
+    'cumulative_sum',
     'max',
+    'mean',
+    'min',
+    'prod',
+    'std',
+    'sum',
+    'var'   
 )
 
-
+# TODO: add kwargs handling
+# TODO: rename module to 'statistical' to be array API compliant
 def _apply_reduction(operation: str, x):
     return x.space._element_reduction(operation=operation, x=x)
 
-def sum(x):
-    return _apply_reduction('sum', x)
+def cumulative_prod(x):
+    """Calculates the cumulative product of elements in the input array x."""
+    return _apply_reduction('cumulative_prod', x)
 
-def prod(x):
-    return _apply_reduction('prod', x)
-
-def min(x):
-    return _apply_reduction('min', x)
+def cumulative_sum(x):
+    """Calculates the cumulative sum of elements in the input array x."""
+    return _apply_reduction('cumulative_sum', x)
 
 def max(x):
+    """Calculates the maximum value of the input array x."""
     return _apply_reduction('max', x)
+
+def mean(x):
+    """Calculates the arithmetic mean of the input array x."""
+    return _apply_reduction('mean', x)
+
+def min(x):
+    """Calculates the minimum value of the input array x."""
+    return _apply_reduction('min', x)
+
+def prod(x):
+    "Calculates the product of input array x elements."
+    return _apply_reduction('prod', x)
+
+def std(x):
+    """Calculates the standard deviation of the input array x."""
+    return _apply_reduction('std', x)
+
+def sum(x):
+    """Calculates the sum of the input array x."""
+    return _apply_reduction('sum', x)
+
+def var(x):
+    """Calculates the variance of the input array x."""
+    return _apply_reduction('var', x)
