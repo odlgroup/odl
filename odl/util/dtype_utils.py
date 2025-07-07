0d13f073f2b365f736eea472d7cf403bed3a39c3
emilien <emilienvalat@gmail.com>
2025-07-07 15:53:32 +0200

    Continuation of removing the reliance of dtype_util functions on numpy. Step 5: Changing the behaviour of real_dtype and complex_dtype so that they don't rely on numpy anymore

--- odl/util/dtype_utils.py
+++ odl/util/dtype_utils.py
@@ -7,8 +7,6 @@ from functools import lru_cache
 import array_api_compat as xp
 # ODL imports
 from odl.array_API_support import lookup_array_backend
-import numpy as np
-from odl.util.print_utils import dtype_repr
 
 __all__ = (
     'is_available_dtype',
@@ -154,189 +152,39 @@ def is_real_dtype(dtype: "str | Number |xp.dtype") -> bool:
 #     """Return ``True`` if ``dtype`` is a real or a complex type."""
 #     return _convert_dtype(dtype) in SCALAR_DTYPES
 
-# def real_dtype(dtype: "str | Number |xp.dtype", default=None) -> str:
-#     """
-#     Returns the real counterpart of ``dtype`` if it exists
-#     Parameters
-#     ----------
-#     dtype :
-#         Input dtype
-#     default :
-#         Object to be returned if no real counterpart is found for
-#         ``dtype``, except for ``None``, in which case an error is raised.
-#     """
-#     dtype = _convert_dtype(dtype)
-#     if dtype in REAL_DTYPES:
-#         return dtype
-#     elif dtype in COMPLEX_DTYPES:
-#         return TYPE_PROMOTION_COMPLEX_TO_REAL[dtype]
-#     else:
-#         if default is None:
-#             raise ValueError(
-#                 f"no real counterpart exists for `dtype` {dtype}")
-#         else:
-#             return default
-        
-# def complex_dtype(dtype: "str | Number |xp.dtype", default=None) -> str:
-#     dtype = _convert_dtype(dtype)
-#     if dtype in COMPLEX_DTYPES:
-#         return dtype
-#     elif dtype in REAL_DTYPES:
-#         return TYPE_PROMOTION_REAL_TO_COMPLEX[dtype]
-#     else:
-#         if default is None:
-#             raise ValueError(
-#                 f"no complex counterpart exists for `dtype` {dtype}")
-#         else:
-#             return default
-
-
-# @lru_cache
-# def is_numeric_dtype(dtype):
-#     """Return ``True`` if ``dtype`` is a numeric type."""
-#     dtype = np.dtype(dtype)
-#     return np.issubdtype(getattr(dtype, 'base', None), np.number)
-
-
-# @lru_cache
-# def is_int_dtype(dtype):
-#     """Return ``True`` if ``dtype`` is an integer type."""
-#     dtype = np.dtype(dtype)
-#     return np.issubdtype(getattr(dtype, 'base', None), np.integer)
-
-
-# @lru_cache
-# def is_floating_dtype(dtype):
-#     """Return ``True`` if ``dtype`` is a floating point type."""
-#     return is_real_floating_dtype(dtype) or is_complex_floating_dtype(dtype)
-
-
-# @lru_cache
-# def is_real_dtype(dtype):
-#     """Return ``True`` if ``dtype`` is a real (including integer) type."""
-#     return is_numeric_dtype(dtype) and not is_complex_dtype(dtype)
-
-
-# @lru_cache
-# def is_floating_dtype(dtype):
-#     """Return ``True`` if ``dtype`` is a real floating point type."""
-#     dtype = np.dtype(dtype)
-#     return np.issubdtype(getattr(dtype, 'base', None), np.floating)
-
-
-# @lru_cache
-# def is_complex_dtype(dtype):
-#     """Return ``True`` if ``dtype`` is a complex floating point type."""
-#     dtype = np.dtype(dtype)
-#     return np.issubdtype(getattr(dtype, 'base', None), np.complexfloating)
-
-
-def real_dtype(dtype, default=None):
-    """Return the real counterpart of ``dtype`` if existing.
-
+def real_dtype(dtype: "str | Number |xp.dtype", default=None) -> str:
+    """
+    Returns the real counterpart of ``dtype`` if it exists
     Parameters
     ----------
     dtype :
-        Real or complex floating point data type. It can be given in any
-        way the `numpy.dtype` constructor understands.
+        Input dtype
     default :
         Object to be returned if no real counterpart is found for
         ``dtype``, except for ``None``, in which case an error is raised.
-
-    Returns
-    -------
-    real_dtype : `numpy.dtype`
-        The real counterpart of ``dtype``.
-
-    Raises
-    ------
-    ValueError
-        if there is no real counterpart to the given data type and
-        ``default == None``.
-
-    See Also
-    --------
-    complex_dtype
-
-    Examples
-    --------
-    Convert scalar dtypes:
-
-    >>> real_dtype(complex)
-    dtype('float64')
-    >>> real_dtype('complex64')
-    dtype('float32')
-    >>> real_dtype(float)
-    dtype('float64')
-
-    Dtypes with shape are also supported:
-
-    >>> real_dtype(np.dtype((complex, (3,))))
-    dtype(('<f8', (3,)))
-    >>> real_dtype(('complex64', (3,)))
-    dtype(('<f4', (3,)))
     """
-    dtype, dtype_in = np.dtype(dtype), dtype
-
-    if is_floating_dtype(dtype):
+    dtype = _convert_dtype(dtype)
+    if dtype in REAL_DTYPES:
         return dtype
-
-    try:
-        real_base_dtype = TYPE_PROMOTION_COMPLEX_TO_REAL[dtype.base]
-    except KeyError:
-        if default is not None:
-            return default
-        else:
-            raise ValueError('no real counterpart exists for `dtype` {}'
-                             ''.format(dtype_repr(dtype_in)))
-    else:
-        return np.dtype((real_base_dtype, dtype.shape))
-
-
-def complex_dtype(dtype, default=None):
-    """Return complex counterpart of ``dtype`` if existing, else ``default``.
-
-    Parameters
-    ----------
-    dtype :
-        Real or complex floating point data type. It can be given in any
-        way the `numpy.dtype` constructor understands.
-    default :
-        Object to be returned if no complex counterpart is found for
-        ``dtype``, except for ``None``, in which case an error is raised.
-
-    Returns
-    -------
-    complex_dtype : `numpy.dtype`
-        The complex counterpart of ``dtype``.
-
-    Raises
-    ------
-    ValueError
-        if there is no complex counterpart to the given data type and
-        ``default == None``.
-
-    Examples
-    --------
-    Convert scalar dtypes:
-
-    >>> complex_dtype(float)
-    dtype('complex128')
-    >>> complex_dtype('float32')
-    dtype('complex64')
-    >>> complex_dtype(complex)
-    dtype('complex128')
-
-    Dtypes with shape are also supported:
-
-    >>> complex_dtype(np.dtype((float, (3,))))
-    dtype(('<c16', (3,)))
-    >>> complex_dtype(('float32', (3,)))
-    dtype(('<c8', (3,)))
-    """
-    if dtype in REAL_DTYPES: 
-        return TYPE_PROMOTION_REAL_TO_COMPLEX[dtype]
     elif dtype in COMPLEX_DTYPES:
-        return dtype
+        return TYPE_PROMOTION_COMPLEX_TO_REAL[dtype]
     else:
-        raise ValueError(f'The dtype {dtype=} is neither complex {COMPLEX_DTYPES} nor real {REAL_DTYPES}. Make sure you pass a string dtype and not a np.dtype or a torch.dtype.')
+        if default is None:
+            raise ValueError(
+                f"no real counterpart exists for `dtype` {dtype}")
+        else:
+            return default
+        
+def complex_dtype(dtype: "str | Number |xp.dtype", default=None) -> str:
+    dtype = _convert_dtype(dtype)
+    if dtype in COMPLEX_DTYPES:
+        return dtype
+    elif dtype in REAL_DTYPES:
+        return TYPE_PROMOTION_REAL_TO_COMPLEX[dtype]
+    else:
+        if default is None:
+            raise ValueError(
+                f"no complex counterpart exists for `dtype` {dtype}")
+        else:
+            return default
+
