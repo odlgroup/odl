bdaa73dea96aea1fda6466d72e97031ae7d7e680
Justus Sagemüller <justussa@kth.se>
2025-09-22 12:10:03 +0200

    Ensure every sparse format is uniquely registered.

--- odl/sparse/backends/sparse_template.py
+++ odl/sparse/backends/sparse_template.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from typing import Callable
 
-_registered_sparse_implementations = {}
 _registered_sparse_formats = {}
 
 @dataclass
@@ -11,5 +10,8 @@ class SparseMatrixFormat:
     constructor : Callable
     is_sparse : Callable
     def __post_init__(self):
-        _registered_sparse_implementations[self.impl]  = self
-        _registered_sparse_formats[self.sparse_format] = self
+        if self.impl not in _registered_sparse_formats:
+            _registered_sparse_formats[self.impl] = {}
+        if self.sparse_format in _registered_sparse_formats[self.impl]:
+            raise KeyError(f"A {self.sparse_format} sparse format for backend {self.impl} is already registered. Every sparse format needs to have a unique identifier combination.")
+        _registered_sparse_formats[self.impl][self.sparse_format] = self
