e146e8c1516d518e85e83db8b3cc84c2e40a19e5
Justus Sagemüller <justussa@kth.se>
2025-10-20 14:14:21 +0200

    Workaround for duplicate module initialization when running in PyTest.

--- odl/core/sparse/backends/pytorch_backend.py
+++ odl/core/sparse/backends/pytorch_backend.py
@@ -1,15 +1,17 @@
 from torch import sparse_coo_tensor, Tensor, sparse_coo, matmul
 
-from odl.core.sparse.backends.sparse_template import SparseMatrixFormat
+from odl.core.sparse.backends.sparse_template import SparseMatrixFormat, _registered_sparse_formats
 
 def is_sparse_COO(matrix):
     return isinstance(matrix, Tensor) and matrix.is_sparse and matrix.layout == sparse_coo
     
-pytorch_coo_tensor = SparseMatrixFormat(
-    sparse_format='COO',
-    impl = 'pytorch',
-    constructor = sparse_coo_tensor,
-    is_of_this_sparse_format = is_sparse_COO,
-    to_dense = lambda matrix: matrix.to_dense(),
-    matmul_spmatrix_with_vector = matmul
-)
+if ('pytorch' not in _registered_sparse_formats
+        or 'COO' not in _registered_sparse_formats['pytorch']):
+    pytorch_coo_tensor = SparseMatrixFormat(
+        sparse_format='COO',
+        impl = 'pytorch',
+        constructor = sparse_coo_tensor,
+        is_of_this_sparse_format = is_sparse_COO,
+        to_dense = lambda matrix: matrix.to_dense(),
+        matmul_spmatrix_with_vector = matmul
+    )
--- odl/core/sparse/backends/scipy_backend.py
+++ odl/core/sparse/backends/scipy_backend.py
@@ -1,13 +1,15 @@
 from scipy.sparse import coo_matrix
 
-from odl.core.sparse.backends.sparse_template import SparseMatrixFormat
+from odl.core.sparse.backends.sparse_template import SparseMatrixFormat, _registered_sparse_formats
 
-scipy_coo_tensor = SparseMatrixFormat(
-    sparse_format='COO',
-    impl = 'scipy',
-    constructor = coo_matrix,
-    is_of_this_sparse_format = lambda x : isinstance(x, coo_matrix),
-    to_dense = lambda matrix: matrix.toarray(),
-    matmul_spmatrix_with_vector = lambda matrix, x: matrix.dot(x)
-)
+if ('scipy' not in _registered_sparse_formats
+        or 'COO' not in _registered_sparse_formats['scipy']):
+    scipy_coo_tensor = SparseMatrixFormat(
+        sparse_format='COO',
+        impl = 'scipy',
+        constructor = coo_matrix,
+        is_of_this_sparse_format = lambda x : isinstance(x, coo_matrix),
+        to_dense = lambda matrix: matrix.toarray(),
+        matmul_spmatrix_with_vector = lambda matrix, x: matrix.dot(x)
+    )
 
