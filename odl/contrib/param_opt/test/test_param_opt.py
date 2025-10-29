71fbc1fd9d71478e64e022f6ee3e60081500f97e
Justus Sagemüller <justussa@kth.se>
2025-10-29 18:40:18 +0100

    Get (hyper-) parameter optimization to work with PyTorch backend.
    Note: this seems to work like the NumPy version, but
    
    a) both versions occasionally fail a test. It is nondeterministic due
       to the use of noise. Not sure if the failures are due to unfulfillable
       initialization or an actual problem with the algorithms.
    b) the PyTorch version is _extremely slow_, roughly a factor 200.
       I suspect there is something bad happening here with the interaction
       between PyTorch building a computational graph, and SciPy running
       iterations over it that don't respect this graph or even allow it to
       get bigger and bigger.
       This should be confirmed by running with no_grad.

--- odl/contrib/param_opt/test/test_param_opt.py
+++ odl/contrib/param_opt/test/test_param_opt.py
@@ -15,10 +15,28 @@ import odl.contrib.fom
 import odl.contrib.param_opt
 from odl.core.util.testutils import simple_fixture
 
-space = simple_fixture('space',
-                       [odl.rn(3),
-                        odl.uniform_discr([0, 0], [1, 1], [9, 11]),
-                        odl.uniform_discr(0, 1, 10)])
+space_ids =    [ "ℝ³ (float32)", "ℝ³ (float64)"
+               , "uniform([0,1])", "uniform([0,1]²)" ]
+space_params = [ ('ℝ³', 'float32'), ('ℝ³', 'float64')
+               , ('uniform', 1), ('uniform', 2) ]
+
+@pytest.fixture(scope="module", ids=space_ids, params=space_params)
+def space(request, odl_impl_device_pairs):
+    name, variation = request.param
+    impl, device = 'numpy', 'cpu'
+    impl, device = odl_impl_device_pairs
+    if name == 'ℝ³':
+        dtype = variation
+        return odl.rn(3, dtype=dtype, impl=impl, device=device)
+    elif name == 'uniform':
+        dimension = variation
+        if dimension == 1:
+            return odl.uniform_discr(0, 1, 10, impl=impl, device=device)
+        elif dimension == 2:
+            return odl.uniform_discr([0, 0], [1, 1], [9, 11], impl=impl, device=device)
+     
+    raise ValueError('undefined space')
+
 
 fom = simple_fixture('fom',
                      [odl.contrib.fom.mean_squared_error,
