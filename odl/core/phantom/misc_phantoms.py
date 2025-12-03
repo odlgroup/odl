6862de193e619fa5a416a3ac635856d8af07cb98
emilien <emilienvalat@gmail.com>
2025-12-03 15:16:43 +0100

    Linting+Black of phantom package.

--- odl/core/phantom/misc_phantoms.py
+++ odl/core/phantom/misc_phantoms.py
@@ -1,4 +1,4 @@
-# Copyright 2014-2020 The ODL contributors
+# Copyright 2014-2025 The ODL contributors
 #
 # This file is part of ODL.
 #
@@ -8,13 +8,11 @@
 
 """Miscellaneous phantoms that do not fit in other categories."""
 
-from __future__ import absolute_import, division, print_function
-
 import sys
 
 import numpy as np
 
-__all__ = ('submarine', 'text')
+__all__ = ("submarine", "text")
 
 
 def submarine(space, smooth=True, taper=20.0):
@@ -39,11 +37,9 @@ def submarine(space, smooth=True, taper=20.0):
     if space.ndim == 2:
         if smooth:
             return _submarine_2d_smooth(space, taper)
-        else:
-            return _submarine_2d_nonsmooth(space)
-    else:
-        raise ValueError('phantom only defined in 2 dimensions, got {}'
-                         ''.format(space.ndim))
+        return _submarine_2d_nonsmooth(space)
+
+    raise ValueError(f"phantom only defined in 2 dimensions, got {space.ndim}")
 
 
 def _submarine_2d_smooth(space, taper):
@@ -51,7 +47,7 @@ def _submarine_2d_smooth(space, taper):
 
     def logistic(x, c):
         """Smoothed step function from 0 to 1, centered at 0."""
-        return 1. / (1 + np.exp(-c * x))
+        return 1.0 / (1 + np.exp(-c * x))
 
     def blurred_ellipse(x):
         """Blurred characteristic function of an ellipse.
@@ -91,8 +87,10 @@ def _submarine_2d_smooth(space, taper):
         out = np.ones_like(x[0])
         for xi, low, upp in zip(x, xlower, xupper):
             length = upp - low
-            out = out * (logistic((xi - low) / length, taper) *
-                         logistic((upp - xi) / length, taper))
+            out = out * (
+                logistic((xi - low) / length, taper)
+                * logistic((upp - xi) / length, taper)
+            )
         return out
 
     out = space.element(blurred_ellipse)
@@ -196,47 +194,45 @@ def text(space, text, font=None, border=0.2, inverted=True):
     from PIL import Image, ImageDraw, ImageFont
 
     if space.ndim != 2:
-        raise ValueError('`space` must be two-dimensional')
+        raise ValueError("`space` must be two-dimensional")
 
     if font is None:
         platform = sys.platform
-        if platform == 'win32':
+        if platform == "win32":
             # Windows
-            font = 'arial'
-        elif platform == 'darwin':
+            font = "arial"
+        elif platform == "darwin":
             # Mac OSX
-            font = 'Helvetica'
+            font = "Helvetica"
         else:
             # Assume platform is linux
-            font = 'LiberationSans-Regular'
+            font = "LiberationSans-Regular"
 
     text = str(text)
 
     # Figure out what font size we should use by creating a very high
     # resolution font and calculating the size of the text in this font
     init_size = 1000
-    init_pil_font = ImageFont.truetype(font + ".ttf", size=init_size,
-                                       encoding="unic")
+    init_pil_font = ImageFont.truetype(font + ".ttf", size=init_size, encoding="unic")
     init_text_width, init_text_height = init_pil_font.getsize(text)
 
     # True size is given by how much too large (or small) the example was
     scaled_init_size = (1.0 - border) * init_size
-    size = scaled_init_size * min([space.shape[0] / init_text_width,
-                                   space.shape[1] / init_text_height])
+    size = scaled_init_size * min(
+        [space.shape[0] / init_text_width, space.shape[1] / init_text_height]
+    )
     size = int(size)
 
     # Create font
-    pil_font = ImageFont.truetype(font + ".ttf", size=size,
-                                  encoding="unic")
+    pil_font = ImageFont.truetype(font + ".ttf", size=size, encoding="unic")
     text_width, text_height = pil_font.getsize(text)
 
     # create a blank canvas with extra space between lines
-    canvas = Image.new('RGB', space.shape, (255, 255, 255))
+    canvas = Image.new("RGB", space.shape, (255, 255, 255))
 
     # draw the text onto the canvas
     draw = ImageDraw.Draw(canvas)
-    offset = ((space.shape[0] - text_width) // 2,
-              (space.shape[1] - text_height) // 2)
+    offset = ((space.shape[0] - text_width) // 2, (space.shape[1] - text_height) // 2)
     white = "#000000"
     draw.text(offset, text, font=pil_font, fill=white)
 
@@ -252,17 +248,17 @@ def text(space, text, font=None, border=0.2, inverted=True):
     return space.element(arr)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Show the phantoms
     import odl
     from odl.core.util.testutils import run_doctests
 
     space = odl.uniform_discr([-1, -1], [1, 1], [300, 300])
-    submarine(space, smooth=False).show('submarine smooth=False')
-    submarine(space, smooth=True).show('submarine smooth=True')
-    submarine(space, smooth=True, taper=50).show('submarine taper=50')
+    submarine(space, smooth=False).show("submarine smooth=False")
+    submarine(space, smooth=True).show("submarine smooth=True")
+    submarine(space, smooth=True, taper=50).show("submarine taper=50")
 
-    text(space, text='phantom').show('phantom')
+    text(space, text="phantom").show("phantom")
 
     # Run also the doctests
     run_doctests()
--- odl/core/phantom/noise.py
+++ odl/core/phantom/noise.py
@@ -1,4 +1,4 @@
-# Copyright 2014-2019 The ODL contributors
+# Copyright 2014-2025 The ODL contributors
 #
 # This file is part of ODL.
 #
@@ -8,15 +8,12 @@
 
 """Functions to create noise samples of different distributions."""
 
-from __future__ import absolute_import, division, print_function
-
 import numpy as np
 
 from odl.core.util import npy_random_seed
 from odl.core.space.base_tensors import Tensor
 
-__all__ = ('white_noise', 'poisson_noise', 'salt_pepper_noise',
-           'uniform_noise')
+__all__ = ("white_noise", "poisson_noise", "salt_pepper_noise", "uniform_noise")
 
 
 def white_noise(space, mean=0, stddev=1, seed=None):
@@ -52,22 +49,18 @@ def white_noise(space, mean=0, stddev=1, seed=None):
 
     with npy_random_seed(seed):
         if isinstance(space, ProductSpace):
-            values = [white_noise(subspace, mean, stddev)
-                      for subspace in space]
+            values = [white_noise(subspace, mean, stddev) for subspace in space]
         else:
             if space.is_complex:
-                real = np.random.normal(
-                    loc=mean.real, scale=stddev, size=space.shape)
-                imag = np.random.normal(
-                    loc=mean.imag, scale=stddev, size=space.shape)
+                real = np.random.normal(loc=mean.real, scale=stddev, size=space.shape)
+                imag = np.random.normal(loc=mean.imag, scale=stddev, size=space.shape)
                 values = real + 1j * imag
             else:
                 if isinstance(mean, Tensor):
                     array_backend = mean.array_backend
                     mean = array_backend.to_numpy(mean)
-                
-                values = np.random.normal(
-                    loc=mean, scale=stddev, size=space.shape)
+
+                values = np.random.normal(loc=mean, scale=stddev, size=space.shape)
 
     return space.element(values)
 
@@ -108,18 +101,14 @@ def uniform_noise(space, low=0, high=1, seed=None):
 
     with npy_random_seed(seed):
         if isinstance(space, ProductSpace):
-            values = [uniform_noise(subspace, low, high)
-                      for subspace in space]
+            values = [uniform_noise(subspace, low, high) for subspace in space]
         else:
             if space.is_complex:
-                real = np.random.uniform(low=low.real, high=high.real,
-                                         size=space.shape)
-                imag = np.random.uniform(low=low.imag, high=high.imag,
-                                         size=space.shape)
+                real = np.random.uniform(low=low.real, high=high.real, size=space.shape)
+                imag = np.random.uniform(low=low.imag, high=high.imag, size=space.shape)
                 values = real + 1j * imag
             else:
-                values = np.random.uniform(low=low, high=high,
-                                           size=space.shape)
+                values = np.random.uniform(low=low, high=high, size=space.shape)
 
     return space.element(values)
 
@@ -162,16 +151,16 @@ def poisson_noise(intensity, seed=None):
 
     with npy_random_seed(seed):
         if isinstance(intensity.space, ProductSpace):
-            values = [poisson_noise(subintensity)
-                      for subintensity in intensity]
+            values = [poisson_noise(subintensity) for subintensity in intensity]
         else:
             values = np.random.poisson(intensity.asarray())
 
     return intensity.space.element(values)
 
 
-def salt_pepper_noise(vector, fraction=0.05, salt_vs_pepper=0.5,
-                      low_val=None, high_val=None, seed=None):
+def salt_pepper_noise(
+    vector, fraction=0.05, salt_vs_pepper=0.5, low_val=None, high_val=None, seed=None
+):
     """Add salt and pepper noise to vector.
 
     Salt and pepper noise replaces random elements in ``vector`` with
@@ -214,20 +203,25 @@ def salt_pepper_noise(vector, fraction=0.05, salt_vs_pepper=0.5,
 
     # Validate input parameters
     fraction, fraction_in = float(fraction), fraction
-    if not (0 <= fraction <= 1):
-        raise ValueError('`fraction` ({}) should be a float in the interval '
-                         '[0, 1]'.format(fraction_in))
+    if not 0 <= fraction <= 1:
+        raise ValueError(
+            f"`fraction` ({fraction_in}) should be a float in the interval [0, 1]"
+        )
 
     salt_vs_pepper, salt_vs_pepper_in = float(salt_vs_pepper), salt_vs_pepper
     if not (0 <= salt_vs_pepper <= 1):
-        raise ValueError('`salt_vs_pepper` ({}) should be a float in the '
-                         'interval [0, 1]'.format(salt_vs_pepper_in))
+        raise ValueError(
+            f"`salt_vs_pepper` ({salt_vs_pepper_in}) should be a float in the interval [0, 1]"
+        )
 
     with npy_random_seed(seed):
         if isinstance(vector.space, ProductSpace):
-            values = [salt_pepper_noise(subintensity, fraction, salt_vs_pepper,
-                                        low_val, high_val)
-                      for subintensity in vector]
+            values = [
+                salt_pepper_noise(
+                    subintensity, fraction, salt_vs_pepper, low_val, high_val
+                )
+                for subintensity in vector
+            ]
         else:
             # Extract vector of values
             values = vector.asarray().flatten()
@@ -241,9 +235,12 @@ def salt_pepper_noise(vector, fraction=0.05, salt_vs_pepper=0.5,
             # Create randomly selected points as a subset of image.
             a = np.arange(vector.size)
             np.random.shuffle(a)
-            salt_indices = a[:int(fraction * vector.size * salt_vs_pepper)]
-            pepper_indices = a[int(fraction * vector.size * salt_vs_pepper):
-                               int(fraction * vector.size)]
+            salt_indices = a[: int(fraction * vector.size * salt_vs_pepper)]
+            pepper_indices = a[
+                int(fraction * vector.size * salt_vs_pepper) : int(
+                    fraction * vector.size
+                )
+            ]
 
             values[salt_indices] = high_val
             values[pepper_indices] = low_val
@@ -252,27 +249,27 @@ def salt_pepper_noise(vector, fraction=0.05, salt_vs_pepper=0.5,
     return vector.space.element(values)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Show the phantoms
     import odl
     from odl.core.util.testutils import run_doctests
 
     r100 = odl.rn(100)
-    white_noise(r100).show('white_noise')
-    uniform_noise(r100).show('uniform_noise')
-    white_noise(r100, mean=5).show('white_noise with mean')
+    white_noise(r100).show("white_noise")
+    uniform_noise(r100).show("uniform_noise")
+    white_noise(r100, mean=5).show("white_noise with mean")
 
     c100 = odl.cn(100)
-    white_noise(c100).show('complex white_noise')
-    uniform_noise(c100).show('complex uniform_noise')
+    white_noise(c100).show("complex white_noise")
+    uniform_noise(c100).show("complex uniform_noise")
 
     discr = odl.uniform_discr([-1, -1], [1, 1], [300, 300])
-    white_noise(discr).show('white_noise 2d')
-    uniform_noise(discr).show('uniform_noise 2d')
+    white_noise(discr).show("white_noise 2d")
+    uniform_noise(discr).show("uniform_noise 2d")
 
     vector = odl.core.phantom.shepp_logan(discr, modified=True)
-    poisson_noise(vector * 100).show('poisson_noise 2d')
-    salt_pepper_noise(vector).show('salt_pepper_noise 2d')
+    poisson_noise(vector * 100).show("poisson_noise 2d")
+    salt_pepper_noise(vector).show("salt_pepper_noise 2d")
 
     # Run also the doctests
     run_doctests()
