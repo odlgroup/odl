0786765684dab5f2c9dca6688873d7e5d585989f
Justus Sagemüller <justussa@kth.se>
2025-12-05 11:06:33 +0100

    Update the copyright years at the start of each file.
    This is an extraction of part of the changes made by @Emvlt within https://github.com/odlgroup/odl/pull/1710.

--- odl/backends/sparse/pytorch_backend.py
+++ odl/backends/sparse/pytorch_backend.py
@@ -1,10 +1,24 @@
+# Copyright 2014-2025 The ODL contributors
+#
+# This file is part of ODL.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at https://mozilla.org/MPL/2.0/.
+
+# pylint: disable=line-too-long
+# pylint: disable=invalid-name
+
+"""PyTorch implementation of Sparse Matrices."""
+
 from torch import sparse_coo_tensor, Tensor, sparse_coo, matmul
 
 from odl.core.sparse.sparse_template import SparseMatrixFormat, _registered_sparse_formats
 
 def is_sparse_COO(matrix):
+    """Convenience function to check whether a matrix is a sparse PyTorch Tensor with the COO format"""
     return isinstance(matrix, Tensor) and matrix.is_sparse and matrix.layout == sparse_coo
-    
+
 if ('pytorch' not in _registered_sparse_formats
         or 'COO' not in _registered_sparse_formats['pytorch']):
     pytorch_coo_tensor = SparseMatrixFormat(
--- odl/backends/sparse/scipy_backend.py
+++ odl/backends/sparse/scipy_backend.py
@@ -1,3 +1,15 @@
+# Copyright 2014-2025 The ODL contributors
+#
+# This file is part of ODL.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at https://mozilla.org/MPL/2.0/.
+
+# pylint: disable=line-too-long
+
+"""Scipy implementation of Sparse Matrices."""
+
 from scipy.sparse import coo_matrix
 
 from odl.core.sparse.sparse_template import SparseMatrixFormat, _registered_sparse_formats
